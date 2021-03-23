ac0de62a95e9458d5a45e3b7a4c76e089d2dc3ea
kmiura <ken.miura1102@gmail.com>
2021-03-23 23:03:55 +0900

    Put validation function into associated function

--- src/authentication.rs
+++ src/authentication.rs
@@ -14,25 +14,61 @@ pub(crate) const SESSION_SIGN_KEY: [u8; 32] = [1; 32];
 // TODO: Consider and change KEY
 const PASSWORD_HASH_KEY: [u8; 4] = [0, 1, 2, 3];
 
+enum ValidationError {
+    EmailAddressFormatError(String),
+    PasswordFormatError(String),
+}
+
+const EMAIL_ADDRESS_MAX_LENGTH: usize = 254;
+//const EMAIL_REGEXP: &str = r"^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+const PASSWORD_MIN_LENGTH: usize = 8;
+const PASSWORD_MAX_LENGTH: usize = 32;
+// TOOD: Add password regexp
+//const PASSWORD_REGEXP: &str = r"";
+
 #[derive(Deserialize)]
 pub(crate) struct AuthInfo {
     email_address: String,
     password: String,
 }
 
+impl AuthInfo {
+    fn validate_format(self: &AuthInfo) -> Result<(), ValidationError> {
+        let mail_addr_length = self.email_address.len();
+        if mail_addr_length > EMAIL_ADDRESS_MAX_LENGTH {
+            let error_message = format!(
+                "email address length is {}: email address length must be {} or less",
+                mail_addr_length, EMAIL_ADDRESS_MAX_LENGTH
+            );
+            return Err(ValidationError::EmailAddressFormatError(error_message));
+        }
+        let pwd_length = self.password.len();
+        if pwd_length < PASSWORD_MIN_LENGTH || pwd_length > PASSWORD_MAX_LENGTH {
+            // NOTE: don't include password related information (pwd_length) for security
+            let error_message = format!(
+                "password length must be {} or more, {} or less",
+                PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH
+            );
+            return Err(ValidationError::PasswordFormatError(error_message));
+        }
+        // TODO: Add regular expression check
+        return Ok(());
+    }
+}
+
 #[post("/auth-request")]
 pub(crate) async fn auth_request(
-    info: web::Json<AuthInfo>,
+    auth_info: web::Json<AuthInfo>,
     pool: web::Data<Pool<ConnectionManager<PgConnection>>>,
     session: Session,
 ) -> HttpResponse {
-    let result = validate_auth_info_format(&info);
+    let result = auth_info.validate_format();
     if let Err(_e) = result {
         // TODO: Consider returning JSON format
         return HttpResponse::from_error(error::ErrorBadRequest("failed to authenticate account"));
     }
-    let mail_addr = info.email_address.clone();
-    let pwd = info.password.clone();
+    let mail_addr = auth_info.email_address.clone();
+    let pwd = auth_info.password.clone();
 
     let conn = pool.get().expect("failed to get connection");
 
@@ -54,7 +90,7 @@ pub(crate) async fn auth_request(
     }
 
     if auth_res {
-        let _ = session.set("email_address", &info.email_address);
+        let _ = session.set("email_address", &auth_info.email_address);
         let contents = "{ \"result\": \"OK\" }";
         HttpResponse::Ok().body(contents)
     } else {
@@ -64,10 +100,10 @@ pub(crate) async fn auth_request(
 
 #[post("/registration-request")]
 pub(crate) async fn registration_request(
-    info: web::Json<AuthInfo>,
+    auth_info: web::Json<AuthInfo>,
     pool: web::Data<Pool<ConnectionManager<PgConnection>>>,
 ) -> HttpResponse {
-    let result = validate_auth_info_format(&info);
+    let result = auth_info.validate_format();
     if let Err(_e) = result {
         // TODO: Consider returning JSON format
         return HttpResponse::from_error(error::ErrorBadRequest("failed to register account"));
@@ -75,11 +111,11 @@ pub(crate) async fn registration_request(
 
     use ring::hmac;
     let key = hmac::Key::new(hmac::HMAC_SHA512, &PASSWORD_HASH_KEY);
-    let hashed_password = hmac::sign(&key, info.password.as_bytes());
+    let hashed_password = hmac::sign(&key, auth_info.password.as_bytes());
 
     // トランザクションで、既存のDBにメールアドレスがあるかチェック＋登録
     // TODO: メールアドレスにUnique制約を追加するのか、トランザクションを利用するのか確認する
-    let mail_addr = info.email_address.clone();
+    let mail_addr = auth_info.email_address.clone();
     let conn = pool.get().expect("failed to get connection");
     let result =
         web::block(move || register_account(&mail_addr, hashed_password.as_ref(), &conn)).await;
@@ -110,7 +146,7 @@ pub(crate) async fn registration_request(
 
     let json_text = format!(
         "{{ \"message\": \"{}宛に登録用URLを送りました。登録用URLにアクセスし、登録を完了させてください（登録用URLの有効期間は24時間です）\"}}",
-        info.email_address
+        auth_info.email_address
     );
     HttpResponse::Ok().body(json_text)
 }
@@ -132,40 +168,6 @@ fn register_account(
         .values(&new_account)
         .execute(conn);
     result
-}
-
-enum ValidationError {
-    EmailAddressFormatError(String),
-    PasswordFormatError(String),
-}
-
-const EMAIL_ADDRESS_MAX_LENGTH: usize = 254;
-//const EMAIL_REGEXP: &str = r"^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
-const PASSWORD_MIN_LENGTH: usize = 8;
-const PASSWORD_MAX_LENGTH: usize = 32;
-// TOOD: Add password regexp
-//const PASSWORD_REGEXP: &str = r"";
-
-fn validate_auth_info_format(auth_info: &AuthInfo) -> Result<(), ValidationError> {
-    let mail_addr_length = auth_info.email_address.len();
-    if mail_addr_length > EMAIL_ADDRESS_MAX_LENGTH {
-        let error_message = format!(
-            "email address length is {}: email address length must be {} or less",
-            mail_addr_length, EMAIL_ADDRESS_MAX_LENGTH
-        );
-        return Err(ValidationError::EmailAddressFormatError(error_message));
-    }
-    let pwd_length = auth_info.password.len();
-    if pwd_length < PASSWORD_MIN_LENGTH || pwd_length > PASSWORD_MAX_LENGTH {
-        // NOTE: don't include password related information (pwd_length) for security
-        let error_message = format!(
-            "password length must be {} or more, {} or less",
-            PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH
-        );
-        return Err(ValidationError::PasswordFormatError(error_message));
-    }
-    // TODO: Add regular expression check
-    return Ok(());
 }
 
 // Use POST for logout: https://stackoverflow.com/questions/3521290/logout-get-or-post
