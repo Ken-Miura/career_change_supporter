269ee3bc50a79eac4bde4568d6ede27733c91d85
kmiura <ken.miura1102@gmail.com>
2021-05-18 23:29:20 +0900

    Add scripts to copy static files

---
+++ client/advisor_app/copy_static_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ../../server/service/static/advisor
+cp -r ./dist/* ../../server/service/static/advisor
+
---
+++ client/user_app/copy_static_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ../../server/service/static/user
+cp -r ./dist/* ../../server/service/static/user
+
