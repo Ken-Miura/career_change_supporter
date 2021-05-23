565ecc92ba984f7e3b780e872cdc5169eb4010b6
kmiura <ken.miura1102@gmail.com>
2021-05-23 17:43:48 +0900

    Change script

--- client/advisor_app/copy_static_files.sh
+++ client/advisor_app/copy_static_files.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mkdir -p ../../server/service/static/advisor
-cp -r ./dist/* ../../server/service/static/advisor
+DSTDIR=../../server/service/static/advisor
+if [ -d $DSTDIR ]; then
+  rm -r $DSTDIR
+fi
+mkdir -p $DSTDIR
+cp -r ./dist/* $DSTDIR 
 
