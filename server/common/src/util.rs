753a1b6199902e79de1c39b01df2a1a96d3ed429
kmiura <ken.miura1102@gmail.com>
2021-08-21 00:48:09 +0900

    Add tests

--- server/common/src/util.rs
+++ server/common/src/util.rs
@@ -93,6 +93,34 @@ mod tests {
     use super::*;
 
     #[test]
+    fn validate_email_address_returns_ok_if_given_valid_email_address_with_various_letters() {
+        let valid_email_address =
+            "The.quick.brown.fox.jumps.over.the.lazy.dog_0123456789@example.com";
+
+        let result = validate_email_address(valid_email_address);
+
+        assert!(
+            result.is_ok(),
+            "valid_email_address: {}",
+            valid_email_address
+        );
+    }
+
+    #[test]
+    fn validate_email_address_returns_ok_if_given_valid_email_address_of_254_characters() {
+        let valid_email_address =
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@example.com";
+
+        let result = validate_email_address(valid_email_address);
+
+        assert!(
+            result.is_ok(),
+            "valid_email_address: {}",
+            valid_email_address
+        );
+    }
+
+    #[test]
     fn validate_email_address_returns_invalid_length_if_given_empty_string() {
         let empty_str = "";
 
@@ -107,6 +135,101 @@ mod tests {
             } => assert!(length == 0, "length: {}", length),
             EmailAddressValidationError::InvalidFormat { email_address } => panic!(
                 "EmailAddressValidationError::InvalidFormat {{ email_address: \"{}\" }}",
+                email_address
+            ),
+        }
+    }
+
+    #[test]
+    fn validate_email_address_returns_invalid_length_if_given_255_characters() {
+        // valid format email address of 255 characters
+        let too_long_email_address = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@example.com";
+
+        let result = validate_email_address(too_long_email_address);
+
+        let err = result.expect_err("failed to get Err");
+        match err {
+            EmailAddressValidationError::InvalidLength {
+                length,
+                min_length: _,
+                max_length: _,
+            } => assert!(length == 255, "length: {}", length),
+            EmailAddressValidationError::InvalidFormat { email_address } => panic!(
+                "EmailAddressValidationError::InvalidFormat {{ email_address: \"{}\" }}",
+                email_address
+            ),
+        }
+    }
+
+    #[test]
+    fn validate_email_address_returns_invalid_format_if_given_one_letter() {
+        let one_letter = "a";
+
+        let result = validate_email_address(one_letter);
+
+        let err = result.expect_err("failed to get Err");
+        match err {
+            EmailAddressValidationError::InvalidLength {
+                length,
+                min_length: _,
+                max_length: _,
+            } => panic!(
+                "EmailAddressValidationError::InvalidLength: length: {}",
+                length
+            ),
+            EmailAddressValidationError::InvalidFormat { email_address } => {
+                assert_eq!(
+                    one_letter, &email_address,
+                    "email_address: {}",
+                    email_address
+                )
+            }
+        }
+    }
+
+    #[test]
+    fn validate_email_address_returns_invalid_format_if_given_email_address_without_domain() {
+        let email_address_without_domain = "test@";
+
+        let result = validate_email_address(email_address_without_domain);
+
+        let err = result.expect_err("failed to get Err");
+        match err {
+            EmailAddressValidationError::InvalidLength {
+                length,
+                min_length: _,
+                max_length: _,
+            } => panic!(
+                "EmailAddressValidationError::InvalidLength: length: {}",
+                length
+            ),
+            EmailAddressValidationError::InvalidFormat { email_address } => assert_eq!(
+                email_address_without_domain, &email_address,
+                "email_address: {}",
+                email_address
+            ),
+        }
+    }
+
+    #[test]
+    fn validate_email_address_returns_invalid_format_if_given_email_address_without_local_part() {
+        let email_address_without_local_part = "@example.com";
+
+        let result = validate_email_address(email_address_without_local_part);
+
+        let err = result.expect_err("failed to get Err");
+        match err {
+            EmailAddressValidationError::InvalidLength {
+                length,
+                min_length: _,
+                max_length: _,
+            } => panic!(
+                "EmailAddressValidationError::InvalidLength: length: {}",
+                length
+            ),
+            EmailAddressValidationError::InvalidFormat { email_address } => assert_eq!(
+                email_address_without_local_part, &email_address,
+                "email_address: {}",
                 email_address
             ),
         }
