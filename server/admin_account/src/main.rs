46dcac7a32aee8d5c2ef599bf804c0977e37f99f
kmiura <ken.miura1102@gmail.com>
2021-08-22 18:18:14 +0900

    Add skelton implement

--- server/admin_account/src/main.rs
+++ server/admin_account/src/main.rs
@@ -1,6 +1,24 @@
 // Copyright 2021 Ken Miura
 
+use std::{env::args, process::exit};
+
+const INVALID_ARG_LENGTH: i32 = 1;
+const INVALID_SUB_COMMAND: i32 = 2;
+
 fn main() {
-    // 世界よ、こんにちは
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    if args.len() < 2 {
+        println!("usage: {} [ create | list | update | delete ] [sub_commarnd_args...]", args[0]);
+        exit(INVALID_ARG_LENGTH);
+    }
+    let cmd = &args[1];
+    if cmd == "create" {
+    } else if cmd == "list" {
+    } else if cmd == "update" {
+    } else if cmd == "delete" {
+    } else {
+        println!("invalid subcommand: {}", cmd);
+        println!("valid subcommand [ create | list | update | delete ]");
+        exit(INVALID_SUB_COMMAND);
+    }
 }
