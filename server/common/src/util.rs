b388015bf64faacf50fb8cca2fcd56b0c8cab406
kmiura <ken.miura1102@gmail.com>
2021-08-22 17:09:21 +0900

    Change hashed password from str to binary

--- server/common/src/util.rs
+++ server/common/src/util.rs
@@ -2,30 +2,42 @@
 
 pub mod validator;
 
+use std::convert::From;
 use std::error::Error;
 use std::fmt::Display;
+use std::string::FromUtf8Error;
+
+use bcrypt::BcryptError;
 
 // TODO: リリース前に値を調整する (パスワードのストレッチングが2^BCRYPT_COST回実行される)
 const BCRYPT_COST: u32 = 7;
 
 /// Hash given password string.
-pub fn hash_password(password: &str) -> Result<String, PasswordHandlingError> {
-    let result = bcrypt::hash(password, BCRYPT_COST);
-    match result {
-        Ok(hashed_pwd_str) => Ok(hashed_pwd_str),
-        Err(e) => Err(PasswordHandlingError::UnexpectedError(Box::new(e))),
-    }
+pub fn hash_password(password: &str) -> Result<Vec<u8>, PasswordHandlingError> {
+    let hashed_pwd_str = bcrypt::hash(password, BCRYPT_COST)?;
+    let binary = hashed_pwd_str.as_bytes();
+    Ok(Vec::from(binary))
 }
 
 /// Check if password given matches hashed one.
 pub fn is_password_match(
     password: &str,
-    hashed_password: &str,
+    hashed_password: &[u8],
 ) -> Result<bool, PasswordHandlingError> {
-    let result = bcrypt::verify(password, &hashed_password);
-    match result {
-        Ok(is_match) => Ok(is_match),
-        Err(e) => Err(PasswordHandlingError::UnexpectedError(Box::new(e))),
+    let hashed_pwd_str = String::from_utf8(Vec::from(hashed_password))?;
+    let is_match = bcrypt::verify(password, &hashed_pwd_str)?;
+    Ok(is_match)
+}
+
+impl From<BcryptError> for PasswordHandlingError {
+    fn from(e: BcryptError) -> Self {
+        PasswordHandlingError::UnexpectedError(Box::new(e))
+    }
+}
+
+impl From<FromUtf8Error> for PasswordHandlingError {
+    fn from(e: FromUtf8Error) -> Self {
+        PasswordHandlingError::UnexpectedError(Box::new(e))
     }
 }
 
@@ -38,13 +50,14 @@ pub enum PasswordHandlingError {
 impl Display for PasswordHandlingError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            PasswordHandlingError::UnexpectedError(e) => write!(f, "failed to handle password: {}", e),
+            PasswordHandlingError::UnexpectedError(e) => {
+                write!(f, "failed to handle password: {}", e)
+            }
         }
     }
 }
 
-impl Error for PasswordHandlingError {
-}
+impl Error for PasswordHandlingError {}
 
 #[cfg(test)]
 mod tests {
@@ -52,18 +65,22 @@ mod tests {
     use validator::validate_password;
 
     #[test]
-    fn handle_password_match_case () {
+    fn handle_password_match_case() {
         let password = "0123456789abcdefghijKLMNOPQR@<>.";
         let _ = validate_password(password).expect("failed to get Ok");
 
         let hashed_pwd = hash_password(password).expect("failed to get Ok");
         let result = is_password_match(password, &hashed_pwd).expect("failed to get Ok");
 
-        assert!(result, "password: {}, hashed password: {}", password, hashed_pwd);
+        assert!(
+            result,
+            "password: {}, hashed password: {:?}",
+            password, hashed_pwd
+        );
     }
 
     #[test]
-    fn handle_password_non_match_case () {
+    fn handle_password_non_match_case() {
         let password1 = "0123456789abcdefghijKLMNOPQR@<>.";
         let password2 = "abcdefghi0123456789";
         let _ = validate_password(password1).expect("failed to get Ok");
@@ -72,6 +89,10 @@ mod tests {
         let hashed_pwd = hash_password(password1).expect("failed to get Ok");
         let result = is_password_match(password2, &hashed_pwd).expect("failed to get Ok");
 
-        assert!(!result, "password1: {}, hashed password: {}, password2: {}", password1, hashed_pwd, password2);
+        assert!(
+            !result,
+            "password1: {}, hashed password: {:?}, password2: {}",
+            password1, hashed_pwd, password2
+        );
     }
 }
