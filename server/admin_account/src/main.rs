45e465b2283612f2e9312c23a67bed848220d337
kmiura <ken.miura1102@gmail.com>
2021-08-22 23:34:54 +0900

    Add validation

--- server/admin_account/src/main.rs
+++ server/admin_account/src/main.rs
@@ -8,14 +8,43 @@ const INVALID_SUB_COMMAND: i32 = 2;
 fn main() {
     let args: Vec<String> = args().collect();
     if args.len() < 2 {
-        println!("usage: {} [ create | list | update | delete ] [sub_commarnd_args...]", args[0]);
+        println!(
+            "usage: {} [ create | list | update | delete ] [SUB_COMMAND_ARGS...]",
+            args[0]
+        );
         exit(INVALID_ARG_LENGTH);
     }
     let cmd = &args[1];
     if cmd == "create" {
+        if args.len() != 4 {
+            println!(
+                "usage: {} create \"admin_email_address\" \"password\"",
+                args[0]
+            );
+            println!("ex: {} create admin@test.com 1234abcdABCD", args[0]);
+            exit(INVALID_ARG_LENGTH);
+        }
     } else if cmd == "list" {
+        if args.len() != 2 {
+            println!("usage: {} list", args[0]);
+            println!("ex: {} list", args[0]);
+            exit(INVALID_ARG_LENGTH);
+        }
     } else if cmd == "update" {
+        if args.len() != 4 {
+            println!(
+                "usage: {} update \"admin_email_address\" \"password\"",
+                args[0]
+            );
+            println!("ex: {} update admin@test.com 1234abcdABCD", args[0]);
+            exit(INVALID_ARG_LENGTH);
+        }
     } else if cmd == "delete" {
+        if args.len() != 3 {
+            println!("usage: {} delete \"admin_email_address\"", args[0]);
+            println!("ex: {} delete admin@test.com", args[0]);
+            exit(INVALID_ARG_LENGTH);
+        }
     } else {
         println!("invalid subcommand: {}", cmd);
         println!("valid subcommand [ create | list | update | delete ]");
