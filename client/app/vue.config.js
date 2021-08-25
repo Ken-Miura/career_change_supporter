a97cbdde03fa47bdf6d9afd571451eca81a216fb
kmiura <ken.miura1102@gmail.com>
2021-08-25 23:04:26 +0900

    Change title

--- client/app/vue.config.js
+++ client/app/vue.config.js
@@ -2,6 +2,12 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map'
   },
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: 'Career Change Suppoter', // TODO: リリース前に変更
+    }
+  },
   devServer: {
     proxy: {
       '^/api/user': {
