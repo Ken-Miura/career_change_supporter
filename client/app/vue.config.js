767aa1f34d5a1f959313c147b540cc1f7dbc0856
kmiura <ken.miura1102@gmail.com>
2021-08-25 22:53:56 +0900

    Add devserver proxy info

--- client/app/vue.config.js
+++ client/app/vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   configureWebpack: {
     devtool: 'source-map'
-  },  
+  },
+  devServer: {
+    proxy: {
+      '^/api/user': {
+        target: 'http://localhost:3000/api/user/',
+        logLevel: 'debug',
+        pathRewrite: { '^/api/user': '/api/user' }
+      },
+      '^/api/advisor': {
+        target: 'http://localhost:3001/api/advisor/',
+        logLevel: 'debug',
+        pathRewrite: { '^/api/advisor': '/api/advisor' }
+      }
+    }
+  }
 }
