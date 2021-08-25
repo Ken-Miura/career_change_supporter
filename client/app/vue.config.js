4e7d14c305afce684b5470bb428d1d14bf49f4ff
kmiura <ken.miura1102@gmail.com>
2021-08-25 23:36:07 +0900

    Remove unnecessary prop

--- client/app/vue.config.js
+++ client/app/vue.config.js
@@ -13,12 +13,10 @@ module.exports = {
       '^/api/user': {
         target: 'http://localhost:3000',
         logLevel: 'debug',
-        pathRewrite: { '^/api/user': '/api/user' }
       },
       '^/api/advisor': {
         target: 'http://localhost:3001',
         logLevel: 'debug',
-        pathRewrite: { '^/api/advisor': '/api/advisor' }
       }
     }
   }
