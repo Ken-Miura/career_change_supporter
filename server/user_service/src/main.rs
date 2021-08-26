931b0bd853291f8bae96370fe1e162c2e85393c2
kmiura <ken.miura1102@gmail.com>
2021-08-27 01:23:13 +0900

    Specify threads num and connection num

--- server/user_service/src/main.rs
+++ server/user_service/src/main.rs
@@ -1,85 +1,56 @@
 // Copyright 2021 Ken Miura
 
-use axum::{
-    handler::{get, Handler},
-    http::StatusCode,
-    response::IntoResponse,
-    service, Router,
-};
-use std::{convert::Infallible, io, net::SocketAddr};
-use tower_http::services::{ServeDir, ServeFile};
+use axum::{handler::get, AddExtensionLayer, Router};
+use diesel::{r2d2::ConnectionManager, r2d2::Pool, PgConnection};
+use dotenv::dotenv;
+use std::env::set_var;
+use std::env::var;
+use std::{net::SocketAddr};
 
-#[tokio::main]
-async fn main() {
-    // Set the RUST_LOG, if it hasn't been explicitly defined
-    if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "service=debug,tower_http=debug");
-    }
+const KEY_TO_DATABASE_URL: &str = "DB_URL_FOR_ADMIN_ACCOUNT_APP";
+
+fn main() {
+    let num = num_cpus::get();
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(num)
+        .enable_all()
+        .build()
+        .expect("failed to build Runtime")
+        .block_on(main_internal(num as u32))
+}
+
+async fn main_internal(num_of_cpus: u32) {
+    let _ = dotenv().ok();
+    set_var("RUST_LOG", "user_service=debug,tower_http=debug");
     tracing_subscriber::fmt::init();
 
-    // our API routes
-    let api_routes = Router::new().route("/users", get(|| async { "users#index" }));
+    let database_url = var(KEY_TO_DATABASE_URL).unwrap_or_else(|_| {
+        panic!(
+            "Not environment variable found: environment variable \"{}\" must be set",
+            KEY_TO_DATABASE_URL
+        )
+    });
+    let manager = ConnectionManager::<PgConnection>::new(&database_url);
+    let pool: Pool<ConnectionManager<PgConnection>> = Pool::builder()
+        .max_size(num_of_cpus)
+        .build(manager)
+        .expect("failed to build connection pool");
 
     let app = Router::new()
         .nest(
-            "/",
-            Router::new()
-                .route(
-                    "/",
-                    service::get(ServeFile::new("service/static/index.html"))
-                        .handle_error(handle_io_error),
-                )
-                .route(
-                    "/index.html",
-                    service::get(ServeFile::new("service/static/index.html"))
-                        .handle_error(handle_io_error),
-                ),
+            "/api/users",
+            Router::new().route("/hello", get(handler)),
         )
-        .nest(
-            "/user",
-            Router::new()
-                .route(
-                    "/",
-                    service::get(ServeDir::new("service/static/user"))
-                        .handle_error(handle_io_error),
-                )
-                .or(
-                    service::any(ServeFile::new("service/static/user/user_app.html"))
-                        .handle_error(handle_io_error),
-                ),
-        )
-        .nest(
-            "/advisor",
-            Router::new()
-                .route(
-                    "/",
-                    service::get(ServeDir::new("service/static/advisor"))
-                        .handle_error(handle_io_error),
-                )
-                .or(
-                    service::any(ServeFile::new("service/static/advisor/advisor_app.html"))
-                        .handle_error(handle_io_error),
-                ),
-        )
-        // serve the API at `/api/*`
-        .nest("/api", api_routes)
-        .or(handler_404.into_service());
+        .layer(AddExtensionLayer::new(pool));
 
-    // run
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    axum::Server::bind(&addr)
+    tracing::info!("listening on {}", addr);
+    let _ = axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
-        .unwrap();
+        .expect("failed to serve app");
 }
 
-fn handle_io_error(error: io::Error) -> Result<impl IntoResponse, Infallible> {
-    Ok((
-        StatusCode::INTERNAL_SERVER_ERROR,
-        format!("Unhandled error: {}", error),
-    ))
-}
-
-async fn handler_404() -> impl IntoResponse {
-    (StatusCode::NOT_FOUND, "nothing to see here")
+async fn handler() -> &'static str {
+    "<h1>Hello, World!</h1>"
 }
