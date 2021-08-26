c970a41bc8a97d6c23ee348d81892d17ef10be3b
kmiura <ken.miura1102@gmail.com>
2021-08-27 02:01:07 +0900

    Extract waiting socket to env value

--- server/user_service/src/main.rs
+++ server/user_service/src/main.rs
@@ -5,9 +5,9 @@ use diesel::{r2d2::ConnectionManager, r2d2::Pool, PgConnection};
 use dotenv::dotenv;
 use std::env::set_var;
 use std::env::var;
-use std::{net::SocketAddr};
 
-const KEY_TO_DATABASE_URL: &str = "DB_URL_FOR_ADMIN_ACCOUNT_APP";
+const KEY_TO_DATABASE_URL: &str = "DB_URL_FOR_USER_APP";
+const KEY_TO_SOCKET: &str = "SOCKET_FOR_USER_APP";
 
 fn main() {
     let num = num_cpus::get();
@@ -31,19 +31,25 @@ async fn main_internal(num_of_cpus: u32) {
         )
     });
     let manager = ConnectionManager::<PgConnection>::new(&database_url);
+    // NOTE: bb8-dieselのcrate (https://crates.io/crates/bb8-diesel) がtokio 1.0系統に対応した後、r2d2からの移行を検討する
     let pool: Pool<ConnectionManager<PgConnection>> = Pool::builder()
         .max_size(num_of_cpus)
         .build(manager)
         .expect("failed to build connection pool");
 
     let app = Router::new()
-        .nest(
-            "/api/users",
-            Router::new().route("/hello", get(handler)),
-        )
+        .nest("/api/users", Router::new().route("/hello", get(handler)))
         .layer(AddExtensionLayer::new(pool));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let socket = var(KEY_TO_SOCKET).unwrap_or_else(|_| {
+            panic!(
+                "Not environment variable found: environment variable \"{}\" (example value: \"127.0.0.1:3000\") must be set",
+                KEY_TO_SOCKET
+            )
+        });
+    let addr = socket
+        .parse()
+        .unwrap_or_else(|_| panic!("failed to parse socket: {}", socket));
     tracing::info!("listening on {}", addr);
     let _ = axum::Server::bind(&addr)
         .serve(app.into_make_service())
