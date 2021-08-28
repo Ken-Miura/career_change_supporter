4398dced906ad2363c45075f22bae4cb281f2314
kmiura <ken.miura1102@gmail.com>
2021-08-28 19:44:37 +0900

    Remove unnecessary dependency

--- server/user_service/src/account.rs
+++ server/user_service/src/account.rs
@@ -1,6 +1,8 @@
 // Copyright 2021 Ken Miura
 
-use common::{DatabaseConnection, ValidCred};
+use axum::{http::StatusCode, Json};
+use common::{DatabaseConnection, RespResult, ValidCred};
+use serde::Serialize;
 
 /// 一時アカウントを作成する。<br>
 /// <br>
@@ -10,5 +12,17 @@ use common::{DatabaseConnection, ValidCred};
 async fn _post_temp_accounts(
     ValidCred(_cred): ValidCred,
     DatabaseConnection(_conn): DatabaseConnection,
-) {
+) -> RespResult<TempAccount> {
+    let ret = (
+        StatusCode::OK,
+        Json(TempAccount {
+            email_address: "test@test.com".to_string(),
+        }),
+    );
+    Ok(ret)
+}
+
+#[derive(Serialize)]
+struct TempAccount {
+    email_address: String,
 }
