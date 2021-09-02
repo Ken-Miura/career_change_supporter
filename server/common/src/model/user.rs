796c848012c609727f6756f1e2ebb03069230782
kmiura <ken.miura1102@gmail.com>
2021-09-03 01:27:11 +0900

    Add user

--- server/common/src/model/user.rs
+++ server/common/src/model/user.rs
@@ -1,6 +1,6 @@
 // Copyright 2021 Ken Miura
 
-// use crate::schema::career_change_supporter_schema::user_account;
+use crate::schema::ccs_schema::user_account;
 use crate::schema::ccs_schema::user_temp_account;
 
 #[derive(Insertable, Debug)]
@@ -20,18 +20,18 @@ pub struct TempAccount {
     pub created_at: chrono::DateTime<chrono::Utc>,
 }
 
-// #[derive(Insertable)]
-// #[table_name = "user_account"]
-// pub struct Account<'a> {
-//     pub email_address: &'a str,
-//     pub hashed_password: &'a [u8],
-//     pub last_login_time: Option<&'a chrono::DateTime<chrono::Utc>>,
-// }
+#[derive(Insertable)]
+#[table_name = "user_account"]
+pub struct NewAccount<'a> {
+    pub email_address: &'a str,
+    pub hashed_password: &'a [u8],
+    pub last_login_time: Option<&'a chrono::DateTime<chrono::Utc>>,
+}
 
-// #[derive(Clone, Queryable)]
-// pub struct AccountQueryResult {
-//     pub user_account_id: i32,
-//     pub email_address: String,
-//     pub hashed_password: Vec<u8>,
-//     pub last_login_time: Option<chrono::DateTime<chrono::Utc>>,
-// }
+#[derive(Clone, Queryable)]
+pub struct Account {
+    pub user_account_id: i32,
+    pub email_address: String,
+    pub hashed_password: Vec<u8>,
+    pub last_login_time: Option<chrono::DateTime<chrono::Utc>>,
+}
--- server/user_service/src/accounts.rs
+++ server/user_service/src/accounts.rs
@@ -1,15 +1,100 @@
 // Copyright 2021 Ken Miura
 
-use axum::extract::Query;
+use axum::http::StatusCode;
+use axum::{extract::Query, Json};
+use chrono::{DateTime, Utc};
+use common::model::user::NewAccount;
+use common::model::user::TempAccount;
+use common::smtp::{SendMail, SmtpClient, SOCKET_FOR_SMTP_SERVER};
+use common::util::validator::validate_uuid;
+use common::{DatabaseConnection, ErrResp, RespResult};
+use diesel::r2d2::{ConnectionManager, PooledConnection};
+use diesel::PgConnection;
 use serde::Deserialize;
+use serde::Serialize;
 
-/// アカウントを作成する
-pub(crate) async fn get_accounts(temp_account: Query<TempAccount>) -> String {
-    temp_account.temp_account_id.clone()
+/// アカウントを作成する<br>
+/// <br>
+/// # Errors
+pub(crate) async fn get_accounts(
+    temp_account: Query<TempAccountId>,
+    DatabaseConnection(conn): DatabaseConnection,
+) -> RespResult<AccountsResult> {
+    let current_date_time = chrono::Utc::now();
+    let op = AccountsOperationImpl::new(conn);
+    let smtp_client = SmtpClient::new(SOCKET_FOR_SMTP_SERVER.to_string());
+    get_accounts_internal(
+        &temp_account.temp_account_id,
+        &current_date_time,
+        op,
+        smtp_client,
+    )
+    .await
+}
+
+#[derive(Serialize, Debug)]
+pub(crate) struct AccountsResult {}
+
+async fn get_accounts_internal(
+    temp_account_id: &str,
+    current_date_time: &DateTime<Utc>,
+    op: impl AccountsOperation,
+    send_mail: impl SendMail,
+) -> RespResult<AccountsResult> {
+    let _ = validate_uuid(temp_account_id).map_err(|e| {});
+    let _ = async move {
+        let temp_account = op.find_temp_account_by_id(temp_account_id).unwrap();
+        // check if expired
+        todo!();
+        let exists = op.user_exists(&temp_account.email_address).unwrap();
+        if exists {
+            todo!();
+        }
+        let account = NewAccount {
+            email_address: &temp_account.email_address,
+            hashed_password: &temp_account.hashed_password,
+            last_login_time: None,
+        };
+        op.create_account(&account)
+    }
+    .await;
+    Ok((StatusCode::OK, Json(AccountsResult {})))
 }
 
 #[derive(Deserialize)]
-pub(crate) struct TempAccount {
-    #[serde(rename="temp-account-id")]
-    temp_account_id: String
+pub(crate) struct TempAccountId {
+    #[serde(rename = "temp-account-id")]
+    temp_account_id: String,
+}
+
+trait AccountsOperation {
+    // DBの分離レベルにはREAD COMITTEDを想定。
+    // その想定の上でトランザクションが必要かどうかを検討し、操作を分離して実装
+    fn find_temp_account_by_id(&self, temp_account_id: &str) -> Result<TempAccount, ErrResp>;
+    fn user_exists(&self, email_addr: &str) -> Result<bool, ErrResp>;
+    fn create_account(&self, temp_account: &NewAccount) -> Result<(), ErrResp>;
+}
+
+struct AccountsOperationImpl {
+    conn: PooledConnection<ConnectionManager<PgConnection>>,
+}
+
+impl AccountsOperationImpl {
+    fn new(conn: PooledConnection<ConnectionManager<PgConnection>>) -> Self {
+        Self { conn }
+    }
+}
+
+impl AccountsOperation for AccountsOperationImpl {
+    fn find_temp_account_by_id(&self, temp_account_id: &str) -> Result<TempAccount, ErrResp> {
+        todo!()
+    }
+
+    fn user_exists(&self, email_addr: &str) -> Result<bool, ErrResp> {
+        todo!()
+    }
+
+    fn create_account(&self, temp_account: &NewAccount) -> Result<(), ErrResp> {
+        todo!()
+    }
 }
