a4988f1ff17c6c939204aa3a68abe49a68bb8c8a
kmiura <ken.miura1102@gmail.com>
2021-09-13 00:07:13 +0900

    Add todo

--- server/user_service/src/logout.rs
+++ server/user_service/src/logout.rs
@@ -2,11 +2,20 @@
 
 use async_redis_session::RedisSessionStore;
 use axum::extract::{Extension, TypedHeader};
-use headers::Cookie;
+use common::ErrResp;
+use headers::{Cookie, HeaderMap};
+use hyper::StatusCode;
 
 /// ログアウトを行う
 pub(crate) async fn post_logout(
     TypedHeader(_cookies): TypedHeader<Cookie>,
     Extension(_store): Extension<RedisSessionStore>,
-) {
+) -> LogoutResult {
+    todo!()
 }
+
+/// ログアウトリクエストの結果を示す型
+pub(crate) type LogoutResult = Result<LogoutResp, ErrResp>;
+
+/// ログアウトに成功した場合に返却される型
+pub(crate) type LogoutResp = (StatusCode, HeaderMap);
