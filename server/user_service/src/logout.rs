f990e8237c7d3844eaff37ef36e1d6cb3d57e2f9
kmiura <ken.miura1102@gmail.com>
2021-09-13 01:17:50 +0900

    Add poc code

--- server/user_service/src/logout.rs
+++ server/user_service/src/logout.rs
@@ -3,22 +3,37 @@
 use async_redis_session::RedisSessionStore;
 use async_session::SessionStore;
 use axum::extract::{Extension, TypedHeader};
+use axum::http::StatusCode;
 use common::ErrResp;
-use headers::{Cookie, HeaderMap};
-use hyper::StatusCode;
+use headers::{Cookie, HeaderMap, HeaderMapExt};
 
 use crate::util::COOKIE_NAME;
+
+use axum::{body::Body, http::Request};
 
 /// ログアウトを行う
 /// <br>
 /// # Errors
 /// リクエストにcookieを含んでいない場合、ステータスコード400を返す<br>
 pub(crate) async fn post_logout(
-    TypedHeader(cookie): TypedHeader<Cookie>,
-    Extension(store): Extension<RedisSessionStore>,
-) -> LogoutResult {
-    post_logout_internal(&cookie, store).await;
-    todo!()
+    //TypedHeader(cookie): TypedHeader<Cookie>,
+    //Extension(store): Extension<RedisSessionStore>,
+    req: Request<Body>, //) -> LogoutResult {
+) {
+    let extentions = req.extensions();
+    let store = extentions
+        .get::<RedisSessionStore>()
+        .expect("failed to get value");
+    let headers = req.headers();
+    let result = headers.typed_try_get::<Cookie>();
+    match result {
+        Ok(option) => match option {
+            Some(cookie) => println!("cookie: {:?}", cookie),
+            None => println!("None"),
+        },
+        Err(e) => println!("err: {}", e),
+    }
+    //post_logout_internal(&cookie, store).await;
 }
 
 /// ログアウトリクエストの結果を示す型
