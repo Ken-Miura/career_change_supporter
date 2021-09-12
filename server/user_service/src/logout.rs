cf5e21625e9fc387852df938b56084e8a5a146ee
kmiura <ken.miura1102@gmail.com>
2021-09-12 23:48:21 +0900

    Add param

--- server/user_service/src/logout.rs
+++ server/user_service/src/logout.rs
@@ -1,3 +1,11 @@
 // Copyright 2021 Ken Miura
 
-pub(crate) async fn post_logout() {}
+use async_redis_session::RedisSessionStore;
+use axum::extract::{Extension, TypedHeader};
+use headers::Cookie;
+
+pub(crate) async fn post_logout(
+    TypedHeader(_cookies): TypedHeader<Cookie>,
+    Extension(_store): Extension<RedisSessionStore>,
+) {
+}
