a4f715c6e6544a8acde6187b576dc4a1b59631e5
kmiura <ken.miura1102@gmail.com>
2021-09-27 23:55:07 +0900

    Add link to landing

--- client/app/src/views/Landing.vue
+++ client/app/src/views/Landing.vue
@@ -4,7 +4,7 @@
     <nav id="header" class="fixed w-full z-30 top-0 text-white">
       <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div class="pl-4 flex items-center">
-          <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+          <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="/">
             就職先・転職先を見極めるためのサイト
           </a>
         </div>
