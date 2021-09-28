54e1b36a0f10f28eb144526090b2de3d68371b72
kmiura <ken.miura1102@gmail.com>
2021-09-28 23:06:00 +0900

    Create register page

--- client/app/src/views/NewAccount.vue
+++ client/app/src/views/NewAccount.vue
@@ -1,39 +1,34 @@
 <template>
   <div class="bg-gradient-to-r from-gray-500 to-gray-900 bo min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
     <header class="max-w-lg mx-auto">
-      <a href="#">
-        <h1 class="text-4xl font-bold text-white text-center">Startup</h1>
-      </a>
+      <router-link to="/">
+        <h1 class="text-2xl font-bold text-white text-center">就職先・転職先を見極めるためのサイト</h1>
+      </router-link>
     </header>
     <main class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
       <section>
-        <h3 class="font-bold text-2xl">Welcome to Startup</h3>
-        <p class="text-gray-600 pt-2">Sign in to your account.</p>
+        <h3 class="font-bold text-2xl">新規登録</h3>
       </section>
       <section class="mt-10">
         <form class="flex flex-col" method="POST" action="#">
           <div class="mb-6 pt-3 rounded bg-gray-200">
-            <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Email</label>
-            <input type="text" id="email" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
+            <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">メールアドレス</label>
+            <input type="email" minlength="1" maxlength="254" id="email" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
           </div>
           <div class="mb-6 pt-3 rounded bg-gray-200">
-            <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Password</label>
-            <input type="password" id="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
+            <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">パスワード</label>
+            <input type="password" id="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
           </div>
-          <div class="flex justify-end">
-            <a href="#" class="text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6">Forgot your password?</a>
+          <div class="mb-6 pt-3 rounded bg-gray-200">
+            <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">パスワード（確認）</label>
+            <input type="password" id="password_confirmation" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
           </div>
-          <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Sign In</button>
+          <button class="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">新規登録</button>
         </form>
       </section>
     </main>
-    <div class="max-w-lg mx-auto text-center mt-12 mb-6">
-      <p class="text-white">Don't have an account? <a href="#" class="font-bold hover:underline">Sign up</a>.</p>
-    </div>
     <footer class="max-w-lg mx-auto flex justify-center text-white">
-      <a href="#" class="hover:underline">Contact</a>
-      <span class="mx-3">•</span>
-      <a href="#" class="hover:underline">Privacy</a>
+      <router-link to="/" class="hover:underline">トップページへ</router-link>
     </footer>
   </div>
 </template>
