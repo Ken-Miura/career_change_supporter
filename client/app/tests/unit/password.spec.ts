5c00711696f4b390802a6a0e6c6ec0618ac6e325
kmiura <ken.miura1102@gmail.com>
2021-10-02 18:17:03 +0900

    Add test

--- client/app/tests/unit/password.spec.ts
+++ client/app/tests/unit/password.spec.ts
@@ -17,4 +17,31 @@ describe('Password.vue', () => {
     })
     expect(wrapper.text()).toMatch(label)
   })
+
+  it('emit on-password-updated event with input value', () => {
+    const wrapper = shallowMount(Password)
+    const input = wrapper.find('input')
+    const pwd = 'abcdABCD1234'
+    input.setValue(pwd)
+    const result = wrapper.emitted('on-password-updated')
+    if (result === undefined || result[0] === undefined) {
+      throw new Error('result === undefined || result[0] === undefined')
+    }
+    const resultArray = result[0] as unknown[]
+    expect(resultArray[0]).toBe(pwd)
+  })
+
+    it('emits on-password-updated event with input value', () => {
+    const wrapper = shallowMount(Password)
+    const input = wrapper.find('input')
+    const pwd = 'abcdABCD1234'
+    input.setValue(pwd)
+    const result = wrapper.emitted('on-password-updated')
+    if (result === undefined || result[0] === undefined) {
+      throw new Error('result === undefined || result[0] === undefined')
+    }
+    // 直前のチェックからresultとresult[0]がundefinedでないことは明白
+    const resultArray = result[0] as string[]
+    expect(resultArray[0]).toBe(pwd)
+  })
 })
