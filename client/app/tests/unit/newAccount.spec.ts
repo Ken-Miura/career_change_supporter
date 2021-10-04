caed37c76f9d5eec6fab020d7e0edcf5361e727a
kmiura <ken.miura1102@gmail.com>
2021-10-05 01:57:59 +0900

    Add router mock

--- client/app/tests/unit/newAccount.spec.ts
+++ client/app/tests/unit/newAccount.spec.ts
@@ -2,38 +2,33 @@ import { mount } from '@vue/test-utils'
 import NewAccount from '@/views/NewAccount.vue'
 import EmailAddress from '@/components/EmailAddress.vue'
 import Password from '@/components/Password.vue'
-import { useRouter } from 'vue-router'
 import { createTempAccount } from '@/util/new-account/CreateTempAccount'
 import { CreateTempAccountResp } from '@/util/new-account/CreateTempAccountResp'
+import { useRouter } from 'vue-router'
 
 jest.mock('@/util/new-account/CreateTempAccount')
 const createTempAccountMock = createTempAccount as jest.MockedFunction<typeof createTempAccount>
 
-jest.mock('vue-router')
-const useRouterMock = useRouter as jest.MockedFunction<typeof useRouter>
+// 参考: https://stackoverflow.com/questions/68763693/vue-routers-injection-fails-during-a-jest-unit-test
+const routerPushMock = jest.fn();
+jest.mock('vue-router', () => ({
+  useRouter: () => ({
+    push: routerPushMock,
+  }),
+}));
+const routerMock = useRouter()
 
 describe('NewAccount.vue', () => {
   it('dispalays', async () => {
     const emailAddress = 'test@example.com'
-    // const mockRoute = {
-    //   params: {
-    //     emailAddress: emailAddress
-    //   }
-    // }
-    // const mockRouter = {
-    //   push: jest.fn()
-    // }
     createTempAccountMock.mockResolvedValue(CreateTempAccountResp.create(emailAddress))
 
-    const wrapper = mount(NewAccount)
-    // const wrapper = mount(NewAccount, {
-    //   global: {
-    //     mocks: {
-    //       $route: mockRoute,
-    //       $router: mockRouter
-    //     }
-    //   }
-    // })
+    const wrapper = mount(NewAccount, {
+      global: {
+        // TODO: 要調査
+        stubs: ['router-link']
+      }
+    })
 
     const emailAddr = wrapper.findComponent(EmailAddress)
     const emailAddrInput = emailAddr.find('input')
@@ -49,7 +44,8 @@ describe('NewAccount.vue', () => {
     const button = wrapper.find('button')
     await button.trigger('submit')
     
-    // expect(mockRouter.push).toHaveBeenCalledTimes(1)
-    // expect(mockRouter.push).toHaveBeenCalledWith('/temp-account-created')
+    expect(routerPushMock).toHaveBeenCalledTimes(1)
+    const data = JSON.parse(`{ "name": "TempAccountCreated", "params": {"emailAddress": "${emailAddress}"} }`)
+    expect(routerPushMock).toHaveBeenCalledWith(data)
   })
 })
