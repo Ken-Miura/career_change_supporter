e6a9d45586cde41d71bc8f03fd63f4ae0e09a04c
kmiura <ken.miura1102@gmail.com>
2021-10-17 22:19:55 +0900

    Add tests

--- client/app/tests/unit/accountCreated.spec.ts
+++ client/app/tests/unit/accountCreated.spec.ts
@@ -4,6 +4,8 @@ import { mount, RouterLinkStub } from '@vue/test-utils'
 import { CreateAccountResp } from '@/util/account/CreateAccountResp'
 import flushPromises from 'flush-promises'
 import { Message } from '@/util/Message'
+import { ApiError, ApiErrorResp } from '@/util/ApiError'
+import { Code } from '@/util/Error'
 
 jest.mock('@/util/account/CreateAccount')
 const createAccountMock = createAccount as jest.MockedFunction<typeof createAccount>
@@ -52,5 +54,22 @@ describe('AccountCreated.vue', () => {
     const mainTag = wrapper.find('main')
     const h3Tag = mainTag.find('h3')
     expect(h3Tag.text()).toMatch(`${Message.INVALID_QUERY_PARAM}`)
+  })
+
+  it(`displays ${Message.INVALID_UUID_MESSAGE} when query has no temp-account-id`, async () => {
+    const apiErr = ApiError.create(Code.INVALID_UUID)
+    createAccountMock.mockResolvedValue(ApiErrorResp.create(400, apiErr))
+    queryObject = { 'temp-account-id': /* 31桁 */ 'bc999c52f1cc4801bfd9216cdebc076' }
+    const wrapper = mount(AccountCreated, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+    const mainTag = wrapper.find('main')
+    const h3Tag = mainTag.find('h3')
+    expect(h3Tag.text()).toMatch(`${Message.INVALID_UUID_MESSAGE}`)
   })
 })
