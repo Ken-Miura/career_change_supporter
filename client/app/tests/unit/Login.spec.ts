0e6825718f9614b34cff9c59566bbf2890be263a
kmiura <ken.miura1102@gmail.com>
2021-10-26 23:16:58 +0900

    Add tests

--- client/app/tests/unit/Login.spec.ts
+++ client/app/tests/unit/Login.spec.ts
@@ -1,5 +1,39 @@
+import { mount, RouterLinkStub } from '@vue/test-utils'
+import Login from '@/views/Login.vue'
+import { refresh } from '@/util/refresh/Refresh'
+import EmailAddress from '@/components/EmailAddress.vue'
+import AlertMessage from '@/components/AlertMessage.vue'
+import Password from '@/components/Password.vue'
+
+jest.mock('@/util/refresh/Refresh')
+const refreshMock = refresh as jest.MockedFunction<typeof refresh>
+
+const routerPushMock = jest.fn()
+jest.mock('vue-router', () => ({
+  useRouter: () => ({
+    push: routerPushMock
+  })
+}))
+
 describe('Login.vue', () => {
-  it('', () => {
-    console.log('test')
+  beforeEach(() => {
+    routerPushMock.mockClear()
+    refreshMock.mockReset()
+  })
+
+  it('has one EmailAddress, one Password and one AlertMessage', () => {
+    const wrapper = mount(Login, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    const emailAddresses = wrapper.findAllComponents(EmailAddress)
+    expect(emailAddresses.length).toBe(1)
+    const passwords = wrapper.findAllComponents(Password)
+    expect(passwords.length).toBe(1)
+    const alertMessages = wrapper.findAllComponents(AlertMessage)
+    expect(alertMessages.length).toBe(1)
   })
 })
