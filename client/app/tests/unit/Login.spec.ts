76a1b27fb1bc679776d79f125c7f9095807d9800
kmiura <ken.miura1102@gmail.com>
2021-10-27 23:55:23 +0900

    Add test

--- client/app/tests/unit/Login.spec.ts
+++ client/app/tests/unit/Login.spec.ts
@@ -4,6 +4,7 @@ import { refresh } from '@/util/refresh/Refresh'
 import EmailAddress from '@/components/EmailAddress.vue'
 import AlertMessage from '@/components/AlertMessage.vue'
 import Password from '@/components/Password.vue'
+import { Message } from '@/util/Message'
 
 jest.mock('@/util/refresh/Refresh')
 const refreshMock = refresh as jest.MockedFunction<typeof refresh>
@@ -79,5 +80,27 @@ describe('Login.vue', () => {
     await flushPromises()
 
     expect(routerPushMock).toHaveBeenCalledTimes(0)
+  })
+
+  it(`displays alert message ${Message.UNEXPECTED_ERR} when connection error happened on opening login page`, async () => {
+    const errDetail = 'connection error'
+    refreshMock.mockRejectedValue(new Error(errDetail))
+
+    const wrapper = mount(Login, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    expect(routerPushMock).toHaveBeenCalledTimes(0)
+    const alertMessage = wrapper.findComponent(AlertMessage)
+    const classes = alertMessage.classes()
+    expect(classes).not.toContain('hidden')
+    const resultMessage = alertMessage.text()
+    expect(resultMessage).toContain(Message.UNEXPECTED_ERR)
+    expect(resultMessage).toContain(errDetail)
   })
 })
