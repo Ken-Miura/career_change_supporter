f8156d359a0785c00fa242e4936d4c451404b52e
kmiura <ken.miura1102@gmail.com>
2021-11-21 20:55:25 +0900

    Add tests

---
+++ client/app/tests/unit/ApplyNewPasswordResultPage.spec.ts
@@ -0,0 +1,32 @@
+import { mount, RouterLinkStub } from '@vue/test-utils'
+import ApplyNewPasswordResultPage from '@/views/ApplyNewPasswordResultPage.vue'
+
+describe('ApplyNewPasswordResultPage.vue', () => {
+  it('just renders message when passed', () => {
+    const message = 'メッセージ'
+    const wrapper = mount(ApplyNewPasswordResultPage, {
+      props: { message },
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    const mainTag = wrapper.find('main')
+    const h3Tag = mainTag.find('h3')
+    expect(h3Tag.text()).toMatch(`${message}`)
+  })
+
+  it('does not render message when no props passed', () => {
+    const wrapper = mount(ApplyNewPasswordResultPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    const mainTag = wrapper.find('main')
+    const h3Tag = mainTag.find('h3')
+    expect(h3Tag.exists()).toBe(false)
+  })
+})
