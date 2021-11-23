0174bda41b3eb3be7f0bb880fe2d666bf3b60a62
kmiura <ken.miura1102@gmail.com>
2021-11-23 15:48:30 +0900

    Add config to remove data-test attrs

--- client/app/vue.config.js
+++ client/app/vue.config.js
@@ -1,3 +1,13 @@
+function removeDataTestAttrs (node) {
+  if (node.type === 1 /* NodeTypes.ELEMENT */) {
+    node.props = node.props.filter(prop =>
+      prop.type === 6 /* NodeTypes.ATTRIBUTE */
+        ? prop.name !== 'data-test'
+        : true
+    )
+  }
+}
+
 module.exports = {
   configureWebpack: {
     devtool: 'source-map'
@@ -16,5 +26,22 @@ module.exports = {
         logLevel: 'debug'
       }
     }
+  },
+  // chainWebpack内はテストのみで利用するdata-testカスタム属性を取り除く処理
+  // vue2用のプラグインはあるが、vue3用には動作しないので直接処理を記載
+  // parallel: falseは、npm run buildをパスさせるために必要となる
+  // 参考: https://stackoverflow.com/a/67923998/6331122
+  parallel: false, // !!IMPORTANT!! - see note below
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        options.compilerOptions = {
+          ...(options.compilerOptions || {}),
+          nodeTransforms: [removeDataTestAttrs]
+        }
+        return options
+      })
   }
 }
