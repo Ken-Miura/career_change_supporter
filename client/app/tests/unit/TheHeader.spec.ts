8a8f03d5dd9abed5f7bee8ac99c46da4bdfdb8a5
kmiura <ken.miura1102@gmail.com>
2021-11-23 16:00:32 +0900

    Add header test

--- client/app/tests/unit/TheHeader.spec.ts
+++ client/app/tests/unit/TheHeader.spec.ts
@@ -28,4 +28,58 @@ describe('TheHeader.vue', () => {
     const logoutHandle = wrapper.find('[data-test="p"]')
     expect(logoutHandle.exists)
   })
+
+  it('has list with a hidden attribute when created', () => {
+    const wrapper = mount(TheHeader, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    const list = wrapper.find('[data-test="div"]')
+    expect(list.exists)
+    const classes = list.classes()
+    expect(classes).toContain('hidden')
+  })
+
+  it('adds/remove hidden attribute on list every time menu button is pushed', async () => {
+    const wrapper = mount(TheHeader, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    const list = wrapper.find('[data-test="div"]')
+    expect(list.exists)
+    const firstState = list.classes()
+    expect(firstState).toContain('hidden')
+
+    const menuButton = wrapper.find('button')
+    await menuButton.trigger('click')
+
+    const secondState = list.classes()
+    expect(secondState).not.toContain('hidden')
+
+    await menuButton.trigger('click')
+
+    const thirdState = list.classes()
+    expect(thirdState).toContain('hidden')
+  })
+
+  it('moves LoginPage when logoutHandle is pushed', async () => {
+    const wrapper = mount(TheHeader, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    const logoutHandle = wrapper.find('[data-test="p"]')
+    await logoutHandle.trigger('click')
+
+    expect(routerPushMock).toHaveBeenCalledTimes(1)
+    expect(routerPushMock).toHaveBeenCalledWith('login')
+  })
 })
