39c2f119f2608a8279ed6b7c186f9aba3ed10274
kmiura <ken.miura1102@gmail.com>
2021-12-30 01:52:21 +0900

    Add bank info

--- client/app/src/views/personalized/ProfilePage.vue
+++ client/app/src/views/personalized/ProfilePage.vue
@@ -14,16 +14,16 @@
         <h3 class="font-bold text-2xl">ユーザー情報</h3>
         <p class="mt-2 text-lg">身分証明のために入力する情報で、相談申し込みを行うために必要となる情報です。他のユーザーに公開されることはありません。</p>
         <div v-if="identity !== null" class="m-4 text-2xl grid grid-cols-3">
-          <div class="mt-2 justify-self-start col-span-1">名前</div><div class="justify-self-start col-span-2">{{identity.last_name}} {{identity.first_name}}</div>
-          <div class="mt-2 justify-self-start col-span-1">フリガナ</div><div class="justify-self-start col-span-2">{{identity.last_name_furigana}} {{identity.first_name_furigana}}</div>
+          <div class="mt-2 justify-self-start col-span-1">名前</div><div class="justify-self-start col-span-2">{{ identity.last_name }} {{ identity.first_name }}</div>
+          <div class="mt-2 justify-self-start col-span-1">フリガナ</div><div class="justify-self-start col-span-2">{{ identity.last_name_furigana }} {{ identity.first_name_furigana }}</div>
           <div class="mt-2 justify-self-start col-span-1">性別</div><div v-if="identity.sex === 'male'" class="justify-self-start col-span-2">男性</div><div v-else class="justify-self-start col-span-2">女性</div>
-          <div class="mt-2 justify-self-start col-span-1">生年月日</div><div class="justify-self-start col-span-2">{{identity.date_of_birth.year}}年{{identity.date_of_birth.month}}月{{identity.date_of_birth.day}}日</div>
+          <div class="mt-2 justify-self-start col-span-1">生年月日</div><div class="justify-self-start col-span-2">{{ identity.date_of_birth.year }}年{{ identity.date_of_birth.month }}月{{ identity.date_of_birth.day }}日</div>
           <div class="mt-2 justify-self-start col-span-3">住所</div>
-          <div class="mt-2 ml-3 justify-self-start col-span-1">都道府県</div><div class="justify-self-start col-span-2">{{identity.prefecture}}</div>
-          <div class="mt-2 ml-3 justify-self-start col-span-1">市区町村</div><div class="justify-self-start col-span-2">{{identity.city}}</div>
-          <div class="mt-2 ml-3 justify-self-start col-span-1">番地</div><div class="justify-self-start col-span-2">{{identity.address_line1}}</div>
-          <div v-if="identity.address_line2 !== null" class="mt-2 ml-3 justify-self-start col-span-1">建物名・部屋番号</div><div v-if="identity.address_line2 !== null" class="justify-self-start col-span-2">{{identity.address_line2}}</div>
-          <div class="mt-2 justify-self-start col-span-1">電話番号</div><div class="justify-self-start col-span-2">{{identity.telephone_number}}</div>
+          <div class="mt-2 ml-3 justify-self-start col-span-1">都道府県</div><div class="justify-self-start col-span-2">{{ identity.prefecture }}</div>
+          <div class="mt-2 ml-3 justify-self-start col-span-1">市区町村</div><div class="justify-self-start col-span-2">{{ identity.city }}</div>
+          <div class="mt-2 ml-3 justify-self-start col-span-1">番地</div><div class="justify-self-start col-span-2">{{ identity.address_line1 }}</div>
+          <div v-if="identity.address_line2 !== null" class="mt-2 ml-3 justify-self-start col-span-1">建物名・部屋番号</div><div v-if="identity.address_line2 !== null" class="justify-self-start col-span-2">{{ identity.address_line2 }}</div>
+          <div class="mt-2 justify-self-start col-span-1">電話番号</div><div class="justify-self-start col-span-2">{{ identity.telephone_number }}</div>
         </div>
         <p v-else class="m-4 text-xl">ユーザー情報が設定されていません。</p>
         <button v-on:click="TODO" class="mt-4 bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200">ユーザー情報を編集する</button>
@@ -36,7 +36,7 @@
       <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
         <h3 class="font-bold text-2xl">相談一回（１時間）の相談料</h3>
         <p class="mt-2 text-lg">相談受け付けを行うために必要となる情報です。<span class=" text-red-500">相談申込みの判断に使われるため、他のユーザーに公開されます。</span></p>
-        <div v-if="feePerHourInYen != null" class="flex justify-end">
+        <div v-if="feePerHourInYen !== null" class="flex justify-end">
           <p class="m-4 text-2xl">{{ feePerHourInYen }}円</p>
         </div>
         <p v-else class="m-4 text-xl">相談料が設定されていません。</p>
@@ -45,12 +45,23 @@
       <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
         <h3 class="font-bold text-2xl">相談料の入金口座</h3>
         <p class="mt-2 text-lg">受け取った相談料を入金するための口座で、相談受け付けを行うために必要となる情報です。他のユーザーに公開されることはありません。ユーザー情報で身分証明が完了した姓名と異なる名義の口座は設定できません。</p>
-        <p class="mt-4 text-lg">相談料の入金口座サンプル</p>
+        <div v-if="bankAccount !== null" class="m-4 text-2xl grid grid-cols-3">
+          <div class="mt-2 justify-self-start col-span-1">銀行コード</div><div class="justify-self-start col-span-2">{{ bankAccount.bank_code }}</div>
+          <div class="mt-2 justify-self-start col-span-1">支店コード</div><div class="justify-self-start col-span-2">{{ bankAccount.branch_code }}</div>
+          <div class="mt-2 justify-self-start col-span-1">預金種別</div><div class="justify-self-start col-span-2">{{ bankAccount.account_type }}</div>
+          <div class="mt-2 justify-self-start col-span-1">口座番号</div><div class="justify-self-start col-span-2">{{ bankAccount.account_number }}</div>
+          <div class="mt-2 justify-self-start col-span-1">口座名義</div><div class="justify-self-start col-span-2">{{ bankAccount.account_holder_name }}</div>
+        </div>
+        <p v-else class="m-4 text-xl">相談料の入金口座が設定されていません。</p>
+        <button v-on:click="TODO" class="mt-4 bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200">相談料の入金口座を編集する</button>
       </div>
       <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
         <h3 class="font-bold text-2xl">今月の相談料の合計</h3>
-        <p class="mt-2 text-lg">今月承諾した相談の相談料の合計です。他のユーザーに公開されることはありません。</p>
-        <p class="mt-4 text-lg">サンプル</p>
+        <p class="mt-2 text-lg">今月受け付けし、承諾した相談の相談料の合計です。他のユーザーに公開されることはありません。</p>
+        <div v-if="profit !== null" class="flex justify-end">
+          <p class="m-4 text-2xl">{{ profit }}円</p>
+        </div>
+        <p v-else class="m-4 text-xl">まだ相談を受け付けていません。</p>
       </div>
       <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
         <h3 class="font-bold text-2xl">入金情報</h3>
@@ -77,6 +88,7 @@ import { GetProfileResp } from '@/util/profile/GetProfileResp'
 import { ApiErrorResp } from '@/util/ApiError'
 import { Identity } from '@/util/profile/Identity'
 import { useGetProfile } from './useGetProfile'
+import { BankAccount } from '@/util/profile/BankAccount'
 
 export default defineComponent({
   name: 'ProfilePage',
@@ -87,8 +99,10 @@ export default defineComponent({
   setup () {
     const { getProfileDone, getProfileFunc } = useGetProfile()
     const emailAddress = ref('')
-    const feePerHourInYen = ref(0 as number | null)
     const identity = ref(null as Identity | null)
+    const feePerHourInYen = ref(0 as number | null)
+    const bankAccount = ref(null as BankAccount | null)
+    const profit = ref(null as number | null)
     const router = useRouter()
     onMounted(async () => {
       const result = await getPageKindToDisplay()
@@ -106,8 +120,10 @@ export default defineComponent({
         const profile = response.getProfile()
         /* eslint-disable camelcase */
         emailAddress.value = profile.email_address
-        feePerHourInYen.value = profile.fee_per_hour_in_yen
         identity.value = profile.identity
+        feePerHourInYen.value = profile.fee_per_hour_in_yen
+        bankAccount.value = profile.bank_account
+        profit.value = profile.profit
         /* eslint-enable camelcase */
       } else if (response instanceof ApiErrorResp) {
         console.log('ApiErrorResp')
@@ -115,7 +131,7 @@ export default defineComponent({
         console.log('else')
       }
     })
-    return { getProfileDone, emailAddress, identity, feePerHourInYen }
+    return { getProfileDone, emailAddress, identity, feePerHourInYen, bankAccount, profit }
   }
 })
 </script>
