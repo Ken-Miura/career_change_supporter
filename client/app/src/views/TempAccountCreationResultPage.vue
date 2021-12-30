f0ddd13573929a23b11fbdf085c21d9ef8bb4676
kmiura <ken.miura1102@gmail.com>
2021-12-30 23:49:00 +0900

    Change layout of temp account creation result

--- client/app/src/views/TempAccountCreationResultPage.vue
+++ client/app/src/views/TempAccountCreationResultPage.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="bg-gradient-to-r from-gray-500 to-gray-900 bo min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
+  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
     <header class="max-w-lg mx-auto">
       <router-link to="/">
         <h1 class="text-2xl font-bold text-white text-center">就職先・転職先を見極めるためのサイト</h1>
       </router-link>
     </header>
-    <main class="flex justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 v-if="emailAddress != undefined" class="font-bold text-lg">{{ emailAddress }}宛にメールを送信しました。メールを確認し、新規登録を完了させて下さい（メールが届いていない場合、迷惑メールに振り分けられていないか、もしくは本サイトのドメインのメールの受信が許可されているかご確認下さい）</h3>
+    <main class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+      <div v-if="emailAddress === undefined" class="flex flex-col justify-center">
+        <p class="font-bold text-2xl">まだ新規登録は完了していません</p>
+        <p class="mt-4 font-bold text-lg">{{ emailAddress }}宛にメールを送信しました。メールに記載されたURLをクリックし、新規登録を完了させて下さい。</p>
+        <div class="mt-4" role="note">
+          <div class="bg-gray-600 text-white font-bold rounded-t px-4 py-2">
+            メールが届かない場合
+          </div>
+          <div class="border border-t-0 border-gray-600 rounded-b bg-white px-4 py-3 text-black">
+            下記の項目についてご確認下さい。
+            <ul class="list-disc list-inside">
+              <li>本サイトのドメインのメールの受信が許可されているかどうか</li>
+              <li>迷惑メールに振り分けられているかどうか、もしくはゴミ箱に入っているかどうか</li>
+              <li>URL付きのメール受信を許可しているかどうか</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </main>
     <footer class="max-w-lg mx-auto flex justify-center text-white">
       <router-link to="/" class="hover:underline">トップページへ</router-link>
--- client/app/src/views/personalized/SchedulePage.vue
+++ client/app/src/views/personalized/SchedulePage.vue
@@ -1,6 +1,6 @@
 <template>
   <TheHeader/>
-  <div class="bg-gradient-to-r from-gray-500 to-gray-900 bo min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
+  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
     <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
       <h3 class="font-bold text-lg">{{ message }}</h3>
     </main>
