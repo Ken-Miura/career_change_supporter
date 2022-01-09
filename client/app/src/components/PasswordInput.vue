11426b852e6a77f8584f788b1da3b17e1ced3130
kmiura <ken.miura1102@gmail.com>
2022-01-09 17:53:35 +0900

    Remove unused for

--- client/app/src/components/PasswordInput.vue
+++ client/app/src/components/PasswordInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pt-3 rounded bg-gray-200">
-    <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">{{ label }}</label>
+    <label class="block text-gray-700 text-sm font-bold mb-2 ml-3">{{ label }}</label>
     <input @input="onInput" type="password" required pattern="[!-~]{10,32}" title="英大文字、英小文字、数字、記号の内、2種類以上を組み合わせた10文字以上32文字以下の文字列" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
   </div>
 </template>
