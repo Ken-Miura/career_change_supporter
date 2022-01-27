39927f49758b4023fd9b4c088743ac920b95858d
kmiura <ken.miura1102@gmail.com>
2022-01-28 00:23:34 +0900

    Add file name

--- server/user_service/src/identity.rs
+++ server/user_service/src/identity.rs
@@ -22,6 +22,10 @@ pub(crate) async fn post_identity(
     println!("account id: {}", account_id);
     while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap().to_string();
+        let file_name_option = field.file_name();
+        if let Some(file_name) = file_name_option {
+            println!("file name:  `{}`", file_name);
+        }
         let data = field.bytes().await.unwrap();
         println!("Length of `{}` is {} bytes", name, data.len());
     }
