57632f13a8518a84399cae1a9ce695ab225455fb
kmiura <ken.miura1102@gmail.com>
2022-02-13 18:09:39 +0900

    Change project settings

---
+++ vetur.config.js
@@ -0,0 +1,16 @@
+// https://vuejs.github.io/vetur/guide/setup.html#advanced
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+    // **optional** default: `{}`
+    // override vscode settings
+    // Notice: It only affects the settings used by Vetur.
+    settings: {
+      "vetur.useWorkspaceDependencies": true,
+      "vetur.experimental.templateInterpolationService": true
+    },
+    // **optional** default: `[{ root: './' }]`
+    // support monorepos
+    projects: [
+      './client/app', // Shorthand for specifying only the project root location
+    ]
+  }
