08246268c9180b976fed05cec876a6bd7cb7a644
kmiura <ken.miura1102@gmail.com>
2022-02-19 16:25:34 +0000

    Add migration

--- server/migration/src/m20220101_000001_create_table.rs
+++ server/migration/src/m20220101_000001_create_table.rs
@@ -1,3 +1,5 @@
+// Copyright 2021 Ken Miura
+
 use entity::sea_orm::{DatabaseBackend, Statement};
 use sea_schema::migration::*;
 
@@ -16,9 +18,21 @@ impl MigrationTrait for Migration {
         let db_backend = manager.get_database_backend();
         let sql = Sql::new(db_backend);
 
+        // SCHEMA定義
         let _ = conn
-            /* TODO: パスワード変更＋管理方法の検討 */
+            /* ccs = Career Change Supporter */
+            .execute(sql.stmt(r"CREATE SCHEMA ccs_schema;"))
+            .await
+            .map(|_| ())?;
+        // ROLE定義
+        // TODO: ROLEのパスワード変更＋管理方法の検討
+        let _ = conn
+            /*  */
             .execute(sql.stmt(r"CREATE ROLE user_app WITH LOGIN PASSWORD 'test1234';"))
+            .await
+            .map(|_| ())?;
+        let _ = conn
+            .execute(sql.stmt(r"GRANT USAGE ON SCHEMA ccs_schema TO user_app;"))
             .await
             .map(|_| ())?;
         let _ = conn
@@ -26,43 +40,41 @@ impl MigrationTrait for Migration {
             .await
             .map(|_| ())?;
         let _ = conn
+            .execute(sql.stmt(r"GRANT USAGE ON SCHEMA ccs_schema TO admin_app;"))
+            .await
+            .map(|_| ())?;
+        let _ = conn
             .execute(sql.stmt(r"CREATE ROLE admin_account_app WITH LOGIN PASSWORD 'test24680';"))
             .await
             .map(|_| ())?;
         let _ = conn
-            /* ccs = Career Change Supporter */
-            .execute(sql.stmt(r"CREATE SCHEMA ccs_schema;"))
+            .execute(sql.stmt(r"GRANT USAGE ON SCHEMA ccs_schema TO admin_account_app;"))
             .await
             .map(|_| ())?;
-
-        // let sql = "GRANT USAGE ON SCHEMA ccs_schema TO user_app;";
-        // let stmt = Statement::from_string(db_backend, sql.to_owned());
-        // let _ = conn.execute(stmt).await.map(|_| ())?;
-
-        // let sql = "GRANT USAGE ON SCHEMA ccs_schema TO admin_app;";
-        // let stmt = Statement::from_string(db_backend, sql.to_owned());
-        // let _ = conn.execute(stmt).await.map(|_| ())?;
-
-        // let sql = "GRANT USAGE ON SCHEMA ccs_schema TO admin_account_app;";
-        // let stmt = Statement::from_string(db_backend, sql.to_owned());
-        // let _ = conn.execute(stmt).await.map(|_| ())?;
-
-        // let sql = r"CREATE DOMAIN ccs_schema.email_address AS VARCHAR (254) CHECK ( VALUE ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );";
-        // let stmt = Statement::from_string(db_backend, sql.to_owned());
-        // let _ = conn.execute(stmt).await.map(|_| ())?;
-
-        // let sql = r"CREATE DOMAIN ccs_schema.uuid_simple_form AS CHAR (32) CHECK ( VALUE ~ '^[a-zA-Z0-9]+$' );";
-        // let stmt = Statement::from_string(db_backend, sql.to_owned());
-        // let _ = conn.execute(stmt).await.map(|_| ())?;
-
-        // let sql = r"CREATE DOMAIN ccs_schema.tenant_id AS VARCHAR (100) CHECK ( VALUE ~ '^[-_0-9a-zA-Z]{1,100}$' );";
-        // let stmt = Statement::from_string(db_backend, sql.to_owned());
-        // let _ = conn.execute(stmt).await.map(|_| ())?;
-
-        // let sql = r"CREATE DOMAIN ccs_schema.contract_type AS VARCHAR (8) CHECK (VALUE ~ 'regular' OR VALUE ~ 'contract' OR VALUE ~ 'other');";
-        // let stmt = Statement::from_string(db_backend, sql.to_owned());
-        // let _ = conn.execute(stmt).await.map(|_| ())?;
-
+        // DOMAIN定義
+        let _ = conn
+            .execute(sql.stmt(r"CREATE DOMAIN ccs_schema.email_address AS VARCHAR (254) CHECK ( VALUE ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );"))
+            .await
+            .map(|_| ())?;
+        let _ = conn
+            /* simpleフォーム (半角英数字32文字。ハイフン、波括弧を含まない) での入出力を行いたいので、標準のUUID型を使わない */
+            .execute(sql.stmt(r"CREATE DOMAIN ccs_schema.uuid_simple_form AS CHAR (32) CHECK ( VALUE ~ '^[a-zA-Z0-9]+$' );"))
+            .await
+            .map(|_| ())?;
+        let _ = conn
+            /* PAY.JPより回答してもらった仕様をそのままチェック */
+            .execute(sql.stmt(r"CREATE DOMAIN ccs_schema.tenant_id AS VARCHAR (100) CHECK ( VALUE ~ '^[-_0-9a-zA-Z]{1,100}$' );"))
+            .await
+            .map(|_| ())?;
+        let _ = conn
+            /* 
+             * regular: 正社員、contract: 契約社員、other: その他
+             * TODO: enumがサポートされた後、修正する
+             */
+            .execute(sql.stmt(r"CREATE DOMAIN ccs_schema.contract_type AS VARCHAR (8) CHECK (VALUE ~ 'regular' OR VALUE ~ 'contract' OR VALUE ~ 'other');"))
+            .await
+            .map(|_| ())?;
+        // その他（TABLE、INDEX等）の定義
         // let sql = r"CREATE TABLE ccs_schema.user_account (
         //     user_account_id SERIAL PRIMARY KEY,
         //     /* NOTE: email_addressがUNIQUEであることに依存するコードとなっているため、UNIQUEを外さない */
