dfa4c4eb98d7871415957f837139f6b3574abe72
kmiura <ken.miura1102@gmail.com>
2022-02-26 05:15:54 +0000

    Change seaorm

--- server/user_service/src/agreement.rs
+++ server/user_service/src/agreement.rs
@@ -8,7 +8,13 @@ use axum::Json;
 use chrono::{DateTime, FixedOffset, Utc};
 use common::ApiError;
 use common::ErrResp;
+use entity::prelude::TermsOfUse;
+use entity::prelude::UserAccount;
+use entity::sea_orm::ActiveModelTrait;
 use entity::sea_orm::DatabaseConnection;
+use entity::sea_orm::EntityTrait;
+use entity::sea_orm::Set;
+use entity::terms_of_use;
 use tower_cookies::Cookies;
 
 use crate::err::unexpected_err_resp;
@@ -38,12 +44,31 @@ async fn handle_agreement_req(
     agreed_at: &DateTime<FixedOffset>,
     op: impl AgreementOperation,
 ) -> Result<StatusCode, ErrResp> {
+    // 利用規約同意のデータに連絡先としてメールアドレスを保管しておきたいため
+    // ユーザー情報を取得する
     let option = op.find_account_by_id(account_id).await?;
     let account = option.ok_or_else(|| {
         // 利用規約に同意するリクエストを送った後、アカウント情報が取得される前にアカウントが削除されるような事態は
         // 通常の操作では発生し得ない。そのため、unexpected_err_respとして処理する。
         unexpected_err_resp()
     })?;
+    let agreement_date_option = op.check_if_already_agreed(account_id, version).await?;
+    if let Some(agreement_date) = agreement_date_option {
+        tracing::error!(
+            "{} (account id: {}) has already agreed terms of use version {} at {}",
+            &account.email_address,
+            account_id,
+            version,
+            agreement_date
+        );
+        return Err((
+            StatusCode::BAD_REQUEST,
+            Json(ApiError {
+                code: AlreadyAgreedTermsOfUse as u32,
+            }),
+        ));
+    }
+    // ACID特性が重要な箇所ではないので、op.check_if_already_agreedとまとめてトランザクションにしていない
     let _ = op
         .agree_terms_of_use(account_id, version, &account.email_address, agreed_at)
         .await?;
@@ -60,6 +85,11 @@ async fn handle_agreement_req(
 #[async_trait]
 trait AgreementOperation {
     async fn find_account_by_id(&self, account_id: i32) -> Result<Option<Account>, ErrResp>;
+    async fn check_if_already_agreed(
+        &self,
+        account_id: i32,
+        version: i32,
+    ) -> Result<Option<AgreedDateTime>, ErrResp>;
     async fn agree_terms_of_use(
         &self,
         account_id: i32,
@@ -69,6 +99,8 @@ trait AgreementOperation {
     ) -> Result<(), ErrResp>;
 }
 
+type AgreedDateTime = DateTime<FixedOffset>;
+
 struct AgreementOperationImpl {
     pool: DatabaseConnection,
 }
@@ -77,30 +109,41 @@ impl AgreementOperationImpl {
     fn new(pool: DatabaseConnection) -> Self {
         Self { pool }
     }
-
-    fn check_if_unique_violation(e: &diesel::result::Error) -> bool {
-        matches!(
-            e,
-            diesel::result::Error::DatabaseError(
-                diesel::result::DatabaseErrorKind::UniqueViolation,
-                _,
-            )
-        )
-    }
 }
 
 #[async_trait]
 impl AgreementOperation for AgreementOperationImpl {
     async fn find_account_by_id(&self, account_id: i32) -> Result<Option<Account>, ErrResp> {
-        // let result = user_account_table
-        //     .find(id)
-        //     .load::<Account>(&self.conn)
-        //     .map_err(|e| {
-        //         tracing::error!("failed to find user account (id: {}): {}", id, e);
-        //         unexpected_err_resp()
-        //     })?;
-        // Ok(result)
-        todo!()
+        let model = UserAccount::find_by_id(account_id)
+            .one(&self.pool)
+            .await
+            .map_err(|e| {
+                tracing::error!("failed to find user (account id: {}): {}", account_id, e);
+                unexpected_err_resp()
+            })?;
+        Ok(model.map(|m| Account {
+            email_address: m.email_address,
+        }))
+    }
+
+    async fn check_if_already_agreed(
+        &self,
+        account_id: i32,
+        version: i32,
+    ) -> Result<Option<AgreedDateTime>, ErrResp> {
+        let model = TermsOfUse::find_by_id((account_id, version))
+            .one(&self.pool)
+            .await
+            .map_err(|e| {
+                tracing::error!(
+                    "failed to find terms of use (account id: {}, version: {}): {}",
+                    account_id,
+                    version,
+                    e
+                );
+                unexpected_err_resp()
+            })?;
+        Ok(model.map(|m| m.agreed_at))
     }
 
     async fn agree_terms_of_use(
@@ -110,40 +153,18 @@ impl AgreementOperation for AgreementOperationImpl {
         email_address: &str,
         agreed_at: &DateTime<FixedOffset>,
     ) -> Result<(), ErrResp> {
-        // let terms_of_use = NewTermsOfUse {
-        //     user_account_id: &id,
-        //     ver: &version,
-        //     email_address,
-        //     agreed_at,
-        // };
-        // let _ = insert_into(terms_of_use_table)
-        //     .values(terms_of_use)
-        //     .execute(&self.conn)
-        //     .map_err(|e| {
-        //         if AgreementOperationImpl::check_if_unique_violation(&e) {
-        //             tracing::error!(
-        //                 "id ({}) has already agreed terms of use(version: {}): {}",
-        //                 id,
-        //                 version,
-        //                 e
-        //             );
-        //             return (
-        //                 StatusCode::BAD_REQUEST,
-        //                 Json(ApiError {
-        //                     code: AlreadyAgreedTermsOfUse as u32,
-        //                 }),
-        //             );
-        //         }
-        //         tracing::error!(
-        //             "failed to insert terms_of_use (id: {}, version: {}): {}",
-        //             id,
-        //             version,
-        //             e
-        //         );
-        //         unexpected_err_resp()
-        //     })?;
-        // Ok(())
-        todo!()
+        let terms_of_use_model = terms_of_use::ActiveModel {
+            user_account_id: Set(account_id),
+            ver: Set(version),
+            email_address: Set(email_address.to_string()),
+            agreed_at: Set(*agreed_at),
+        };
+        let _ = terms_of_use_model.insert(&self.pool).await.map_err(|e| {
+            tracing::error!("failed to insert terms of use agreement (account id: {}, email address: {}, version: {}): {}", 
+            account_id, email_address, version, e);
+            unexpected_err_resp()
+        })?;
+        Ok(())
     }
 }
 
@@ -155,18 +176,19 @@ struct Account {
 #[cfg(test)]
 mod tests {
     use axum::async_trait;
-    use axum::Json;
     use chrono::TimeZone;
-    use common::{ApiError, ErrResp};
+    use common::ErrResp;
     use hyper::StatusCode;
 
     use crate::{err::Code::AlreadyAgreedTermsOfUse, util::JAPANESE_TIME_ZONE};
 
     use super::Account;
+    use super::AgreedDateTime;
     use super::{handle_agreement_req, AgreementOperation};
 
     struct AgreementOperationMock<'a> {
         already_agreed_terms_of_use: bool,
+        agreed_at_before: &'a chrono::DateTime<chrono::FixedOffset>,
         account_id: i32,
         version: i32,
         email_address: &'a str,
@@ -176,6 +198,7 @@ mod tests {
     impl<'a> AgreementOperationMock<'a> {
         fn new(
             already_agreed_terms_of_use: bool,
+            agreed_at_before: &'a chrono::DateTime<chrono::FixedOffset>,
             account_id: i32,
             version: i32,
             email_address: &'a str,
@@ -183,6 +206,7 @@ mod tests {
         ) -> Self {
             Self {
                 already_agreed_terms_of_use,
+                agreed_at_before,
                 account_id,
                 version,
                 email_address,
@@ -201,6 +225,19 @@ mod tests {
             Ok(Some(account))
         }
 
+        async fn check_if_already_agreed(
+            &self,
+            account_id: i32,
+            version: i32,
+        ) -> Result<Option<AgreedDateTime>, ErrResp> {
+            assert_eq!(self.account_id, account_id);
+            assert_eq!(self.version, version);
+            if self.already_agreed_terms_of_use {
+                return Ok(Some(*self.agreed_at_before));
+            }
+            Ok(None)
+        }
+
         async fn agree_terms_of_use(
             &self,
             account_id: i32,
@@ -212,14 +249,6 @@ mod tests {
             assert_eq!(self.version, version);
             assert_eq!(self.email_address, email_address);
             assert_eq!(self.agreed_at, agreed_at);
-            if self.already_agreed_terms_of_use {
-                return Err((
-                    StatusCode::BAD_REQUEST,
-                    Json(ApiError {
-                        code: AlreadyAgreedTermsOfUse as u32,
-                    }),
-                ));
-            }
             Ok(())
         }
     }
@@ -233,7 +262,15 @@ mod tests {
             .ymd(2021, 11, 7)
             .and_hms(11, 00, 40)
             .with_timezone(&JAPANESE_TIME_ZONE.to_owned());
-        let op = AgreementOperationMock::new(false, id, version, email_address, &agreed_at);
+        let agreed_at_before = agreed_at - chrono::Duration::days(1);
+        let op = AgreementOperationMock::new(
+            false,
+            &agreed_at_before,
+            id,
+            version,
+            email_address,
+            &agreed_at,
+        );
 
         let result = handle_agreement_req(id, version, &agreed_at, op)
             .await
@@ -251,7 +288,15 @@ mod tests {
             .ymd(2021, 11, 7)
             .and_hms(11, 00, 40)
             .with_timezone(&JAPANESE_TIME_ZONE.to_owned());
-        let op = AgreementOperationMock::new(true, id, version, email_address, &agreed_at);
+        let agreed_at_before = agreed_at - chrono::Duration::days(1);
+        let op = AgreementOperationMock::new(
+            true,
+            &agreed_at_before,
+            id,
+            version,
+            email_address,
+            &agreed_at,
+        );
 
         let result = handle_agreement_req(id, version, &agreed_at, op)
             .await
