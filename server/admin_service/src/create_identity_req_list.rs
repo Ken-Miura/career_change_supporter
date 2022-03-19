7cb8d57867dd65178c9d69fe0b4aff5a6a582d6d
kmiura <ken.miura1102@gmail.com>
2022-03-19 17:22:40 +0000

    Change name

--- server/admin_service/src/create_identity_req_list.rs
+++ server/admin_service/src/create_identity_req_list.rs
@@ -11,7 +11,7 @@ use crate::util::{session::Admin, validate_page_size, Pagination};
 pub(crate) async fn get_create_identity_requests(
     Admin { account_id: _ }: Admin, // 認証されていることを保証するために必須
     pagination: Query<Pagination>,
-) -> RespResult<Vec<CreateIdentityReqEntry>> {
+) -> RespResult<Vec<CreateIdentityReqItem>> {
     let pagination = pagination.0;
     let _ = validate_page_size(pagination.per_page)?;
     tracing::debug!("{}, {}", pagination.page, pagination.per_page);
@@ -19,7 +19,7 @@ pub(crate) async fn get_create_identity_requests(
 }
 
 #[derive(Serialize)]
-pub(crate) struct CreateIdentityReqEntry {
+pub(crate) struct CreateIdentityReqItem {
     id: i32,
     reqested_at: DateTime<Utc>,
     name: String,
