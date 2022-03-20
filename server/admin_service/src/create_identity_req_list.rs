5add275f07675f44d3782200bc592b6df360f6ff
kmiura <ken.miura1102@gmail.com>
2022-03-20 15:44:02 +0000

    Add tests

--- server/admin_service/src/create_identity_req_list.rs
+++ server/admin_service/src/create_identity_req_list.rs
@@ -28,7 +28,7 @@ pub(crate) async fn get_create_identity_requests(
     get_create_identity_request_items(pagination, op).await
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug, Clone, PartialEq)]
 pub(crate) struct CreateIdentityReqItem {
     account_id: i64,
     reqested_at: DateTime<FixedOffset>,
@@ -78,5 +78,151 @@ impl CreateIdentityRequestItemsOperation for CreateIdentityRequestItemsOperation
                 name: model.last_name.clone() + " " + model.first_name.as_str(),
             })
             .collect::<Vec<CreateIdentityReqItem>>())
+    }
+}
+
+// ロジックはDBへのクエリのみでテストは必要ないかもしれないが、
+// DBへのクエリ（ORMのAPI）への期待する動作を記すためにテストを記載しておく。
+#[cfg(test)]
+mod tests {
+    use axum::async_trait;
+    use axum::http::StatusCode;
+    use chrono::{Duration, TimeZone, Utc};
+    use common::{ErrResp, JAPANESE_TIME_ZONE};
+
+    use crate::util::Pagination;
+
+    use super::{
+        get_create_identity_request_items, CreateIdentityReqItem,
+        CreateIdentityRequestItemsOperation,
+    };
+
+    struct CreateIdentityRequestItemsOperationMock {
+        items: Vec<CreateIdentityReqItem>,
+    }
+
+    #[async_trait]
+    impl CreateIdentityRequestItemsOperation for CreateIdentityRequestItemsOperationMock {
+        async fn get_items(
+            &self,
+            page: usize,
+            page_size: usize,
+        ) -> Result<Vec<CreateIdentityReqItem>, ErrResp> {
+            let items = self.items.clone();
+            let length = items.len();
+            let start = page * page_size;
+            if start >= length {
+                return Ok(vec![]);
+            }
+            let end = if start + page_size > length {
+                length
+            } else {
+                start + page_size
+            };
+            let items = items.get(start..end).expect("failed to get value");
+            Ok(items.to_vec())
+        }
+    }
+
+    #[tokio::test]
+    async fn get_create_identity_request_items_success1() {
+        let items = create_dummy_items();
+        let op_mock = CreateIdentityRequestItemsOperationMock {
+            items: items.clone(),
+        };
+        let pagination = Pagination {
+            page: 0,
+            per_page: items.len(),
+        };
+
+        let result = get_create_identity_request_items(pagination, op_mock).await;
+
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(items, resp.1 .0);
+    }
+
+    #[tokio::test]
+    async fn get_create_identity_request_items_success2() {
+        let items = create_dummy_items();
+        let op_mock = CreateIdentityRequestItemsOperationMock {
+            items: items.clone(),
+        };
+        let pagination = Pagination {
+            page: 0,
+            per_page: items.len() - 1,
+        };
+
+        let result = get_create_identity_request_items(pagination, op_mock).await;
+
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(items.get(0..2).expect("failed to get value"), resp.1 .0);
+    }
+
+    #[tokio::test]
+    async fn get_create_identity_request_items_success3() {
+        let items = create_dummy_items();
+        let op_mock = CreateIdentityRequestItemsOperationMock {
+            items: items.clone(),
+        };
+        let pagination = Pagination {
+            page: 1,
+            per_page: items.len() - 1,
+        };
+
+        let result = get_create_identity_request_items(pagination, op_mock).await;
+
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        let item = items.get(2).expect("failed to get value");
+        assert_eq!(vec![item.clone()], resp.1 .0);
+    }
+
+    #[tokio::test]
+    async fn get_create_identity_request_items_success4() {
+        let items = create_dummy_items();
+        let op_mock = CreateIdentityRequestItemsOperationMock {
+            items: items.clone(),
+        };
+        let pagination = Pagination {
+            page: 2,
+            per_page: items.len() - 1,
+        };
+
+        let result = get_create_identity_request_items(pagination, op_mock).await;
+
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(Vec::<CreateIdentityReqItem>::with_capacity(0), resp.1 .0);
+    }
+
+    fn create_dummy_items() -> Vec<CreateIdentityReqItem> {
+        let mut items = Vec::with_capacity(3);
+        let reqested_at_1 = Utc
+            .ymd(2021, 9, 11)
+            .and_hms(15, 30, 45)
+            .with_timezone(&JAPANESE_TIME_ZONE.to_owned());
+        let item1 = CreateIdentityReqItem {
+            account_id: 1,
+            reqested_at: reqested_at_1,
+            name: String::from("山田 太郎"),
+        };
+        items.push(item1);
+        let requested_at_2 = reqested_at_1 + Duration::days(1);
+        let item2 = CreateIdentityReqItem {
+            account_id: 2,
+            reqested_at: requested_at_2,
+            name: String::from("佐藤 次郎"),
+        };
+        items.push(item2);
+        let requested_at_3 = requested_at_2 + Duration::days(1);
+        let item3 = CreateIdentityReqItem {
+            account_id: 3,
+            reqested_at: requested_at_3,
+            name: String::from("田中 三郎"),
+        };
+        items.push(item3);
+        items
     }
 }
