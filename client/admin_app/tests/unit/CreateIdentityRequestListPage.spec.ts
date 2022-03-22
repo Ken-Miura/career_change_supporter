a5e35ee091a8cca915dd715d99b39097d4e8bd0b
kmiura <ken.miura1102@gmail.com>
2022-03-22 15:51:24 +0000

    Add tests

--- client/admin_app/tests/unit/CreateIdentityRequestListPage.spec.ts
+++ client/admin_app/tests/unit/CreateIdentityRequestListPage.spec.ts
@@ -9,6 +9,7 @@ import AlertMessage from '@/components/AlertMessage.vue'
 import { Message } from '@/util/Message'
 import { Code } from '@/util/Error'
 import { ApiError, ApiErrorResp } from '@/util/ApiError'
+import { getNumOfItems, NUM_OF_ITEMS } from '@/util/NumOfItems'
 
 const routerPushMock = jest.fn()
 jest.mock('vue-router', () => ({
@@ -16,6 +17,9 @@ jest.mock('vue-router', () => ({
     push: routerPushMock
   })
 }))
+
+jest.mock('@/util/NumOfItems')
+const getNumOfItemsMock = getNumOfItems as jest.MockedFunction<typeof getNumOfItems>
 
 const waitingRequestDoneMock = ref(false)
 const getCreateIdentityRequestsFuncMock = jest.fn()
@@ -29,6 +33,8 @@ jest.mock('@/util/personalized/create-identity-request-list/useGetCreateIdentity
 describe('CreateIdentityRequestListPage.vue', () => {
   beforeEach(() => {
     routerPushMock.mockClear()
+    getNumOfItemsMock.mockReset()
+    getNumOfItemsMock.mockReturnValue(NUM_OF_ITEMS)
     waitingRequestDoneMock.value = false
     getCreateIdentityRequestsFuncMock.mockReset()
   })
@@ -100,7 +106,7 @@ describe('CreateIdentityRequestListPage.vue', () => {
     expect(routerPushMock).toHaveBeenCalledWith('login')
   })
 
-  it('disabled previous button just after opening page', async () => {
+  it('disables previous button just after opening page', async () => {
     const date = new Date(Date.UTC(2022, 0, 1, 23, 59, 59))
     const item = {
       account_id: 1,
@@ -121,5 +127,107 @@ describe('CreateIdentityRequestListPage.vue', () => {
 
     const button = wrapper.find('[data-test="prev-button"]')
     expect(button.attributes()).toHaveProperty('disabled')
+  })
+
+  it('disables next button if items returned are less than displayable items per page', async () => {
+    const date = new Date(Date.UTC(2022, 0, 1, 23, 59, 59))
+    const item = {
+      account_id: 1,
+      name: '佐藤 次郎',
+      requested_at: date
+    } as CreateIdentityRequestItem
+    const items = [item]
+    const resp = GetCreateIdentityRequestsResp.create(items)
+    getCreateIdentityRequestsFuncMock.mockResolvedValue(resp)
+    getNumOfItemsMock.mockReset()
+    getNumOfItemsMock.mockReturnValue(2)
+    const wrapper = mount(CreateIdentityRequestListPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const button = wrapper.find('[data-test="next-button"]')
+    expect(button.attributes()).toHaveProperty('disabled')
+  })
+
+  it('does not disable next button if items returned are equal to displayable items per page', async () => {
+    const date1Utc = Date.UTC(2022, 0, 1, 23, 59, 59)
+    const date1 = new Date(date1Utc)
+    const item1 = {
+      account_id: 1,
+      name: '佐藤 次郎',
+      requested_at: date1
+    } as CreateIdentityRequestItem
+    // 気にするのは順序のみで、date1Utcよりあとであればなんでもよいので適当の数字を足す。
+    const date2Utc = date1Utc + 60
+    const date2 = new Date(date2Utc)
+    const item2 = {
+      account_id: 1,
+      name: '田中 太郎',
+      requested_at: date2
+    } as CreateIdentityRequestItem
+    const items = [item1, item2]
+    const resp = GetCreateIdentityRequestsResp.create(items)
+    getCreateIdentityRequestsFuncMock.mockResolvedValue(resp)
+    getNumOfItemsMock.mockReset()
+    getNumOfItemsMock.mockReturnValue(2)
+    const wrapper = mount(CreateIdentityRequestListPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const button = wrapper.find('[data-test="next-button"]')
+    expect(button.attributes()).not.toHaveProperty('disabled')
+  })
+
+  // 現在の実装では、取得する際に指定する最大Item数とページに表示する最大Item数が同じため（最大50件サーバから取得し、最大50件を1ページに表示するため）
+  // このケースは存在しないが、一応テストしておく
+  it('does not disable next button if items returned are greater than displayable items per page', async () => {
+    const date1Utc = Date.UTC(2022, 0, 1, 23, 59, 59)
+    const date1 = new Date(date1Utc)
+    const item1 = {
+      account_id: 1,
+      name: '佐藤 次郎',
+      requested_at: date1
+    } as CreateIdentityRequestItem
+    // 気にするのは順序のみで、date1Utcよりあとであればなんでもよいので適当の数字を足す。
+    const date2Utc = date1Utc + 60
+    const date2 = new Date(date2Utc)
+    const item2 = {
+      account_id: 1,
+      name: '田中 太郎',
+      requested_at: date2
+    } as CreateIdentityRequestItem
+    const date3Utc = date2Utc + 60
+    const date3 = new Date(date3Utc)
+    const item3 = {
+      account_id: 3,
+      name: '鈴木 圭一',
+      requested_at: date3
+    } as CreateIdentityRequestItem
+    const items = [item1, item2, item3]
+    const resp = GetCreateIdentityRequestsResp.create(items)
+    getCreateIdentityRequestsFuncMock.mockResolvedValue(resp)
+    getNumOfItemsMock.mockReset()
+    getNumOfItemsMock.mockReturnValue(2)
+    const wrapper = mount(CreateIdentityRequestListPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const button = wrapper.find('[data-test="next-button"]')
+    expect(button.attributes()).not.toHaveProperty('disabled')
   })
 })
