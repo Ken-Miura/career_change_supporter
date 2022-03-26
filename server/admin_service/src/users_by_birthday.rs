0f5055af5058c2b2ae758d37edbf51cecaf441ad
kmiura <ken.miura1102@gmail.com>
2022-03-26 09:35:59 +0000

    Add test

--- server/admin_service/src/users_by_birthday.rs
+++ server/admin_service/src/users_by_birthday.rs
@@ -111,5 +111,187 @@ impl UsersByBirthdayOperation for UsersByBirthdayOperationImpl {
     }
 }
 
+// ロジックはDBへのクエリのみでテストは必要ないかもしれないが、
+// DBへのクエリ（ORMのAPI）への期待する動作を記すためにテストを記載しておく。
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::err::Code::IllegalDate;
+    use axum::async_trait;
+    use axum::http::StatusCode;
+    use chrono::{Datelike, NaiveDate, TimeZone, Utc};
+    use common::ErrResp;
+
+    use super::{get_users_by_birthday_internal, User, UsersByBirthdayOperation};
+
+    struct UsersByBirthdayOperationMock {
+        users: Vec<User>,
+    }
+
+    #[async_trait]
+    impl UsersByBirthdayOperation for UsersByBirthdayOperationMock {
+        async fn get_users_by_birthday(&self, birthday: NaiveDate) -> Result<Vec<User>, ErrResp> {
+            let mut users = Vec::with_capacity(self.users.len());
+            for user in &self.users {
+                if user.date_of_birth == birthday {
+                    users.push(user.clone());
+                }
+            }
+            Ok(users)
+        }
+    }
+
+    #[tokio::test]
+    async fn get_users_by_birthday_internal_success_one_user_found() {
+        let date_of_birth = Utc.ymd(1991, 4, 1).naive_local();
+        let user = create_dummy_user1(date_of_birth);
+        let op_mock = UsersByBirthdayOperationMock {
+            users: vec![user.clone()],
+        };
+
+        let result = get_users_by_birthday_internal(
+            date_of_birth.year(),
+            date_of_birth.month(),
+            date_of_birth.day(),
+            op_mock,
+        )
+        .await;
+
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(vec![user], resp.1 .0);
+    }
+
+    #[tokio::test]
+    async fn get_users_by_birthday_internal_success_no_user_found() {
+        let date_of_birth = Utc.ymd(1991, 4, 1).naive_local();
+        let user = create_dummy_user1(date_of_birth);
+        let op_mock = UsersByBirthdayOperationMock {
+            users: vec![user.clone()],
+        };
+
+        let result = get_users_by_birthday_internal(
+            date_of_birth.year() + 1,
+            date_of_birth.month(),
+            date_of_birth.day(),
+            op_mock,
+        )
+        .await;
+
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(Vec::<User>::with_capacity(0), resp.1 .0);
+    }
+
+    #[tokio::test]
+    async fn get_users_by_birthday_internal_success_one_user_found_one_user_not_found() {
+        let date_of_birth1 = Utc.ymd(1991, 4, 1).naive_local();
+        let user1 = create_dummy_user1(date_of_birth1);
+        let date_of_birth2 = Utc.ymd(1991, date_of_birth1.month() + 1, 1).naive_local();
+        let user2 = create_dummy_user2(date_of_birth2);
+        let op_mock = UsersByBirthdayOperationMock {
+            users: vec![user1.clone(), user2],
+        };
+
+        let result = get_users_by_birthday_internal(
+            date_of_birth1.year(),
+            date_of_birth1.month(),
+            date_of_birth1.day(),
+            op_mock,
+        )
+        .await;
+
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(vec![user1], resp.1 .0);
+    }
+
+    #[tokio::test]
+    async fn get_users_by_birthday_internal_success_multiple_users_found() {
+        let date_of_birth1 = Utc.ymd(1991, 4, 1).naive_local();
+        let user1 = create_dummy_user1(date_of_birth1);
+        let date_of_birth2 = Utc.ymd(1991, date_of_birth1.month() + 1, 1).naive_local();
+        let user2 = create_dummy_user2(date_of_birth2);
+        let date_of_birth3 = Utc
+            .ymd(
+                date_of_birth1.year(),
+                date_of_birth1.month(),
+                date_of_birth1.day(),
+            )
+            .naive_local();
+        let user3 = create_dummy_user3(date_of_birth3);
+        let op_mock = UsersByBirthdayOperationMock {
+            users: vec![user1.clone(), user2, user3.clone()],
+        };
+
+        let result = get_users_by_birthday_internal(
+            date_of_birth1.year(),
+            date_of_birth1.month(),
+            date_of_birth1.day(),
+            op_mock,
+        )
+        .await;
+
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(vec![user1, user3], resp.1 .0);
+    }
+
+    #[tokio::test]
+    async fn get_users_by_birthday_internal_fail_illegal_date() {
+        let op_mock = UsersByBirthdayOperationMock { users: vec![] };
+
+        let result = get_users_by_birthday_internal(1990, 1, 0, op_mock).await;
+
+        let err_resp = result.expect_err("failed to get Err");
+        assert_eq!(StatusCode::BAD_REQUEST, err_resp.0);
+        assert_eq!(IllegalDate as u32, err_resp.1 .0.code);
+    }
+
+    fn create_dummy_user1(date_of_birth: NaiveDate) -> User {
+        User {
+            user_account_id: 341,
+            last_name: String::from("田中"),
+            first_name: String::from("太郎"),
+            last_name_furigana: String::from("タナカ"),
+            first_name_furigana: String::from("タロウ"),
+            date_of_birth,
+            prefecture: String::from("東京都"),
+            city: String::from("町田市"),
+            address_line1: String::from("森の里２−２２−２"),
+            address_line2: None,
+            telephone_number: String::from("09012345678"),
+        }
+    }
+
+    fn create_dummy_user2(date_of_birth: NaiveDate) -> User {
+        User {
+            user_account_id: 567,
+            last_name: String::from("佐藤"),
+            first_name: String::from("次郎"),
+            last_name_furigana: String::from("サトウ"),
+            first_name_furigana: String::from("次郎"),
+            date_of_birth,
+            prefecture: String::from("沖縄県"),
+            city: String::from("那覇市"),
+            address_line1: String::from("泉崎１ー１ー１"),
+            address_line2: None,
+            telephone_number: String::from("08012345678"),
+        }
+    }
+
+    fn create_dummy_user3(date_of_birth: NaiveDate) -> User {
+        User {
+            user_account_id: 5767,
+            last_name: String::from("田中"),
+            first_name: String::from("三郎"),
+            last_name_furigana: String::from("タナカ"),
+            first_name_furigana: String::from("サブロウ"),
+            date_of_birth,
+            prefecture: String::from("北海道"),
+            city: String::from("札幌市"),
+            address_line1: String::from("中央区北1条西2丁目"),
+            address_line2: None,
+            telephone_number: String::from("08087654321"),
+        }
+    }
+}
