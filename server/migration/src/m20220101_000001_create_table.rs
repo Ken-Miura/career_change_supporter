6bc2efce0e434910801a26a1c3fdb377f971edca
kmiura <ken.miura1102@gmail.com>
2022-03-30 16:37:04 +0000

    Change migration

--- server/migration/src/m20220101_000001_create_table.rs
+++ server/migration/src/m20220101_000001_create_table.rs
@@ -150,7 +150,7 @@ impl MigrationTrait for Migration {
             .map(|_| ())?;
 
         let _ = conn
-            /* user_account一つに対して、identity_infoは0もしくは1の関係とする。従って、user_account_idを外部キーかつ主キーとする */
+            /* user_account一つに対して、identityは0もしくは1の関係とする。従って、user_account_idを外部キーかつ主キーとする */
             /* prefecture => 都道府県の最大文字数は4文字（神奈川県、鹿児島県、和歌山県） */
             /* city => 市区町村の最大文字数は6文字。しかし、市区町村は頻繁に名前が変更される可能性があるので長さに余裕をもたせる */
             /*
@@ -158,7 +158,7 @@ impl MigrationTrait for Migration {
              * 電話番号の最大桁数は15桁、国内向けのみのサービスを考えているので最大13桁とする。 
              * 参考: https://www.accumu.jp/vol22-23/%E3%82%84%E3%81%95%E3%81%97%E3%81%9D%E3%81%86%E3%81%AB%E8%A6%8B%E3%81%88%E3%82%8B%E9%9B%BB%E8%A9%B1%E7%95%AA%E5%8F%B7%E3%81%AE%E9%9B%A3%E3%81%97%E3%81%95%20%E7%B7%8F%E5%8B%99%E5%A4%A7%E8%87%A3%E8%B3%9E%E3%82%92%E5%8F%97%E8%B3%9E%E3%81%97%E3%81%A6.html#:~:text=%E6%97%A5%E6%9C%AC%E3%81%AE%E5%A0%B4%E5%90%88%EF%BC%8C%E5%9B%BD%E7%95%AA%E5%8F%B7,%E3%81%AF%E9%99%A4%E3%81%84%E3%81%A6%E6%95%B0%E3%81%88%E3%81%BE%E3%81%99%E3%80%82
              */
-            .execute(sql.stmt(r"CREATE TABLE ccs_schema.identity_info (
+            .execute(sql.stmt(r"CREATE TABLE ccs_schema.identity (
               user_account_id BIGINT PRIMARY KEY REFERENCES ccs_schema.user_account(user_account_id) ON DELETE CASCADE ON UPDATE RESTRICT,
               last_name VARCHAR (64) NOT NULL,
               first_name VARCHAR (64) NOT NULL,
@@ -176,31 +176,31 @@ impl MigrationTrait for Migration {
         let _ = conn
             /* 身分情報は、管理者 (admin_app) が提出されたエビデンスを確認し、レコードを挿入、更新する。従って、ユーザー (user_app) には挿入、更新権限は持たせない。*/
             /* アカウント削除はユーザー自身が行う。そのため削除権限はユーザー (user_app) に付与する */
-            .execute(sql.stmt(r"GRANT SELECT, DELETE ON ccs_schema.identity_info To user_app;"))
+            .execute(sql.stmt(r"GRANT SELECT, DELETE ON ccs_schema.identity To user_app;"))
             .await
             .map(|_| ())?;
         let _ = conn
-            .execute(
-                sql.stmt(r"GRANT SELECT, INSERT, UPDATE ON ccs_schema.identity_info To admin_app;"),
-            )
+            .execute(sql.stmt(r"GRANT SELECT, INSERT, UPDATE ON ccs_schema.identity To admin_app;"))
             .await
             .map(|_| ())?;
         let _ = conn
-            /* 
+            /*
              * 管理者が本人確認の際に、既に登録済のユーザー情報があるかどうか確認する必要がある。
              * 既に登録済のユーザー情報かどうかを調べるため、生年月日が一致するユーザー情報を抽出 (*) し、その他の項目が一致するか確認し、ユーザー情報が既に登録済か確認する。
              * (*) 生年月日がユーザー情報の中で最も一致率が低いと考えられるため、生年月日を利用する
              *（住所を示す複数カラムにインデックスを張る選択肢もあるが、住所は変更され、インデックス張り直しの可能性があるため避ける）
              */
-            .execute(sql.stmt(r"CREATE INDEX identity_info_date_of_birth_idx ON ccs_schema.identity_info (date_of_birth);"))
+            .execute(sql.stmt(
+                r"CREATE INDEX identity_date_of_birth_idx ON ccs_schema.identity (date_of_birth);",
+            ))
             .await
             .map(|_| ())?;
 
         let _ = conn
             .execute(
                 /* annual_income_in_man_yen => 万円単位での年収 */
-                sql.stmt(r"CREATE TABLE ccs_schema.career_info (
-                    career_info_id BIGSERIAL PRIMARY KEY,
+                sql.stmt(r"CREATE TABLE ccs_schema.career (
+                    career_id BIGSERIAL PRIMARY KEY,
                     user_account_id BIGINT NOT NULL REFERENCES ccs_schema.user_account(user_account_id) ON DELETE CASCADE ON UPDATE RESTRICT,
                     company_name VARCHAR (256) NOT NULL,
                     department_name VARCHAR (256),
@@ -222,20 +222,18 @@ impl MigrationTrait for Migration {
             .execute(
                 /* 職務経歴は、管理者 (admin_app) が提出されたエビデンスを確認し、レコードを挿入、更新する。従って、ユーザー (user_app) には挿入、更新権限は持たせない。*/
                 /* アカウント削除はユーザー自身が行う。そのため削除権限はユーザー (user_app) に付与する */
-                sql.stmt(r"GRANT SELECT, DELETE ON ccs_schema.career_info To user_app;"),
+                sql.stmt(r"GRANT SELECT, DELETE ON ccs_schema.career To user_app;"),
             )
+            .await
+            .map(|_| ())?;
+        let _ = conn
+            .execute(sql.stmt(r"GRANT SELECT, INSERT, UPDATE ON ccs_schema.career To admin_app;"))
             .await
             .map(|_| ())?;
         let _ = conn
             .execute(
-                sql.stmt(r"GRANT SELECT, INSERT, UPDATE ON ccs_schema.career_info To admin_app;"),
+                sql.stmt(r"GRANT USAGE ON SEQUENCE ccs_schema.career_career_id_seq TO admin_app;"),
             )
-            .await
-            .map(|_| ())?;
-        let _ = conn
-            .execute(sql.stmt(
-                r"GRANT USAGE ON SEQUENCE ccs_schema.career_info_career_info_id_seq TO admin_app;",
-            ))
             .await
             .map(|_| ())?;
 
@@ -276,7 +274,7 @@ impl MigrationTrait for Migration {
 
         let _ = conn
             /*
-             * user_account一つに対して、create_identity_info_req (本人確認依頼 (新規)) は0もしくは1の関係とする。従って、user_account_idをPRIMARY KEYに指定する
+             * user_account一つに対して、create_identity_req (本人確認依頼 (新規)) は0もしくは1の関係とする。従って、user_account_idをPRIMARY KEYに指定する
              * 画像ファイルの実体は、データベース外に保存している。user_account_idを外部キーにすると、user_accountの操作時に同時にこちらのテーブルのレコードも操作されて、
              * 画像の実体との紐づけが知らないうちに解除される可能性がある。そのため、user_account_idは外部キーとしない
              */
@@ -286,7 +284,7 @@ impl MigrationTrait for Migration {
              * UNIQUEにしたときのNULLの扱いがデータベースごとに異なる可能性がある。従って、その点も考慮し、NULL利用があるカラムにUNIQUE付与を避ける
              */
             .execute(sql.stmt(
-                r"CREATE TABLE ccs_schema.create_identity_info_req (
+                r"CREATE TABLE ccs_schema.create_identity_req (
                   user_account_id BIGINT PRIMARY KEY,
                   last_name VARCHAR (64) NOT NULL,
                   first_name VARCHAR (64) NOT NULL,
@@ -307,28 +305,108 @@ impl MigrationTrait for Migration {
             .map(|_| ())?;
         let _ = conn
             .execute(
-                sql.stmt(
-                    r"GRANT SELECT, INSERT ON ccs_schema.create_identity_info_req To user_app;",
-                ),
+                sql.stmt(r"GRANT SELECT, INSERT ON ccs_schema.create_identity_req To user_app;"),
             )
             .await
             .map(|_| ())?;
-        let _ =
-            conn.execute(sql.stmt(
-                r"GRANT SELECT, DELETE ON ccs_schema.create_identity_info_req To admin_app;",
-            ))
+        let _ = conn
+            .execute(
+                sql.stmt(r"GRANT SELECT, DELETE ON ccs_schema.create_identity_req To admin_app;"),
+            )
             .await
             .map(|_| ())?;
         let _ = conn
             .execute(sql.stmt(
-                r"CREATE INDEX create_identity_info_req_requested_at_idx ON ccs_schema.create_identity_info_req (requested_at);",
+                r"CREATE INDEX create_identity_req_requested_at_idx ON ccs_schema.create_identity_req (requested_at);",
             ))
             .await
             .map(|_| ())?;
 
         let _ = conn
             /*
-             * user_account一つに対して、update_identity_info_req (本人確認依頼 (更新)) は0もしくは1の関係とする。従って、user_account_idをPRIMARY KEYに指定する
+             * user_accountのuser_account_idはBIGSERIALなので重複する心配はない。そのため、
+             * approved_create_identity_req (本人確認依頼 (新規) の承認) のPRIMARY KEYとしてuser_account_idを利用する
+             */
+            /*
+             * user_account_idを外部キーにすると、user_accountの操作時に同時にこちらのテーブルのレコードも操作されて、
+             * 管理者の把握しないうちに承認した記録が消去される可能性がある。そのため、user_account_idは外部キーとしない
+             */
+            .execute(sql.stmt(
+                r"CREATE TABLE ccs_schema.approved_create_identity_req (
+                  user_account_id BIGINT PRIMARY KEY,
+                  last_name VARCHAR (64) NOT NULL,
+                  first_name VARCHAR (64) NOT NULL,
+                  last_name_furigana VARCHAR (64) NOT NULL,
+                  first_name_furigana VARCHAR (64) NOT NULL,
+                  date_of_birth DATE NOT NULL,
+                  prefecture VARCHAR (4) NOT NULL,
+                  city VARCHAR (32) NOT NULL,
+                  address_line1 VARCHAR (128) NOT NULL,
+                  address_line2 VARCHAR (128),
+                  telephone_number VARCHAR (13) NOT NULL,
+                  image1_file_name_without_ext ccs_schema.uuid_simple_form NOT NULL,
+                  image2_file_name_without_ext ccs_schema.uuid_simple_form,
+                  approved_at TIMESTAMP WITH TIME ZONE NOT NULL
+                );",
+            ))
+            .await
+            .map(|_| ())?;
+        let _ = conn
+            .execute(sql.stmt(
+                r"GRANT SELECT, INSERT ON ccs_schema.approved_create_identity_req To admin_app;",
+            ))
+            .await
+            .map(|_| ())?;
+
+        let _ = conn
+            /*
+             * 複数回拒否の記録が残る可能性があるため、user_accountのuser_account_idをPRIMARY KEYとしては扱わない。
+             */
+            /*
+             * user_account_idを外部キーにすると、user_accountの操作時に同時にこちらのテーブルのレコードも操作されて、
+             * 管理者の把握しないうちに拒否した記録が消去される可能性がある。そのため、user_account_idは外部キーとしない
+             */
+            /*
+             * 拒否した場合、アップロードされた画像は削除するため、image1_file_name_without_ext, image2_file_name_without_extは保持しない。
+             */
+            /*
+             * PRIMARY KEYはSEQUENCE名にしたときに識別子の63文字制限に引っかからないように命名する（rjd_cre_identity_id）
+             */
+            .execute(sql.stmt(
+                r"CREATE TABLE ccs_schema.rejected_create_identity_req (
+                  rjd_cre_identity_id BIGSERIAL PRIMARY KEY,
+                  user_account_id BIGINT NOT NULL,
+                  last_name VARCHAR (64) NOT NULL,
+                  first_name VARCHAR (64) NOT NULL,
+                  last_name_furigana VARCHAR (64) NOT NULL,
+                  first_name_furigana VARCHAR (64) NOT NULL,
+                  date_of_birth DATE NOT NULL,
+                  prefecture VARCHAR (4) NOT NULL,
+                  city VARCHAR (32) NOT NULL,
+                  address_line1 VARCHAR (128) NOT NULL,
+                  address_line2 VARCHAR (128),
+                  telephone_number VARCHAR (13) NOT NULL,
+                  rejected_at TIMESTAMP WITH TIME ZONE NOT NULL
+                );",
+            ))
+            .await
+            .map(|_| ())?;
+        let _ = conn
+            .execute(sql.stmt(
+                r"GRANT SELECT, INSERT ON ccs_schema.approved_create_identity_req To admin_app;",
+            ))
+            .await
+            .map(|_| ())?;
+        let _ = conn
+            .execute(sql.stmt(
+                r"GRANT USAGE ON SEQUENCE ccs_schema.rejected_create_identity_req_rjd_cre_identity_id_seq TO admin_app;",
+            ))
+            .await
+            .map(|_| ())?;
+
+        let _ = conn
+            /*
+             * user_account一つに対して、update_identity_req (本人確認依頼 (更新)) は0もしくは1の関係とする。従って、user_account_idをPRIMARY KEYに指定する
              * 画像ファイルの実体は、データベース外に保存している。user_account_idを外部キーにすると、user_accountの操作時に同時にこちらのテーブルのレコードも操作されて、
              * 画像の実体との紐づけが知らないうちに解除される可能性がある。そのため、user_account_idは外部キーとしない
              */
@@ -338,7 +416,7 @@ impl MigrationTrait for Migration {
              * UNIQUEにしたときのNULLの扱いがデータベースごとに異なる可能性がある。従って、その点も考慮し、NULL利用があるカラムにUNIQUE付与を避ける
              */
             .execute(sql.stmt(
-                r"CREATE TABLE ccs_schema.update_identity_info_req (
+                r"CREATE TABLE ccs_schema.update_identity_req (
                     user_account_id BIGINT PRIMARY KEY,
                     last_name VARCHAR (64) NOT NULL,
                     first_name VARCHAR (64) NOT NULL,
@@ -359,21 +437,19 @@ impl MigrationTrait for Migration {
             .map(|_| ())?;
         let _ = conn
             .execute(
-                sql.stmt(
-                    r"GRANT SELECT, INSERT ON ccs_schema.update_identity_info_req To user_app;",
-                ),
+                sql.stmt(r"GRANT SELECT, INSERT ON ccs_schema.update_identity_req To user_app;"),
             )
             .await
             .map(|_| ())?;
-        let _ =
-            conn.execute(sql.stmt(
-                r"GRANT SELECT, DELETE ON ccs_schema.update_identity_info_req To admin_app;",
-            ))
+        let _ = conn
+            .execute(
+                sql.stmt(r"GRANT SELECT, DELETE ON ccs_schema.update_identity_req To admin_app;"),
+            )
             .await
             .map(|_| ())?;
         let _ = conn
             .execute(sql.stmt(
-                r"CREATE INDEX update_identity_info_req_requested_at_idx ON ccs_schema.update_identity_info_req (requested_at);",
+                r"CREATE INDEX update_identity_req_requested_at_idx ON ccs_schema.update_identity_req (requested_at);",
             ))
             .await
             .map(|_| ())?;
