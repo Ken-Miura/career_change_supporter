28c84657310d08c557faa68e82137d867fd20dfd
kmiura <ken.miura1102@gmail.com>
2022-04-02 15:35:52 +0000

    Add approval page

--- client/admin_app/src/views/personalized/CreateIdentityRequestApprovalPage.vue
+++ client/admin_app/src/views/personalized/CreateIdentityRequestApprovalPage.vue
@@ -5,7 +5,7 @@
         <h1 class="text-2xl font-bold text-white text-center">就職先・転職先を見極めるためのサイト</h1>
       </router-link>
     </header>
-    <main class="flex justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+    <main class="flex flex-col justify-center items-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
       <h3 class="font-bold text-lg">本人確認依頼（新規）を承認しました。</h3>
       <router-link to="/create-identity-request-list" class="m-4 hover:underline">本人確認依頼（新規）一覧へ</router-link>
     </main>
