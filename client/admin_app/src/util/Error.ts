6d58e891bc87b19f8bd06b47f2b9e349d7acb339
kmiura <ken.miura1102@gmail.com>
2022-04-09 02:07:38 +0000

    Add error

--- client/admin_app/src/util/Error.ts
+++ client/admin_app/src/util/Error.ts
@@ -12,6 +12,10 @@ export namespace Code {
   export const UNEXPECTED_ERR_ADMIN = 30000
   export const EMAIL_OR_PWD_INCORRECT = 30001
   export const UNAUTHORIZED = 30002
+  export const ILLEGAL_PAGE_SIZE = 30003
+  export const NO_CREATE_IDENTITY_REQ_DETAIL_FOUND = 30004
+  export const ILLEGAL_DATE = 30005
+  export const INVALID_FORMAT_REASON = 30006
 }
 
 export function createErrorMessage (code: number): string {
@@ -27,6 +31,14 @@ export function createErrorMessage (code: number): string {
     return `${Message.EMAIL_OR_PWD_INCORRECT_MESSAGE} (${code})`
   } else if (code === Code.UNAUTHORIZED) {
     return `${Message.UNAUTHORIZED_MESSAGE} (${code})`
+  } else if (code === Code.ILLEGAL_PAGE_SIZE) {
+    return `${Message.ILLEGAL_PAGE_SIZE_MESSAGE} (${code})`
+  } else if (code === Code.NO_CREATE_IDENTITY_REQ_DETAIL_FOUND) {
+    return `${Message.NO_CREATE_IDENTITY_REQ_DETAIL_FOUND_MESSAGE} (${code})`
+  } else if (code === Code.ILLEGAL_DATE) {
+    return `${Message.ILLEGAL_DATE_MESSAGE} (${code})`
+  } else if (code === Code.INVALID_FORMAT_REASON) {
+    return `${Message.INVALID_FORMAT_REASON_MESSAGE} (${code})`
   } else {
     throw new Error(`unexpected code: ${code}`)
   }
--- client/admin_app/src/util/Message.ts
+++ client/admin_app/src/util/Message.ts
@@ -1,4 +1,5 @@
 const RETRY_REQUEST = '通信環境を確認し、一定時間後に再度お試し下さい'
+const ASK_ADMIN = '予期せぬエラーが発生しました。表示されている数字を管理者にお伝え下さい'
 
 // classを利用を検討したが、constにできないためnamespaceを選択
 // namespaceは、非推奨ではないため、代替可能な手段ができるまで利用
@@ -11,4 +12,8 @@ export namespace Message {
     export const EMAIL_OR_PWD_INCORRECT_MESSAGE = 'メールアドレス、もしくはパスワードが間違っています'
     export const LOGIN_FAILED = `ログインに失敗しました。${RETRY_REQUEST}`
     export const UNAUTHORIZED_MESSAGE = '認証が必要です'
+    export const ILLEGAL_PAGE_SIZE_MESSAGE = `${ASK_ADMIN}`
+    export const NO_CREATE_IDENTITY_REQ_DETAIL_FOUND_MESSAGE = `${ASK_ADMIN}`
+    export const ILLEGAL_DATE_MESSAGE = `${ASK_ADMIN}`
+    export const INVALID_FORMAT_REASON_MESSAGE = `${ASK_ADMIN}`
 }
