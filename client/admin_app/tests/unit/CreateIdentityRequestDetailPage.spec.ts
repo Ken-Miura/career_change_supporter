08682b43446e1958f0367e0823688568b67a6ff8
kmiura <ken.miura1102@gmail.com>
2022-04-11 12:51:06 +0000

    Add test

--- client/admin_app/tests/unit/CreateIdentityRequestDetailPage.spec.ts
+++ client/admin_app/tests/unit/CreateIdentityRequestDetailPage.spec.ts
@@ -278,4 +278,48 @@ describe('CreateIdentityRequestRejectionDetailPage.vue', () => {
     const resultMessage = alertMessage.text()
     expect(resultMessage).toContain(`${Message.NO_CREATE_IDENTITY_REQ_DETAIL_FOUND_MESSAGE} (${Code.NO_CREATE_IDENTITY_REQ_DETAIL_FOUND})`)
   })
+
+  it(`displays ${Message.ILLEGAL_DATE_MESSAGE} if ${Code.ILLEGAL_DATE} after getUsersByDateOfBirth`, async () => {
+    routeParam = '1'
+    const detail = {
+      last_name: '田中',
+      first_name: '太郎',
+      last_name_furigana: 'タナカ',
+      first_name_furigana: 'タロウ',
+      date_of_birth: {
+        year: 1994,
+        month: 5,
+        day: 21
+      },
+      prefecture: '北海道',
+      city: '札幌市',
+      address_line1: '北区２−１',
+      address_line2: null,
+      telephone_number: '09012345678',
+      image1_file_name_without_ext: 'c9df65633f6fa4ff2960000535156eda',
+      image2_file_name_without_ext: null,
+      requested_at: new Date(Date.UTC(2022, 4, 10, 16, 38, 43))
+    }
+    const resp1 = GetCreateIdentityRequestDetailResp.create(detail)
+    getCreateIdentityRequestDetailFuncMock.mockResolvedValue(resp1)
+    const apiErrResp = ApiErrorResp.create(400, ApiError.create(Code.ILLEGAL_DATE))
+    getUsersByDateOfBirthFuncMock.mockResolvedValue(apiErrResp)
+    const wrapper = mount(CreateIdentityRequestDetailPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    expect(routerPushMock).toHaveBeenCalledTimes(0)
+    const alertMessages = wrapper.findAllComponents(AlertMessage)
+    expect(alertMessages.length).toBe(1)
+    const alertMessage = alertMessages[0]
+    const classes = alertMessage.classes()
+    expect(classes).not.toContain('hidden')
+    const resultMessage = alertMessage.text()
+    expect(resultMessage).toContain(`${Message.ILLEGAL_DATE_MESSAGE} (${Code.ILLEGAL_DATE})`)
+  })
 })
