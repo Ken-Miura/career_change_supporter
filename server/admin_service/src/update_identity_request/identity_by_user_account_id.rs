195b505bd8218c1269f2ab300392068fb0028eb7
kmiura <ken.miura1102@gmail.com>
2022-04-16 15:16:18 +0000

    Add tests

--- server/admin_service/src/update_identity_request/identity_by_user_account_id.rs
+++ server/admin_service/src/update_identity_request/identity_by_user_account_id.rs
@@ -102,4 +102,98 @@ impl IdentityOperationImpl {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use axum::async_trait;
+    use axum::http::StatusCode;
+    use common::{
+        util::{Identity, Ymd},
+        ErrResp,
+    };
+
+    use crate::err::Code;
+
+    use super::{get_identity_by_user_account_id_internal, IdentityOperation};
+
+    struct IdentityOperationMock {
+        user_account_id: i64,
+        identity: Identity,
+    }
+
+    #[async_trait]
+    impl IdentityOperation for IdentityOperationMock {
+        async fn get_identity_by_user_account_id(
+            &self,
+            user_account_id: i64,
+        ) -> Result<Option<Identity>, ErrResp> {
+            if self.user_account_id != user_account_id {
+                return Ok(None);
+            }
+            Ok(Some(self.identity.clone()))
+        }
+    }
+
+    #[tokio::test]
+
+    async fn get_identity_by_user_account_id_internal_success() {
+        let user_account_id = 64431;
+        let identity = Identity {
+            last_name: String::from("田中"),
+            first_name: String::from("太郎"),
+            last_name_furigana: String::from("タナカ"),
+            first_name_furigana: String::from("タロウ"),
+            date_of_birth: Ymd {
+                year: 1978,
+                month: 11,
+                day: 23,
+            },
+            prefecture: String::from("和歌山県"),
+            city: String::from("和歌山市"),
+            address_line1: String::from("小松原通１−１"),
+            address_line2: None,
+            telephone_number: String::from("08043218765"),
+        };
+        let op_mock = IdentityOperationMock {
+            user_account_id,
+            identity: identity.clone(),
+        };
+
+        let result = get_identity_by_user_account_id_internal(user_account_id, op_mock).await;
+
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(identity, resp.1 .0);
+    }
+
+    #[tokio::test]
+
+    async fn get_identity_by_user_account_id_internal_fail_no_identity_found() {
+        let user_account_id = 64431;
+        let identity = Identity {
+            last_name: String::from("田中"),
+            first_name: String::from("太郎"),
+            last_name_furigana: String::from("タナカ"),
+            first_name_furigana: String::from("タロウ"),
+            date_of_birth: Ymd {
+                year: 1978,
+                month: 11,
+                day: 23,
+            },
+            prefecture: String::from("和歌山県"),
+            city: String::from("和歌山市"),
+            address_line1: String::from("小松原通１−１"),
+            address_line2: None,
+            telephone_number: String::from("08043218765"),
+        };
+        let op_mock = IdentityOperationMock {
+            user_account_id,
+            identity: identity.clone(),
+        };
+        let dummy_id = user_account_id + 451;
+
+        let result = get_identity_by_user_account_id_internal(dummy_id, op_mock).await;
+
+        let resp = result.expect_err("failed to get Err");
+        assert_eq!(StatusCode::BAD_REQUEST, resp.0);
+        assert_eq!(Code::NoIdentityFound as u32, resp.1 .0.code);
+    }
+}
