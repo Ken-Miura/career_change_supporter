713d2bd883d187a2d8ac0c9e29b8dd9301281a0b
kmiura <ken.miura1102@gmail.com>
2022-04-27 15:53:38 +0000

    Add description

--- client/user_app/src/views/personalized/IdentityPage.vue
+++ client/user_app/src/views/personalized/IdentityPage.vue
@@ -80,7 +80,7 @@
               <input v-bind:value="form.addressLine2" v-on:input="setAddressLine2" type="text" minlength="0" maxlength="128" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
             </div>
             <div data-test="tel-div" class="mt-4 text-2xl justify-self-start col-span-6 pt-3">
-              電話番号
+              電話番号（ハイフンを含めず、半角数字のみで入力）
             </div>
             <div data-test="tel-input-div" class="mt-2 w-full justify-self-start col-span-6 pt-3 pl-2 rounded bg-gray-200">
               <input v-bind:value="form.telephoneNumber" v-on:input="setTelephoneNumber" type="text" inputmode="tel" pattern="[0-9]{10,13}" title="半角数字のみで10桁以上13桁以下でご入力下さい。" required minlength="10" maxlength="13" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
