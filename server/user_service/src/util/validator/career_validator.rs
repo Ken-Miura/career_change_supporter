e3764706e30fe98f1cfc8bbdfd806c692cc8fbac
kmiura <ken.miura1102@gmail.com>
2022-04-27 16:23:17 +0000

    Update axum and tower-http

--- server/user_service/src/util/validator/career_validator.rs
+++ server/user_service/src/util/validator/career_validator.rs
@@ -32,6 +32,19 @@ pub(crate) fn validate_career(career: &Career) -> Result<(), CareerValidationErr
     Ok(())
 }
 
+// NOTE:
+// - 英単語の区切りに空白が許可されているので、空白のチェックはしない
+// - 脆弱性の作り込みをさけるため、半角記号は許可しない。記号が必要な場合、全角を用いてもらう
+// 補足：
+//   日本の会社名は、仕様としていくつかの記号が許可されている（※1）
+//   今後サービスを改善し、半角記号を利用可能にする場合、アプリのみでなく、ORMとDBを含めた自動の結合テストを必ず用意する。
+//   そして用意されたテストでは、SQLインジェクションが発生しないことを必ずテストする（※2）
+//   （'（アポストロフィー）が、会社名の仕様として許可されているので、特にそれが問題ないことは確認する）
+// （※1）https://vs-group.jp/tax/startup/48check/10check/
+// （※2）ORMがアポストロフィー含め、エスケープが必要な文字をすべてエスケープしていること、
+//        DBがORMの実装しているエスケープ方法（DBやそのDBのバージョンによってエスケープが必要な文字に対するエスケープ方法が異なるケースがある）に対応していること、
+//        は、ぞれぞれORMとDBを含めた結合テストまで実施しないと確認できない。重要なセキュリティインシデントにつながる可能性があるため、
+//        必ず自動化されたテストとして実装し、テストの実施漏れによる問題の検出漏れをさけるようにする必要がある。
 fn validate_company_name(company_name: &str) -> Result<(), CareerValidationError> {
     let company_name_length = company_name.chars().count();
     if !(COMPANY_NAME_MIN_LENGTH..=COMPANY_NAME_MAX_LENGTH).contains(&company_name_length) {
@@ -46,19 +59,6 @@ fn validate_company_name(company_name: &str) -> Result<(), CareerValidationError
             company_name.to_string(),
         ));
     }
-    // NOTE:
-    // - 英単語の区切りに空白が許可されているので、空白のチェックはしない
-    // - 脆弱性の作り込みをさけるため、半角記号は許可しない。記号が必要な場合、全角を用いてもらう
-    // 補足：
-    //   日本の会社名は、仕様としていくつかの記号が許可されている（※1）
-    //   今後サービスを改善し、半角記号を利用可能にする場合、アプリのみでなく、ORMとDBを含めた自動の結合テストを必ず用意する。
-    //   そして用意されたテストでは、SQLインジェクションが発生しないことを必ずテストする（※2）
-    //   （'（アポストロフィー）が、会社名の仕様として許可されているので、特にそれが問題ないことは確認する）
-    // （※1）https://vs-group.jp/tax/startup/48check/10check/
-    // （※2）ORMがアポストロフィー含め、エスケープが必要な文字をすべてエスケープしていること、
-    //        DBがORMの実装しているエスケープ方法（DBやそのDBのバージョンによってエスケープが必要な文字に対するエスケープ方法が異なるケースがある）に対応していること、
-    //        は、ぞれぞれORMとDBを含めた結合テストまで実施しないと確認できない。重要なセキュリティインシデントにつながる可能性があるため、
-    //        必ず自動化されたテストとして実装し、テストの実施漏れによる問題の検出漏れをさけるようにする必要がある。
     if SYMBOL_CHAR_RE.is_match(company_name) {
         return Err(CareerValidationError::IllegalCharInCompanyName(
             company_name.to_string(),
