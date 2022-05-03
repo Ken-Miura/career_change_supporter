d75baf1208eabbc5006a6ecf2658783e974bd74b
kmiura <ken.miura1102@gmail.com>
2022-05-03 15:27:46 +0000

    Add test

--- server/user_service/src/util/validator/career_validator.rs
+++ server/user_service/src/util/validator/career_validator.rs
@@ -3578,4 +3578,36 @@ mod tests {
             let _ = validate_career(&career).expect("failed to get Ok");
         }
     }
+
+    #[test]
+    fn validate_career_returns_err_if_note_includes_space_and_new_line() {
+        let career = Career {
+            company_name: String::from("佐藤商事"),
+            department_name: None,
+            office: None,
+            career_start_date: Ymd {
+                year: 2006,
+                month: 4,
+                day: 1,
+            },
+            career_end_date: None,
+            contract_type: String::from("regular"),
+            profession: None,
+            annual_income_in_man_yen: None,
+            is_manager: true,
+            position_name: None,
+            is_new_graduate: false,
+            note: Some(String::from(
+                "備考は、
+            
+            改行や\n
+             　空白を\r
+             受け入れます。\r\n
+             
+             ",
+            )),
+        };
+
+        let _ = validate_career(&career).expect("failed to get Ok");
+    }
 }
