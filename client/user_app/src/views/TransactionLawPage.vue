75d49ba17e7577d4bd07f1a346f527f4f313d45d
kmiura <ken.miura1102@gmail.com>
2022-05-15 14:05:22 +0000

    Change word

--- client/user_app/src/views/TransactionLawPage.vue
+++ client/user_app/src/views/TransactionLawPage.vue
@@ -27,7 +27,7 @@
         <h2 class="font-bold text-xl">相談料と手数料</h2>
         <p class="ml-2 text-lg">相談料は消費税を含む価格で表示されています。相談料は、相談を受け付けるユーザーの詳細に記載されています。相談を受け付けるユーザーに対して、以下の手数料が課されます。</p>
         <ol class="list-disc ml-8 text-lg">
-          <li>プラットフォーム利用料：相談料の{{ PLATFORM_FEE_IN_PERCENTAGE }}パーセント</li>
+          <li>プラットフォーム利用料：相談が成立した際、相談料の{{ PLATFORM_FEE_IN_PERCENTAGE }}パーセント</li>
           <li>報酬の入金口座への振込手数料：{{ TRANSFER_FEE_IN_YEN }}円</li>
         </ol>
       </div>
