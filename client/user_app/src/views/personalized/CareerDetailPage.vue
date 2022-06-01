661def2c5f55c85190aaed17b23e19adef48dcc5
kmiura <ken.miura1102@gmail.com>
2022-06-01 16:22:41 +0000

    Add career detail page

--- client/user_app/src/views/personalized/CareerDetailPage.vue
+++ client/user_app/src/views/personalized/CareerDetailPage.vue
@@ -13,21 +13,30 @@
       <div v-else>
         <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
           <h3 class="font-bold text-2xl">職務経歴</h3>
-          <!--
-          <div v-if="identity !== null" data-test="identity-set" class="m-4 text-2xl grid grid-cols-3">
-            <div class="mt-2 justify-self-start col-span-1">名前</div><div class="justify-self-start col-span-2">{{ identity.last_name }} {{ identity.first_name }}</div>
-            <div class="mt-2 justify-self-start col-span-1">フリガナ</div><div class="justify-self-start col-span-2">{{ identity.last_name_furigana }} {{ identity.first_name_furigana }}</div>
-            <div class="mt-2 justify-self-start col-span-1">生年月日</div><div class="justify-self-start col-span-2">{{ identity.date_of_birth.year }}年{{ identity.date_of_birth.month }}月{{ identity.date_of_birth.day }}日</div>
-            <div class="mt-2 justify-self-start col-span-3">住所</div>
-            <div class="mt-2 ml-3 justify-self-start col-span-1">都道府県</div><div class="justify-self-start col-span-2">{{ identity.prefecture }}</div>
-            <div class="mt-2 ml-3 justify-self-start col-span-1">市区町村</div><div class="justify-self-start col-span-2">{{ identity.city }}</div>
-            <div class="mt-2 ml-3 justify-self-start col-span-1">番地</div><div class="justify-self-start col-span-2">{{ identity.address_line1 }}</div>
-            <div v-if="identity.address_line2 !== null" class="mt-2 ml-3 justify-self-start col-span-1">建物名・部屋番号</div><div v-if="identity.address_line2 !== null" class="justify-self-start col-span-2">{{ identity.address_line2 }}</div>
-            <div class="mt-2 justify-self-start col-span-1">電話番号</div><div class="justify-self-start col-span-2">{{ identity.telephone_number }}</div>
+          <div v-if="career !== null" data-test="career-set" class="m-4 text-2xl grid grid-cols-3">
+            <div class="mt-2 justify-self-start col-span-1">勤務先名称</div><div class="justify-self-start col-span-2">{{ career.company_name }}</div>
+            <div v-if="career.department_name !== null" class="mt-2 ml-3 justify-self-start col-span-1">部署名</div><div v-if="career.department_name !== null" class="justify-self-start col-span-2">{{ career.department_name }}</div>
+            <div v-if="career.office !== null" class="mt-2 ml-3 justify-self-start col-span-1">勤務地</div><div v-if="career.office !== null" class="justify-self-start col-span-2">{{ career.office }}</div>
+            <div class="mt-2 justify-self-start col-span-1">入社日</div><div class="justify-self-start col-span-2">{{ career.career_start_date.year }}年{{ career.career_start_date.month }}月{{ career.career_start_date.day }}日</div>
+            <div v-if="career.career_end_date !== null" class="mt-2 justify-self-start col-span-1">退社日</div><div v-if="career.career_end_date !== null" class="justify-self-start col-span-2">{{ career.career_end_date.year }}年{{ career.career_end_date.month }}月{{ career.career_end_date.day }}日</div>
+            <div class="mt-2 justify-self-start col-span-1">雇用形態</div>
+            <div v-if="career.contract_type === 'regular'" class="justify-self-start col-span-2">正社員</div>
+            <div v-else-if="career.contract_type === 'contract'" class="justify-self-start col-span-2">契約社員</div>
+            <div v-else-if="career.contract_type === 'other'" class="justify-self-start col-span-2">その他</div>
+            <div v-else class="justify-self-start col-span-2">想定外の値です。管理者にご連絡下さい</div>
+            <div v-if="career.profession !== null" class="mt-2 ml-3 justify-self-start col-span-1">職種</div><div v-if="career.profession !== null" class="justify-self-start col-span-2">{{ career.profession }}</div>
+            <div v-if="career.annual_income_in_man_yen !== null" class="mt-2 ml-3 justify-self-start col-span-1">年収（単位：万円）</div><div v-if="career.annual_income_in_man_yen !== null" class="justify-self-start col-span-2">{{ career.annual_income_in_man_yen }}</div>
+            <div class="mt-2 justify-self-start col-span-1">管理職区分</div>
+            <div v-if="career.is_manager" class="justify-self-start col-span-2">管理職</div>
+            <div v-else class="justify-self-start col-span-2">非管理職</div>
+            <div v-if="career.position_name !== null" class="mt-2 ml-3 justify-self-start col-span-1">職位</div><div v-if="career.position_name !== null" class="justify-self-start col-span-2">{{ career.position_name }}</div>
+            <div class="mt-2 justify-self-start col-span-1">入社区分</div>
+            <div v-if="career.is_new_graduate" class="justify-self-start col-span-2">新卒入社</div>
+            <div v-else class="justify-self-start col-span-2">中途入社</div>
+            <div v-if="career.note !== null" class="mt-2 ml-3 justify-self-start col-span-1">備考</div><div v-if="career.note !== null" class="justify-self-start col-span-2">{{ career.note }}</div>
           </div>
-          <p v-else data-test="no-identity-set" class="m-4 text-xl">職務経歴を取得出来ませんでした。</p>
-          <button data-test="move-to-identity-page-button" v-on:click="moveToIdentityPage" class="mt-4 bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200">ユーザー情報を編集する</button>
-          -->
+          <p v-else data-test="no-career-set" class="m-4 text-xl">職務経歴を取得出来ませんでした。</p>
+          <button data-test="move-to-career-deletion-confirm-page-button" v-on:click="moveToCareerDeletionConfirmPage" class="mt-4 bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200">職務経歴を削除する</button>
         </div>
       </div>
     </main>
@@ -41,24 +50,32 @@
 import { defineComponent, onMounted, reactive, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import TheHeader from '@/components/TheHeader.vue'
+import AlertMessage from '@/components/AlertMessage.vue'
+import WaitingCircle from '@/components/WaitingCircle.vue'
 import { refresh } from '@/util/personalized/refresh/Refresh'
 import { RefreshResp } from '@/util/personalized/refresh/RefreshResp'
 import { ApiErrorResp } from '@/util/ApiError'
-import { Code } from '@/util/Error'
+import { Code, createErrorMessage } from '@/util/Error'
 import { useGetCareer } from '@/util/personalized/career-detail/useGetCareer'
+import { Message } from '@/util/Message'
+import { GetCareerResp } from '@/util/personalized/career-detail/GetCareerResp'
+import { Career } from '@/util/personalized/Career'
 
 export default defineComponent({
   name: 'CareerDetailPage',
   components: {
-    TheHeader
+    TheHeader,
+    WaitingCircle,
+    AlertMessage
   },
   setup () {
     const error = reactive({
       exists: false,
       message: ''
     })
-    const careerId = ref('' as string)
+    const career = ref(null as Career | null)
     const route = useRoute()
+    const careerId = route.params.career_id as string
     const router = useRouter()
     const {
       getCareerDone,
@@ -67,25 +84,52 @@ export default defineComponent({
     onMounted(async () => {
       try {
         const resp = await refresh()
-        if (resp instanceof RefreshResp) {
-          // セッションが存在し、利用規約に同意済のため、ログイン後のページを表示可能
-          // TODO: 正常系の処理
-          // TODO: 実装メモ
-          careerId.value = route.params.career_id as string
-        } else if (resp instanceof ApiErrorResp) {
+        if (!(resp instanceof RefreshResp)) {
+          if (!(resp instanceof ApiErrorResp)) {
+            throw new Error(`unexpected result on getting request detail: ${resp}`)
+          }
           const code = resp.getApiError().getCode()
           if (code === Code.UNAUTHORIZED) {
             await router.push('/login')
+            return
           } else if (code === Code.NOT_TERMS_OF_USE_AGREED_YET) {
             await router.push('/terms-of-use')
+            return
           }
-          // TODO: エラー処理
+          error.exists = true
+          error.message = createErrorMessage(resp.getApiError().getCode())
+          return
         }
+        const response = await getCareerFunc(parseInt(careerId))
+        if (!(response instanceof GetCareerResp)) {
+          if (!(response instanceof ApiErrorResp)) {
+            throw new Error(`unexpected result on getting request detail: ${response}`)
+          }
+          const code = response.getApiError().getCode()
+          if (code === Code.UNAUTHORIZED) {
+            await router.push('/login')
+            return
+          } else if (code === Code.NOT_TERMS_OF_USE_AGREED_YET) {
+            await router.push('/terms-of-use')
+            return
+          }
+          error.exists = true
+          error.message = createErrorMessage(response.getApiError().getCode())
+          return
+        }
+        career.value = response.getCareer()
       } catch (e) {
-        // TODO: エラー処理
+        error.exists = true
+        error.message = `${Message.UNEXPECTED_ERR}: ${e}`
       }
     })
-    return { error, getCareerDone, careerId }
+
+    const moveToCareerDeletionConfirmPage = async () => {
+      const route = { name: 'CareerDeletionConfirmPage', params: { career_id: careerId } }
+      await router.push(route)
+    }
+
+    return { error, career, getCareerDone, moveToCareerDeletionConfirmPage }
   }
 })
 </script>
