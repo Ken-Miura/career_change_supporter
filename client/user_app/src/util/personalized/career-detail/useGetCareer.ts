152a06f90b4a90ebab528ec4fa599c7507f208a2
kmiura <ken.miura1102@gmail.com>
2022-06-01 16:26:31 +0000

    Fix issue

--- client/user_app/src/util/personalized/career-detail/useGetCareer.ts
+++ client/user_app/src/util/personalized/career-detail/useGetCareer.ts
@@ -2,14 +2,14 @@ import { ref } from 'vue'
 import { getCareer } from './GetCareer'
 
 export function useGetCareer () {
-  const getCareerDone = ref(false)
+  const getCareerDone = ref(true)
   const getCareerFunc = async (careerId: number) => {
     try {
-      getCareerDone.value = true
+      getCareerDone.value = false
       const response = await getCareer(careerId)
       return response
     } finally {
-      getCareerDone.value = false
+      getCareerDone.value = true
     }
   }
   return {
