0152543e52185659d4e0a76553df93f9b01c8296
kmiura <ken.miura1102@gmail.com>
2022-06-08 14:20:57 +0000

    Add tests

---
+++ client/user_app/tests/unit/CareerDeletionConfirmPage.spec.ts
@@ -0,0 +1,83 @@
+import { flushPromises, mount, RouterLinkStub } from '@vue/test-utils'
+import { ref } from 'vue'
+import WaitingCircle from '@/components/WaitingCircle.vue'
+import AlertMessage from '@/components/AlertMessage.vue'
+import CareerDeletionConfirmPage from '@/views/personalized/CareerDeletionConfirmPage.vue'
+import TheHeader from '@/components/TheHeader.vue'
+import { DeleteCareerResp } from '@/util/personalized/career-deletion-confirm/DeleteCareerResp'
+import { refresh } from '@/util/personalized/refresh/Refresh'
+import { RefreshResp } from '@/util/personalized/refresh/RefreshResp'
+
+jest.mock('@/util/personalized/refresh/Refresh')
+const refreshMock = refresh as jest.MockedFunction<typeof refresh>
+
+const deleteCareerDoneMock = ref(true)
+const deleteCareerFuncMock = jest.fn()
+jest.mock('@/util/personalized/career-deletion-confirm/useDeleteCareer', () => ({
+  useDeleteCareer: () => ({
+    deleteCareerDone: deleteCareerDoneMock,
+    deleteCareerFunc: deleteCareerFuncMock
+  })
+}))
+
+let routeParam = ''
+const routerPushMock = jest.fn()
+jest.mock('vue-router', () => ({
+  useRoute: () => ({
+    params: {
+      career_id: routeParam
+    }
+  }),
+  useRouter: () => ({
+    push: routerPushMock
+  })
+}))
+
+describe('CareerDeletionConfirmPage.vue', () => {
+  beforeEach(() => {
+    routeParam = '1'
+    refreshMock.mockReset()
+    deleteCareerDoneMock.value = true
+    deleteCareerFuncMock.mockReset()
+    routerPushMock.mockClear()
+  })
+
+  it('has WaitingCircle and TheHeader while waiting response', async () => {
+    refreshMock.mockResolvedValue(RefreshResp.create())
+    deleteCareerDoneMock.value = false
+    const resp = DeleteCareerResp.create()
+    deleteCareerFuncMock.mockResolvedValue(resp)
+    const wrapper = mount(CareerDeletionConfirmPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const waitingCircles = wrapper.findAllComponents(WaitingCircle)
+    expect(waitingCircles.length).toBe(1)
+    const headers = wrapper.findAllComponents(TheHeader)
+    expect(headers.length).toBe(1)
+    // ユーザーに待ち時間を表すためにWaitingCircleが出ていることが確認できれば十分のため、
+    // mainが出ていないことまで確認しない。
+  })
+
+  it('does not display AlertMessage when error does not occur', async () => {
+    refreshMock.mockResolvedValue(RefreshResp.create())
+    const resp = DeleteCareerResp.create()
+    deleteCareerFuncMock.mockResolvedValue(resp)
+    const wrapper = mount(CareerDeletionConfirmPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const alertMessages = wrapper.findAllComponents(AlertMessage)
+    expect(alertMessages.length).toBe(0)
+  })
+})
