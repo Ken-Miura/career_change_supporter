c1574a332b5429d28decf374abb07e0dfb73a4b2
kmiura <ken.miura1102@gmail.com>
2022-06-10 16:06:06 +0000

    Add impl

--- client/user_app/src/views/personalized/FeePerHourInYenPage.vue
+++ client/user_app/src/views/personalized/FeePerHourInYenPage.vue
@@ -1,10 +1,24 @@
 <template>
   <TheHeader/>
   <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
-    <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 class="font-bold text-lg">{{ message }}</h3>
-      <p v-if="fee !== null" class="font-bold text-lg">{{ fee }}</p>
-      <p v-else class="font-bold text-lg">fee === null</p>
+    <div v-if="!true" class="m-6">
+      <WaitingCircle />
+    </div>
+    <main v-else class="flex flex-col justify-center bg-white max-w-2xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+      <h3 class="font-bold text-2xl">相談一回（１時間）の相談料</h3>
+      <p class="mt-2 text-lg">相談一回（１時間）の相談料には、3,000円以上、50,000円以下の値を設定出来ます。</p>
+      <form @submit.prevent="submitFeePerHourInYen">
+        <div class="m-4 text-2xl grid grid-cols-6">
+          <div class="mt-2 min-w-full justify-self-start col-span-5 pt-3 rounded bg-gray-200">
+            <input v-bind:value="feePerHourInYen" v-on:input="setFeePerHourInYen" type="text" minlength="4" maxlength="5" pattern="\d*" title="半角数字でご入力下さい。" class="text-right bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
+          </div>
+          <div class="mt-2 ml-4 min-w-full justify-self-start col-span-1 pt-3">
+            円
+          </div>
+        </div>
+        <button data-test="submit-button" class="mt-4 min-w-full bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">相談一回（１時間）の相談料を設定する</button>
+        <AlertMessage v-bind:class="['mt-6', { 'hidden': !error.exists }]" v-bind:message="error.message"/>
+      </form>
     </main>
     <footer class="max-w-lg mx-auto flex justify-center text-white">
       <router-link to="/" class="hover:underline">トップページへ</router-link>
@@ -13,49 +27,67 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, onMounted, reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import TheHeader from '@/components/TheHeader.vue'
+import AlertMessage from '@/components/AlertMessage.vue'
+import WaitingCircle from '@/components/WaitingCircle.vue'
 import { useStore } from 'vuex'
 import { refresh } from '@/util/personalized/refresh/Refresh'
 import { RefreshResp } from '@/util/personalized/refresh/RefreshResp'
 import { ApiErrorResp } from '@/util/ApiError'
-import { Code } from '@/util/Error'
+import { Code, createErrorMessage } from '@/util/Error'
 
 export default defineComponent({
   name: 'FeePerHourInYenPage',
   components: {
-    TheHeader
+    TheHeader,
+    WaitingCircle,
+    AlertMessage
   },
   setup () {
-    const message = ref('相談料用テストページ')
-    const fee = ref(null as number | null)
+    const error = reactive({
+      exists: false,
+      message: ''
+    })
+    const feePerHourInYen = ref(null as string | null)
     const router = useRouter()
     const store = useStore()
     onMounted(async () => {
-      try {
-        const resp = await refresh()
-        if (resp instanceof RefreshResp) {
-          // セッションが存在し、利用規約に同意済のため、ログイン後のページを表示可能
-          // TODO: 正常系の処理
-          fee.value = store.state.feePerHourInYen
-        } else if (resp instanceof ApiErrorResp) {
-          const code = resp.getApiError().getCode()
-          if (code === Code.UNAUTHORIZED) {
-            await router.push('/login')
-            return
-          } else if (code === Code.NOT_TERMS_OF_USE_AGREED_YET) {
-            await router.push('/terms-of-use')
-            return
-          }
-          // TODO: エラー処理
+      const resp = await refresh()
+      if (!(resp instanceof RefreshResp)) {
+        if (!(resp instanceof ApiErrorResp)) {
+          throw new Error(`unexpected result on getting request detail: ${resp}`)
         }
-      } catch (e) {
-        // TODO: エラー処理
+        const code = resp.getApiError().getCode()
+        if (code === Code.UNAUTHORIZED) {
+          await router.push('/login')
+          return
+        } else if (code === Code.NOT_TERMS_OF_USE_AGREED_YET) {
+          await router.push('/terms-of-use')
+          return
+        }
+        error.exists = true
+        error.message = createErrorMessage(resp.getApiError().getCode())
+        return
       }
-      console.log('TODO: 実装後削除')
+      const value = store.state.feePerHourInYen
+      feePerHourInYen.value = value ? value.toString() : ''
     })
-    return { message, fee }
+
+    const setFeePerHourInYen = (e: Event) => {
+      const target = (e && e.target)
+      if (!(target instanceof HTMLInputElement)) {
+        throw new Error(`!(target instanceof HTMLInputElement): target is ${target}`)
+      }
+      feePerHourInYen.value = target.value
+    }
+
+    const submitFeePerHourInYen = async () => {
+      console.log('submitFeePerHourInYen')
+    }
+
+    return { error, feePerHourInYen, setFeePerHourInYen, submitFeePerHourInYen }
   }
 })
 </script>
