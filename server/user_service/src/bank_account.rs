715a401b90c7037a44e591be2fd4df38fe1f5061
kmiura <ken.miura1102@gmail.com>
2022-06-16 15:02:12 +0000

    Add impl

--- server/user_service/src/bank_account.rs
+++ server/user_service/src/bank_account.rs
@@ -1,12 +1,14 @@
 // Copyright 2022 Ken Miura
 
+use axum::async_trait;
 use axum::http::StatusCode;
 use axum::{Extension, Json};
 use common::{ApiError, ErrResp, RespResult};
-use entity::sea_orm::DatabaseConnection;
+use entity::sea_orm::{DatabaseConnection, EntityTrait};
 use serde::Serialize;
 use tracing::error;
 
+use crate::err::unexpected_err_resp;
 use crate::{
     err::Code,
     util::{
@@ -37,16 +39,47 @@ async fn handle_bank_account_req(
         error!("invalid bank account: {}", e);
         create_invalid_bank_account_err(&e)
     })?;
+
+    let identity_exists = op.check_if_identity_exists(account_id).await?;
+    if !identity_exists {
+        error!("identity is not registered (account id: {})", account_id);
+        return Err((
+            StatusCode::BAD_REQUEST,
+            Json(ApiError {
+                code: Code::NoIdentityRegistered as u32,
+            }),
+        ));
+    }
+
     todo!()
 }
 
-trait SubmitBankAccountOperation {}
+#[async_trait]
+trait SubmitBankAccountOperation {
+    /// Identityが存在するか確認する。存在する場合、trueを返す。そうでない場合、falseを返す。
+    async fn check_if_identity_exists(&self, account_id: i64) -> Result<bool, ErrResp>;
+}
 
 struct SubmitBankAccountOperationImpl {
     pool: DatabaseConnection,
 }
 
-impl SubmitBankAccountOperation for SubmitBankAccountOperationImpl {}
+#[async_trait]
+impl SubmitBankAccountOperation for SubmitBankAccountOperationImpl {
+    async fn check_if_identity_exists(&self, account_id: i64) -> Result<bool, ErrResp> {
+        let model = entity::prelude::Identity::find_by_id(account_id)
+            .one(&self.pool)
+            .await
+            .map_err(|e| {
+                error!(
+                    "failed to find identity (user_account_id: {}): {}",
+                    account_id, e
+                );
+                unexpected_err_resp()
+            })?;
+        Ok(model.is_some())
+    }
+}
 
 fn create_invalid_bank_account_err(e: &BankAccountValidationError) -> ErrResp {
     let code;
