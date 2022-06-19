0228d08d6e2f9cfc639ed98e7bf45adf4fada469
kmiura <ken.miura1102@gmail.com>
2022-06-19 15:14:42 +0000

    Add impl

--- client/user_app/src/views/personalized/BankAccountPage.vue
+++ client/user_app/src/views/personalized/BankAccountPage.vue
@@ -7,39 +7,40 @@
     <main v-else>
       <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
         <h3 class="font-bold text-2xl">報酬の入金口座</h3>
-        <p class="mt-2 text-lg">受け取った報酬を入金するための口座で、相談受け付けを行うために必要となる情報です。他のユーザーに公開されることはありません。ユーザー情報で本人確認が完了した姓名と異なる名義の口座は設定できません。</p>
         <form @submit.prevent="submitBankAccount">
-          <!-- <div class="m-4 text-2xl grid grid-cols-6">
+          <div class="m-4 text-2xl grid grid-cols-6">
             <div class="mt-2 text-2xl justify-self-start col-span-6 pt-3">
-              氏名
+              銀行コード
             </div>
-            <div data-test="last-name-div" class="mt-2 justify-self-start col-span-3 pt-3 rounded bg-gray-200">
-              <label class="block text-gray-700 text-sm font-bold mb-2 ml-3">姓</label>
-              <input v-bind:value="form.lastName" v-on:input="setLastName" type="text" required minlength="1" maxlength="64" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
-            </div>
-            <div data-test="first-name-div" class="mt-2 ml-1 justify-self-start col-span-3 pt-3 rounded bg-gray-200">
-              <label class="block text-gray-700 text-sm font-bold mb-2 ml-3">名</label>
-              <input v-bind:value="form.firstName" v-on:input="setFirstName" type="text" required minlength="1" maxlength="64" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
+            <div data-test="bank-code-div" class="mt-2 justify-self-start col-span-6 pt-3 rounded bg-gray-200">
+              <input v-bind:value="bankAccount.bank_code" v-on:input="setBankCode" type="text" required minlength="4" maxlength="4" pattern="\d*" title="半角数字4桁でご入力下さい。" class="text-right bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
             </div>
             <div class="mt-2 text-2xl justify-self-start col-span-6 pt-3">
-              フリガナ
+              支店コード
             </div>
-            <div data-test="last-name-furigana-div" class="mt-2 justify-self-start col-span-3 pt-3 rounded bg-gray-200">
-              <label class="block text-gray-700 text-sm font-bold mb-2 ml-3">セイ</label>
-              <input v-bind:value="form.lastNameFurigana" v-on:input="setLastNameFurigana" type="text" required minlength="1" maxlength="64" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
+            <div data-test="branch-code-div" class="mt-2 justify-self-start col-span-6 pt-3 rounded bg-gray-200">
+              <input v-bind:value="bankAccount.branch_code" v-on:input="setBranchCode" type="text" required minlength="3" maxlength="3" pattern="\d*" title="半角数字3桁でご入力下さい。" class="text-right bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
             </div>
-            <div data-test="first-name-furigana-div" class="mt-2 ml-1 justify-self-start col-span-3 pt-3 rounded bg-gray-200">
-              <label class="block text-gray-700 text-sm font-bold mb-2 ml-3">メイ</label>
-              <input v-bind:value="form.firstNameFurigana" v-on:input="setFirstNameFurigana" type="text" required minlength="1" maxlength="64" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
+            <div class="mt-2 text-2xl justify-self-start col-span-6 pt-3">
+              預金種別（預金種別は普通のみサポートしております）
             </div>
-            <div data-test="tel-div" class="mt-4 text-2xl justify-self-start col-span-6 pt-3">
-              電話番号（ハイフンを含めず、半角数字のみで入力）
+            <div data-test="account-type-div" class="mt-2 justify-self-start col-span-6 pt-3 rounded bg-gray-200">
+              <label class="text-right bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">{{ bankAccount.account_type }}</label>
             </div>
-            <div data-test="tel-input-div" class="mt-2 w-full justify-self-start col-span-6 pt-3 pl-2 rounded bg-gray-200">
-              <input v-bind:value="form.telephoneNumber" v-on:input="setTelephoneNumber" type="text" inputmode="tel" pattern="[0-9]{10,13}" title="半角数字のみで10桁以上13桁以下でご入力下さい。" required minlength="10" maxlength="13" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
+            <div class="mt-2 text-2xl justify-self-start col-span-6 pt-3">
+              口座番号
             </div>
-          </div> -->
-          <button data-test="submit-button" class="mt-4 min-w-full bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">本人確認を依頼する</button>
+            <div data-test="account-number-div" class="mt-2 justify-self-start col-span-6 pt-3 rounded bg-gray-200">
+              <input v-bind:value="bankAccount.account_number" v-on:input="setAccountNumber" type="text" required minlength="7" maxlength="8" pattern="\d*" title="半角数字7桁または8桁でご入力下さい。" class="text-right bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
+            </div>
+            <div class="mt-2 text-2xl justify-self-start col-span-6 pt-3">
+              口座名義（全角カタカナと全角空白のみを使い、セイとメイの間に全角空白を入れて下さい）
+            </div>
+            <div data-test="account-holder-name-div" class="mt-2 justify-self-start col-span-6 pt-3 rounded bg-gray-200">
+              <input v-bind:value="bankAccount.account_holder_name" v-on:input="setAccountHolderName" type="text" required minlength="3" maxlength="129" pattern="^[ァ-ヴー　]+$" title="全角カタカナと全角空白のみで、3文字以上129文字以内でご入力下さい。" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
+            </div>
+          </div>
+          <button data-test="submit-button" class="mt-4 min-w-full bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">報酬の入金口座を設定する</button>
           <AlertMessage v-bind:class="['mt-6', { 'hidden': !error.exists }]" v-bind:message="error.message"/>
         </form>
       </div>
@@ -145,11 +146,48 @@ export default defineComponent({
         error.message = `${Message.UNEXPECTED_ERR}: ${e}`
       }
     }
+
+    const setBankCode = (e: Event) => {
+      const target = (e && e.target)
+      if (!(target instanceof HTMLInputElement)) {
+        throw new Error(`!(target instanceof HTMLInputElement): target is ${target}`)
+      }
+      bankAccount.value.bank_code = target.value
+    }
+
+    const setBranchCode = (e: Event) => {
+      const target = (e && e.target)
+      if (!(target instanceof HTMLInputElement)) {
+        throw new Error(`!(target instanceof HTMLInputElement): target is ${target}`)
+      }
+      bankAccount.value.branch_code = target.value
+    }
+
+    const setAccountNumber = (e: Event) => {
+      const target = (e && e.target)
+      if (!(target instanceof HTMLInputElement)) {
+        throw new Error(`!(target instanceof HTMLInputElement): target is ${target}`)
+      }
+      bankAccount.value.account_number = target.value
+    }
+
+    const setAccountHolderName = (e: Event) => {
+      const target = (e && e.target)
+      if (!(target instanceof HTMLInputElement)) {
+        throw new Error(`!(target instanceof HTMLInputElement): target is ${target}`)
+      }
+      bankAccount.value.account_holder_name = target.value
+    }
+
     return {
       error,
       bankAccount,
       postBankAccountDone,
-      submitBankAccount
+      submitBankAccount,
+      setBankCode,
+      setBranchCode,
+      setAccountNumber,
+      setAccountHolderName
     }
   }
 })
