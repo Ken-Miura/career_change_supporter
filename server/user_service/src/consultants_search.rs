b9beb04266657fd168b4276e7f2a0769bab4c288
kmiura <ken.miura1102@gmail.com>
2022-07-23 13:59:38 +0000

    Add test

--- server/user_service/src/consultants_search.rs
+++ server/user_service/src/consultants_search.rs
@@ -1707,6 +1707,48 @@ mod tests {
                 )),
             },
             TestCase {
+                name: "equal_or_more exceeds equal_or_less annual income in man yen".to_string(),
+                input: Input {
+                    account_id: 1,
+                    param: ConsultantSearchParam {
+                        career_param: CareerParam {
+                            company_name: None,
+                            department_name: None,
+                            office: None,
+                            years_of_service: None,
+                            employed: None,
+                            contract_type: None,
+                            profession: None,
+                            annual_income_in_man_yen: AnnualInComeInManYenParam {
+                                equal_or_more: Some(1),
+                                equal_or_less: Some(0),
+                            },
+                            is_manager: None,
+                            position_name: None,
+                            is_new_graduate: None,
+                            note: None,
+                        },
+                        fee_per_hour_yen_param: FeePerHourYenParam {
+                            equal_or_more: None,
+                            equal_or_less: None,
+                        },
+                        sort_param: None,
+                        from: 0,
+                        size: 20,
+                    },
+                    op: ConsultantsSearchOperationMock {
+                        account_id: 2,
+                        query_result: create_empty_result(),
+                    },
+                },
+                expected: Err((
+                    StatusCode::BAD_REQUEST,
+                    Json(ApiError {
+                        code: Code::EqualOrMoreExceedsEqualOrLessInAnnualIncomInManYen as u32,
+                    }),
+                )),
+            },
+            TestCase {
                 name: "no identity registered".to_string(),
                 input: Input {
                     account_id: 1,
