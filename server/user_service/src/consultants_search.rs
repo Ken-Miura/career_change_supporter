feb293518a39c73aab5e2417cc8dc26c1b5a10cf
kmiura <ken.miura1102@gmail.com>
2022-07-23 13:24:25 +0000

    Add test

--- server/user_service/src/consultants_search.rs
+++ server/user_service/src/consultants_search.rs
@@ -1287,6 +1287,90 @@ mod tests {
                 )),
             },
             TestCase {
+                name: "invalid department name length".to_string(),
+                input: Input {
+                    account_id: 1,
+                    param: ConsultantSearchParam {
+                        career_param: CareerParam {
+                            company_name: None,
+                            department_name: Some("".to_string()),
+                            office: None,
+                            years_of_service: None,
+                            employed: None,
+                            contract_type: None,
+                            profession: None,
+                            annual_income_in_man_yen: AnnualInComeInManYenParam {
+                                equal_or_more: None,
+                                equal_or_less: None,
+                            },
+                            is_manager: None,
+                            position_name: None,
+                            is_new_graduate: None,
+                            note: None,
+                        },
+                        fee_per_hour_yen_param: FeePerHourYenParam {
+                            equal_or_more: None,
+                            equal_or_less: None,
+                        },
+                        sort_param: None,
+                        from: 0,
+                        size: 20,
+                    },
+                    op: ConsultantsSearchOperationMock {
+                        account_id: 2,
+                        query_result: create_empty_result(),
+                    },
+                },
+                expected: Err((
+                    StatusCode::BAD_REQUEST,
+                    Json(ApiError {
+                        code: Code::InvalidDepartmentNameLength as u32,
+                    }),
+                )),
+            },
+            TestCase {
+                name: "illegal department name".to_string(),
+                input: Input {
+                    account_id: 1,
+                    param: ConsultantSearchParam {
+                        career_param: CareerParam {
+                            company_name: None,
+                            department_name: Some("*".to_string()),
+                            office: None,
+                            years_of_service: None,
+                            employed: None,
+                            contract_type: None,
+                            profession: None,
+                            annual_income_in_man_yen: AnnualInComeInManYenParam {
+                                equal_or_more: None,
+                                equal_or_less: None,
+                            },
+                            is_manager: None,
+                            position_name: None,
+                            is_new_graduate: None,
+                            note: None,
+                        },
+                        fee_per_hour_yen_param: FeePerHourYenParam {
+                            equal_or_more: None,
+                            equal_or_less: None,
+                        },
+                        sort_param: None,
+                        from: 0,
+                        size: 20,
+                    },
+                    op: ConsultantsSearchOperationMock {
+                        account_id: 2,
+                        query_result: create_empty_result(),
+                    },
+                },
+                expected: Err((
+                    StatusCode::BAD_REQUEST,
+                    Json(ApiError {
+                        code: Code::IllegalCharInDepartmentName as u32,
+                    }),
+                )),
+            },
+            TestCase {
                 name: "no identity registered".to_string(),
                 input: Input {
                     account_id: 1,
