91addf9b9053f9916deeec41db2e91a05f9c703f
kmiura <ken.miura1102@gmail.com>
2022-08-06 09:41:25 +0000

    Add test

--- client/user_app/tests/unit/ConsultantListPage.spec.ts
+++ client/user_app/tests/unit/ConsultantListPage.spec.ts
@@ -1,7 +1,14 @@
 import { getPageSize, PAGE_SIZE } from '@/util/PageSize'
 import { ConsultantSearchParam } from '@/util/personalized/ConsultantSearchParam'
 import { refresh } from '@/util/personalized/refresh/Refresh'
+import { RefreshResp } from '@/util/personalized/refresh/RefreshResp'
+import { flushPromises, mount, RouterLinkStub } from '@vue/test-utils'
 import { ref } from 'vue'
+import WaitingCircle from '@/components/WaitingCircle.vue'
+import TheHeader from '@/components/TheHeader.vue'
+import ConsultantListPage from '@/views/personalized/ConsultantListPage.vue'
+import { PostConsultantsSearchResp } from '@/util/personalized/consultant-list/PostConsultantsSearchResp'
+import { ConsultantsSearchResult } from '@/util/personalized/consultant-list/ConsultantsSearchResult'
 
 jest.mock('@/util/personalized/refresh/Refresh')
 const refreshMock = refresh as jest.MockedFunction<typeof refresh>
@@ -44,7 +51,29 @@ describe('ConsultantListPage.vue', () => {
     postConsultantsSearchFuncMock.mockReset()
   })
 
-  it('', () => {
-    console.log('TODO: Add test')
+  it('has WaitingCircle and TheHeader while waiting response', async () => {
+    refreshMock.mockResolvedValue(RefreshResp.create())
+    postConsultantsSearchDoneMock.value = false
+    const result = {
+      total: 0,
+      consultants: []
+    } as ConsultantsSearchResult
+    const resp = PostConsultantsSearchResp.create(result)
+    postConsultantsSearchFuncMock.mockResolvedValue(resp)
+    const wrapper = mount(ConsultantListPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const waitingCircles = wrapper.findAllComponents(WaitingCircle)
+    expect(waitingCircles.length).toBe(1)
+    const headers = wrapper.findAllComponents(TheHeader)
+    expect(headers.length).toBe(1)
+    // ユーザーに待ち時間を表すためにWaitingCircleが出ていることが確認できれば十分のため、
+    // mainが出ていないことまで確認しない。
   })
 })
