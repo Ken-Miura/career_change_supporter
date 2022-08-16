152cb200b770a13e0ccbca77285f84630a9cbf5c
kmiura <ken.miura1102@gmail.com>
2022-08-16 16:36:34 +0000

    Add impl

--- server/user_service/src/consultant_detail.rs
+++ server/user_service/src/consultant_detail.rs
@@ -1,7 +1,9 @@
 // Copyright 2022 Ken Miura
 
+use async_session::serde_json::Value;
+use axum::async_trait;
 use axum::{extract::Query, Extension};
-use common::RespResult;
+use common::{ErrResp, RespResult};
 use entity::sea_orm::DatabaseConnection;
 use opensearch::OpenSearch;
 use serde::{Deserialize, Serialize};
@@ -15,10 +17,8 @@ pub(crate) async fn get_consultant_detail(
     Extension(index_client): Extension<OpenSearch>,
 ) -> RespResult<ConsultantDetail> {
     let query = query.0;
-    if !query.consultant_id.is_positive() {
-        todo!()
-    }
-    todo!()
+    let op = ConsultantDetailOperationImpl { pool, index_client };
+    handle_consultant_detail(account_id, query.consultant_id, op).await
 }
 
 #[derive(Deserialize)]
@@ -28,3 +28,37 @@ pub(crate) struct ConsultantDetailQuery {
 
 #[derive(Clone, Debug, Serialize)]
 pub(crate) struct ConsultantDetail {}
+
+#[async_trait]
+trait ConsultantDetailOperation {
+    /// Identityが存在するか確認する。存在する場合、trueを返す。そうでない場合、falseを返す。
+    async fn check_if_identity_exists(&self, account_id: i64) -> Result<bool, ErrResp>;
+    async fn search_consultant(&self, index_name: &str, query: &Value) -> Result<Value, ErrResp>;
+}
+
+struct ConsultantDetailOperationImpl {
+    pool: DatabaseConnection,
+    index_client: OpenSearch,
+}
+
+#[async_trait]
+impl ConsultantDetailOperation for ConsultantDetailOperationImpl {
+    async fn check_if_identity_exists(&self, account_id: i64) -> Result<bool, ErrResp> {
+        todo!()
+    }
+
+    async fn search_consultant(&self, index_name: &str, query: &Value) -> Result<Value, ErrResp> {
+        todo!()
+    }
+}
+
+async fn handle_consultant_detail(
+    account_id: i64,
+    consultant_id: i64,
+    op: impl ConsultantDetailOperation,
+) -> RespResult<ConsultantDetail> {
+    if !consultant_id.is_positive() {
+        todo!()
+    }
+    todo!()
+}
