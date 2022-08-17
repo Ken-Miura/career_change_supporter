1d95e2ed1a03290cc25c8e7030cd8537df2e476a
kmiura <ken.miura1102@gmail.com>
2022-08-17 17:27:12 +0000

    Add consultant career detail

--- server/user_service/src/consultant_detail.rs
+++ server/user_service/src/consultant_detail.rs
@@ -4,10 +4,12 @@ use async_session::serde_json::Value;
 use axum::async_trait;
 use axum::{extract::Query, Extension};
 use common::{ErrResp, RespResult};
-use entity::sea_orm::DatabaseConnection;
+use entity::sea_orm::{DatabaseConnection, EntityTrait};
 use opensearch::OpenSearch;
 use serde::{Deserialize, Serialize};
+use tracing::error;
 
+use crate::err::unexpected_err_resp;
 use crate::util::session::User;
 
 pub(crate) async fn get_consultant_detail(
@@ -23,11 +25,34 @@ pub(crate) async fn get_consultant_detail(
 
 #[derive(Deserialize)]
 pub(crate) struct ConsultantDetailQuery {
-    pub(crate) consultant_id: i64,
+    pub consultant_id: i64,
 }
 
 #[derive(Clone, Debug, Serialize)]
-pub(crate) struct ConsultantDetail {}
+pub(crate) struct ConsultantDetail {
+    pub consultant_id: i64,
+    pub fee_per_hour_in_yen: i32,
+    pub rating: Option<f64>,
+    pub num_of_rated: i32,
+    pub careers: Vec<ConsultantCareerDetail>,
+}
+
+#[derive(Clone, Debug, Serialize, PartialEq)]
+pub(crate) struct ConsultantCareerDetail {
+    pub career_id: i64,
+    pub company_name: String,
+    pub department_name: Option<String>,
+    pub office: Option<String>,
+    pub years_of_service: String,
+    pub employed: bool,
+    pub contract_type: String,
+    pub profession: Option<String>,
+    pub annual_income_in_man_yen: Option<i32>,
+    pub is_manager: bool,
+    pub position_name: Option<String>,
+    pub is_new_graduate: bool,
+    pub note: Option<String>,
+}
 
 #[async_trait]
 trait ConsultantDetailOperation {
@@ -44,7 +69,17 @@ struct ConsultantDetailOperationImpl {
 #[async_trait]
 impl ConsultantDetailOperation for ConsultantDetailOperationImpl {
     async fn check_if_identity_exists(&self, account_id: i64) -> Result<bool, ErrResp> {
-        todo!()
+        let model = entity::prelude::Identity::find_by_id(account_id)
+            .one(&self.pool)
+            .await
+            .map_err(|e| {
+                error!(
+                    "failed to find identity (user_account_id: {}): {}",
+                    account_id, e
+                );
+                unexpected_err_resp()
+            })?;
+        Ok(model.is_some())
     }
 
     async fn search_consultant(&self, index_name: &str, query: &Value) -> Result<Value, ErrResp> {
