fe4424c5dc5ef7f2467fb798b9e3a4fe4bc13f5f
kmiura <ken.miura1102@gmail.com>
2022-08-31 16:36:35 +0000

    Add test

--- client/user_app/tests/unit/ConsultantDetailPage.spec.ts
+++ client/user_app/tests/unit/ConsultantDetailPage.spec.ts
@@ -8,7 +8,7 @@ import ConsultantDetailPage from '@/views/personalized/ConsultantDetailPage.vue'
 import { GetConsultantDetailResp } from '@/util/personalized/consultant-detail/GetConsultantDetailResp'
 import { ConsultantDetail } from '@/util/personalized/consultant-detail/ConsultantDetail'
 import { ConsultantCareerDetail } from '@/util/personalized/consultant-detail/ConsultantCareerDetail'
-import { LESS_THAN_THREE_YEARS } from '@/util/personalized/consultant-detail/YearsOfService'
+import { LESS_THAN_THREE_YEARS, THREE_YEARS_OR_MORE_LESS_THAN_FIVE_YEARS } from '@/util/personalized/consultant-detail/YearsOfService'
 import { Message } from '@/util/Message'
 import { Code } from '@/util/Error'
 import { ApiError, ApiErrorResp } from '@/util/ApiError'
@@ -54,6 +54,33 @@ const consultant1 = {
     position_name: null,
     is_new_graduate: true,
     note: null
+  } as ConsultantCareerDetail]
+} as ConsultantDetail
+
+const consultant2 = {
+  consultant_id: 2,
+  fee_per_hour_in_yen: 50000,
+  rating: 4.5,
+  num_of_rated: 10,
+  careers: [{
+    counsultant_career_detail_id: 1,
+    company_name: 'テスト（株）',
+    department_name: '開発部',
+    office: '那覇事業所',
+    years_of_service: THREE_YEARS_OR_MORE_LESS_THAN_FIVE_YEARS,
+    employed: false,
+    contract_type: 'contract',
+    profession: 'エンジニア',
+    annual_income_in_man_yen: 400,
+    is_manager: true,
+    position_name: '係長',
+    is_new_graduate: false,
+    note: `
+    備考テスト
+    改行１
+    改行２
+    改行３
+    `
   } as ConsultantCareerDetail]
 } as ConsultantDetail
 
@@ -208,7 +235,7 @@ describe('ConsultantDetailPage.vue', () => {
     const ratingLabel = wrapper.find('[data-test="rating-label"]')
     expect(ratingLabel.text()).toContain(`評価（評価件数：${consultant1.num_of_rated} 件）`)
     const ratingValue = wrapper.find('[data-test="rating-value"]')
-    expect(ratingValue.text()).toContain('0')
+    expect(ratingValue.text()).toContain('0/5')
 
     const careerLabel = wrapper.find('[data-test="career-label"]')
     expect(careerLabel.text()).toContain('職務経歴')
@@ -277,5 +304,108 @@ describe('ConsultantDetailPage.vue', () => {
     expect(noteLabel.exists()).toBe(false)
     const noteValue = career0.find('[data-test="note-value"]')
     expect(noteValue.exists()).toBe(false)
+  })
+
+  it('displays consultant detail case 2', async () => {
+    routeParam = '2'
+    const resp = GetConsultantDetailResp.create(consultant2)
+    getConsultantDetailFuncMock.mockResolvedValue(resp)
+    const wrapper = mount(ConsultantDetailPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const consultantDetailLabel = wrapper.find('[data-test="consultant-detail-label"]')
+    expect(consultantDetailLabel.text()).toContain('コンサルタント詳細')
+
+    const consultantIdLabel = wrapper.find('[data-test="consultant-id-label"]')
+    expect(consultantIdLabel.text()).toContain('コンサルタントID')
+    const consultantIdValue = wrapper.find('[data-test="consultant-id-value"]')
+    expect(consultantIdValue.text()).toContain(`${consultant2.consultant_id}`)
+
+    const feePerHourInYenLabel = wrapper.find('[data-test="fee-per-hour-in-yen-label"]')
+    expect(feePerHourInYenLabel.text()).toContain('相談一回（１時間）の相談料')
+    const feePerHourInYenValue = wrapper.find('[data-test="fee-per-hour-in-yen-value"]')
+    expect(feePerHourInYenValue.text()).toContain(`${consultant2.fee_per_hour_in_yen}円`)
+
+    const ratingLabel = wrapper.find('[data-test="rating-label"]')
+    expect(ratingLabel.text()).toContain(`評価（評価件数：${consultant2.num_of_rated} 件）`)
+    const ratingValue = wrapper.find('[data-test="rating-value"]')
+    expect(ratingValue.text()).toContain(`${consultant2.rating}/5`)
+
+    const careerLabel = wrapper.find('[data-test="career-label"]')
+    expect(careerLabel.text()).toContain('職務経歴')
+
+    const career0 = wrapper.find('[data-test="career-detail-0"]')
+
+    const career0DetailLabel = career0.find('[data-test="career-detail-label"]')
+    expect(career0DetailLabel.text()).toContain('職務経歴1')
+
+    const companyNameLabel = career0.find('[data-test="company-name-label"]')
+    expect(companyNameLabel.text()).toContain('勤務先名称')
+    const companyNameValue = career0.find('[data-test="company-name-value"]')
+    expect(companyNameValue.text()).toContain(`${consultant2.careers[0].company_name}`)
+
+    const departmentNameLabel = career0.find('[data-test="department-name-label"]')
+    expect(departmentNameLabel.text()).toContain('部署名')
+    const departmentNameValue = career0.find('[data-test="department-name-value"]')
+    expect(departmentNameValue.text()).toContain(`${consultant2.careers[0].department_name}`)
+
+    const officeLabel = career0.find('[data-test="office-label"]')
+    expect(officeLabel.text()).toContain('勤務地')
+    const officeValue = career0.find('[data-test="office-value"]')
+    expect(officeValue.text()).toContain(`${consultant2.careers[0].office}`)
+
+    const yearsOfServiceLabel = career0.find('[data-test="years-of-service-label"]')
+    expect(yearsOfServiceLabel.text()).toContain('在籍年数')
+    const yearsOfServiceValue = career0.find('[data-test="years-of-service-value"]')
+    expect(yearsOfServiceValue.text()).toContain('3年以上5年未満')
+
+    const employedLabel = career0.find('[data-test="employed-label"]')
+    expect(employedLabel.text()).toContain('在籍の有無')
+    const employedValue = career0.find('[data-test="employed-value"]')
+    expect(employedValue.text()).toContain('退職済')
+
+    const contractTypeLabel = career0.find('[data-test="contract-type-label"]')
+    expect(contractTypeLabel.text()).toContain('雇用形態')
+    const contractTypeValue = career0.find('[data-test="contract-type-value"]')
+    expect(contractTypeValue.text()).toContain('契約社員')
+
+    const professionLabel = career0.find('[data-test="profession-label"]')
+    expect(professionLabel.text()).toContain('職種')
+    const professionValue = career0.find('[data-test="profession-value"]')
+    expect(professionValue.text()).toContain(`${consultant2.careers[0].profession}`)
+
+    const annualIncomeInManYenLabel = career0.find('[data-test="annual-income-in-man-yen-label"]')
+    expect(annualIncomeInManYenLabel.text()).toContain('年収')
+    const annualIncomeInManYenValue = career0.find('[data-test="annual-income-in-man-yen-value"]')
+    expect(annualIncomeInManYenValue.text()).toContain(`${consultant2.careers[0].annual_income_in_man_yen}万円`)
+
+    const isManagerLabel = career0.find('[data-test="is-manager-label"]')
+    expect(isManagerLabel.text()).toContain('管理職区分')
+    const isManagerValue = career0.find('[data-test="is-manager-value"]')
+    expect(isManagerValue.text()).toContain('管理職')
+
+    const positionNameLabel = career0.find('[data-test="position-name-label"]')
+    expect(positionNameLabel.text()).toContain('職位')
+    const positionNameValue = career0.find('[data-test="position-name-value"]')
+    expect(positionNameValue.text()).toContain(`${consultant2.careers[0].position_name}`)
+
+    const isNewGraduateLabel = career0.find('[data-test="is-new-graduate-label"]')
+    expect(isNewGraduateLabel.text()).toContain('入社区分')
+    const isNewGraduateValue = career0.find('[data-test="is-new-graduate-value"]')
+    expect(isNewGraduateValue.text()).toContain('中途入社')
+
+    const noteLabel = career0.find('[data-test="note-label"]')
+    expect(noteLabel.text()).toContain('備考')
+    const noteValue = career0.find('[data-test="note-value"]')
+    expect(noteValue.text()).toContain(`備考テスト
+    改行１
+    改行２
+    改行３`)
   })
 })
