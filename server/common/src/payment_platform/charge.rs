546de0c25636218d3851abfbcd40212afc5182da
kmiura <ken.miura1102@gmail.com>
2022-09-23 14:18:46 +0000

    Add test

--- server/common/src/payment_platform/charge.rs
+++ server/common/src/payment_platform/charge.rs
@@ -965,4 +965,30 @@ mod tests {
         assert_eq!(None, create_charge.tenant());
         assert_eq!(None, create_charge.three_d_secure());
     }
+
+    #[test]
+    fn create_charge_success_description() {
+        let price = (50, "jpy".to_string());
+        let card_id = "tok_bdf884b520c6421d6df4b997c426";
+        let description = "description for test";
+
+        let result = CreateCharge::build()
+            .price(&price)
+            .card(card_id)
+            .description(description)
+            .finish();
+        let create_charge = result.expect("failed to get Ok");
+
+        assert_eq!(Some(price), create_charge.price());
+        assert_eq!(None, create_charge.product());
+        assert_eq!(None, create_charge.customer());
+        assert_eq!(Some(card_id.to_string()), create_charge.card());
+        assert_eq!(Some(description.to_string()), create_charge.description());
+        assert_eq!(None, create_charge.capture());
+        assert_eq!(None, create_charge.expiry_days());
+        assert_eq!(None, create_charge.metadata());
+        assert_eq!(None, create_charge.platform_fee());
+        assert_eq!(None, create_charge.tenant());
+        assert_eq!(None, create_charge.three_d_secure());
+    }
 }
