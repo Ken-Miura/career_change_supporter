6abd5ba58aae6a467e43f77985d273970902db24
kmiura <ken.miura1102@gmail.com>
2022-10-25 14:09:19 +0000

    Add computed

--- client/user_app/src/util/personalized/request-consultation/useCandidate.ts
+++ client/user_app/src/util/personalized/request-consultation/useCandidate.ts
@@ -1,4 +1,4 @@
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue'
 
 // eslint-disable-next-line
 export function useCandidate () {
@@ -16,7 +16,37 @@ export function useCandidate () {
     thirdCandidateDayInJst: '',
     thirdCandidateHourInJst: ''
   })
+
+  const allCandidatesAreNotEmpty = computed(() => {
+    return candidates.firstCandidateYearInJst !== '' &&
+      candidates.firstCandidateMonthInJst !== '' &&
+      candidates.firstCandidateDayInJst !== '' &&
+      candidates.firstCandidateHourInJst !== '' &&
+      candidates.secondCandidateYearInJst !== '' &&
+      candidates.secondCandidateMonthInJst !== '' &&
+      candidates.secondCandidateDayInJst !== '' &&
+      candidates.secondCandidateHourInJst !== '' &&
+      candidates.thirdCandidateYearInJst !== '' &&
+      candidates.thirdCandidateMonthInJst !== '' &&
+      candidates.thirdCandidateDayInJst !== '' &&
+      candidates.thirdCandidateHourInJst !== ''
+  })
+
+  const checkIfCandidatesAreSame = (year1: string, month1: string, day1: string, hour1: string, year2: string, month2: string, day2: string, hour2: string) => {
+    return year1 === year2 &&
+      month1 === month2 &&
+      day1 === day2 &&
+      hour1 === hour2
+  }
+  const sameCandidatesExist = computed(() => {
+    return checkIfCandidatesAreSame(candidates.firstCandidateYearInJst, candidates.firstCandidateMonthInJst, candidates.firstCandidateDayInJst, candidates.firstCandidateHourInJst, candidates.secondCandidateYearInJst, candidates.secondCandidateMonthInJst, candidates.secondCandidateDayInJst, candidates.secondCandidateHourInJst) ||
+      checkIfCandidatesAreSame(candidates.firstCandidateYearInJst, candidates.firstCandidateMonthInJst, candidates.firstCandidateDayInJst, candidates.firstCandidateHourInJst, candidates.thirdCandidateYearInJst, candidates.thirdCandidateMonthInJst, candidates.thirdCandidateDayInJst, candidates.thirdCandidateHourInJst) ||
+      checkIfCandidatesAreSame(candidates.thirdCandidateYearInJst, candidates.thirdCandidateMonthInJst, candidates.thirdCandidateDayInJst, candidates.thirdCandidateHourInJst, candidates.secondCandidateYearInJst, candidates.secondCandidateMonthInJst, candidates.secondCandidateDayInJst, candidates.secondCandidateHourInJst)
+  })
+
   return {
-    candidates
+    candidates,
+    allCandidatesAreNotEmpty,
+    sameCandidatesExist
   }
 }
