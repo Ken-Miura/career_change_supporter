95bb952a8b06458504ad81a987e2a4add406eb07
kmiura <ken.miura1102@gmail.com>
2022-12-03 16:34:04 +0000

    Add layout

--- client/user_app/src/views/personalized/ConsultationRequestDetailPage.vue
+++ client/user_app/src/views/personalized/ConsultationRequestDetailPage.vue
@@ -1,8 +1,26 @@
 <template>
   <TheHeader/>
   <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
-    <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 class="font-bold text-lg">{{ message }}, {{ consultationReqId }}</h3>
+    <div v-if="false" class="m-6">
+      <WaitingCircle />
+    </div>
+    <main v-else>
+      <div v-if="error.exists">
+        <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+          <AlertMessage class="mt-2" v-bind:message="error.message"/>
+        </div>
+      </div>
+      <div v-else>
+        <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+          <h3 class="font-bold text-2xl">相談申し込み詳細</h3>
+          <p class="mt-2 text-lg">詳細を確認し、相談申し込みを受けるかどうか選択して下さい。</p>
+          <div class="grid grid-cols-2 mt-4 ml-4">
+            <h3 class="text-xl justify-self-start col-span-1">ユーザーID</h3><h3 class="text-xl justify-self-start col-span-1">2</h3>
+            <div class="mt-3 justify-self-start col-span-1 text-xl">評価</div><div class="mt-3 justify-self-start col-span-1 text-xl"><span v-if="true"> 4.5</span><span v-else>0</span>/5（評価件数：21 件）</div>
+            <p class="mt-3 justify-self-start col-span-1 text-xl">支払われる相談料</p><p class="mt-3 justify-self-start col-span-1 text-xl">5000 円</p>
+          </div>
+        </div>
+      </div>
     </main>
     <footer class="max-w-lg mx-auto flex justify-center text-white">
       <router-link to="/" class="hover:underline">トップページへ</router-link>
@@ -11,9 +29,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, onMounted, reactive, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import TheHeader from '@/components/TheHeader.vue'
+import AlertMessage from '@/components/AlertMessage.vue'
+import WaitingCircle from '@/components/WaitingCircle.vue'
 import { refresh } from '@/util/personalized/refresh/Refresh'
 import { RefreshResp } from '@/util/personalized/refresh/RefreshResp'
 import { ApiErrorResp } from '@/util/ApiError'
@@ -22,10 +42,15 @@ import { Code } from '@/util/Error'
 export default defineComponent({
   name: 'ConsultationRequestDetailPage',
   components: {
-    TheHeader
+    TheHeader,
+    AlertMessage,
+    WaitingCircle
   },
   setup () {
-    const message = ref('相談申し込み詳細ページ')
+    const error = reactive({
+      exists: false,
+      message: ''
+    })
     const router = useRouter()
     const route = useRoute()
     const consultationReqId = route.params.consultation_req_id as string
@@ -51,7 +76,7 @@ export default defineComponent({
       }
       console.log('TODO: 実装後削除')
     })
-    return { message, consultationReqId }
+    return { error }
   }
 })
 </script>
