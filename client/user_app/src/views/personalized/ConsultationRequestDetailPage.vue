0f3ad5110a63e508eeb88fb0aa8f2a1aea84c7a9
kmiura <ken.miura1102@gmail.com>
2022-12-04 06:19:38 +0000

    Add layout

--- client/user_app/src/views/personalized/ConsultationRequestDetailPage.vue
+++ client/user_app/src/views/personalized/ConsultationRequestDetailPage.vue
@@ -37,6 +37,23 @@
               </div>
             </div>
           </div>
+          <div class="mt-4 ml-4 text-2xl justify-self-start col-span-6 pt-3">
+            <p>確認事項</p>
+            <p class="text-lg">相談申し込みを受け付けるためには、下記に記載の内容が正しいことを確認し、チェックをつけて下さい</p>
+          </div>
+          <div class="mt-2 ml-4 justify-self-start col-span-6 pt-2 rounded bg-gray-200">
+            <div class="m-4 text-xl grid grid-cols-6 justify-center items-center">
+              <div class="col-span-5">私は社外秘が何かを理解しおり、それを口外することはありません。</div>
+              <input type="checkbox" class="ml-5 col-span-1 justify-self-center bg-gray-200 rounded h-6 w-6 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500">
+            </div>
+          </div>
+          <div class="flex justify-center mt-8">
+            <button class="mr-10 bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200">相談申し込みを拒否する</button>
+            <button class="ml-10 bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200">相談申し込みを受ける</button>
+          </div>
+          <div v-if="errorBelowBtn.exists">
+            <AlertMessage class="mt-6" v-bind:message="errorBelowBtn.message"/>
+          </div>
         </div>
       </div>
     </main>
@@ -72,6 +89,10 @@ export default defineComponent({
     const router = useRouter()
     const route = useRoute()
     const consultationReqId = route.params.consultation_req_id as string
+    const errorBelowBtn = reactive({
+      exists: false,
+      message: ''
+    })
     onMounted(async () => {
       try {
         const resp = await refresh()
@@ -94,7 +115,7 @@ export default defineComponent({
       }
       console.log('TODO: 実装後削除')
     })
-    return { error }
+    return { error, errorBelowBtn }
   }
 })
 </script>
