201903630d30f2ea46dae4a190893c7d1f27b997
kmiura <ken.miura1102@gmail.com>
2023-01-03 08:01:49 +0000

    Improve error message generation

--- client/admin_app/src/util/Error.ts
+++ client/admin_app/src/util/Error.ts
@@ -23,35 +23,35 @@ export namespace Code {
 }
 
 export function createErrorMessage (code: number): string {
-  if (code === Code.UNEXPECTED_ERR_COMMON || code === Code.UNEXPECTED_ERR_ADMIN) {
-    return `${Message.UNEXPECTED_ERR} (${code})`
-  } else if (code === Code.INVALID_EMAIL_ADDRESS_FORMAT) {
-    return `${Message.INVALID_EMAIL_ADDRESS_FORMAT_MESSAGE} (${code})`
-  } else if (code === Code.INVALID_PASSWORD_FORMAT) {
-    return `${Message.INVALID_PASSWORD_FORMAT_MESSAGE} (${code})`
-  } else if (code === Code.INVALID_UUID_FORMAT) {
-    return `${Message.INVALID_UUID_FORMAT_MESSAGE} (${code})`
-  } else if (code === Code.EMAIL_OR_PWD_INCORRECT) {
-    return `${Message.EMAIL_OR_PWD_INCORRECT_MESSAGE} (${code})`
-  } else if (code === Code.UNAUTHORIZED) {
-    return `${Message.UNAUTHORIZED_MESSAGE} (${code})`
-  } else if (code === Code.ILLEGAL_PAGE_SIZE) {
-    return `${Message.ILLEGAL_PAGE_SIZE_MESSAGE} (${code})`
-  } else if (code === Code.NO_CREATE_IDENTITY_REQ_DETAIL_FOUND) {
-    return `${Message.NO_CREATE_IDENTITY_REQ_DETAIL_FOUND_MESSAGE} (${code})`
-  } else if (code === Code.ILLEGAL_DATE) {
-    return `${Message.ILLEGAL_DATE_MESSAGE} (${code})`
-  } else if (code === Code.INVALID_FORMAT_REASON) {
-    return `${Message.INVALID_FORMAT_REASON_MESSAGE} (${code})`
-  } else if (code === Code.NO_UPDATE_IDENTITY_REQ_DETAIL_FOUND) {
-    return `${Message.NO_UPDATE_IDENTITY_REQ_DETAIL_FOUND_MESSAGE} (${code})`
-  } else if (code === Code.NO_USER_ACCOUNT_FOUND) {
-    return `${Message.NO_USER_ACCOUNT_FOUND_MESSAGE} (${code})`
-  } else if (code === Code.NO_IDENTITY_FOUND) {
-    return `${Message.NO_IDENTITY_FOUND_MESSAGE} (${code})`
-  } else if (code === Code.NO_CREATE_CAREER_REQ_DETAIL_FOUND) {
-    return `${Message.NO_CREATE_CAREER_REQ_DETAIL_FOUND_MESSAGE} (${code})`
-  } else {
+  // mapのキーとしてcodeを利用してる。そのキー同士の比較に === が使われるため、浮動少数（number）を使わないようにstringに変換する。
+  // code同士の加減乗除は行っておらず、codeに利用する数字は整数のみのため、問題は発生しないと思われる。
+  // しかし、一般論として浮動少数の等価比較（===）は避けるべきと考えられるため、上記の対応を行う。
+  const codeStr = code.toString()
+  const result = createErrorMessageInternal(codeStr)
+  return result
+}
+
+export function createErrorMessageInternal (code: string): string {
+  const message = codeToMessage.get(code)
+  if (!message) {
     throw new Error(`unexpected code: ${code}`)
   }
+  return message
 }
+
+const codeToMessage = new Map<string, string>()
+codeToMessage.set(Code.UNEXPECTED_ERR_COMMON.toString(), `${Message.UNEXPECTED_ERR} (${Code.UNEXPECTED_ERR_COMMON})`)
+codeToMessage.set(Code.UNEXPECTED_ERR_ADMIN.toString(), `${Message.UNEXPECTED_ERR} (${Code.UNEXPECTED_ERR_ADMIN})`)
+codeToMessage.set(Code.INVALID_EMAIL_ADDRESS_FORMAT.toString(), `${Message.INVALID_EMAIL_ADDRESS_FORMAT_MESSAGE} (${Code.INVALID_EMAIL_ADDRESS_FORMAT})`)
+codeToMessage.set(Code.INVALID_PASSWORD_FORMAT.toString(), `${Message.INVALID_PASSWORD_FORMAT_MESSAGE} (${Code.INVALID_PASSWORD_FORMAT})`)
+codeToMessage.set(Code.INVALID_UUID_FORMAT.toString(), `${Message.INVALID_UUID_FORMAT_MESSAGE} (${Code.INVALID_UUID_FORMAT})`)
+codeToMessage.set(Code.EMAIL_OR_PWD_INCORRECT.toString(), `${Message.EMAIL_OR_PWD_INCORRECT_MESSAGE} (${Code.EMAIL_OR_PWD_INCORRECT})`)
+codeToMessage.set(Code.UNAUTHORIZED.toString(), `${Message.UNAUTHORIZED_MESSAGE} (${Code.UNAUTHORIZED})`)
+codeToMessage.set(Code.ILLEGAL_PAGE_SIZE.toString(), `${Message.ILLEGAL_PAGE_SIZE_MESSAGE} (${Code.ILLEGAL_PAGE_SIZE})`)
+codeToMessage.set(Code.NO_CREATE_IDENTITY_REQ_DETAIL_FOUND.toString(), `${Message.NO_CREATE_IDENTITY_REQ_DETAIL_FOUND_MESSAGE} (${Code.NO_CREATE_IDENTITY_REQ_DETAIL_FOUND})`)
+codeToMessage.set(Code.ILLEGAL_DATE.toString(), `${Message.ILLEGAL_DATE_MESSAGE} (${Code.ILLEGAL_DATE})`)
+codeToMessage.set(Code.INVALID_FORMAT_REASON.toString(), `${Message.INVALID_FORMAT_REASON_MESSAGE} (${Code.INVALID_FORMAT_REASON})`)
+codeToMessage.set(Code.NO_UPDATE_IDENTITY_REQ_DETAIL_FOUND.toString(), `${Message.NO_UPDATE_IDENTITY_REQ_DETAIL_FOUND_MESSAGE} (${Code.NO_UPDATE_IDENTITY_REQ_DETAIL_FOUND})`)
+codeToMessage.set(Code.NO_USER_ACCOUNT_FOUND.toString(), `${Message.NO_USER_ACCOUNT_FOUND_MESSAGE} (${Code.NO_USER_ACCOUNT_FOUND})`)
+codeToMessage.set(Code.NO_IDENTITY_FOUND.toString(), `${Message.NO_IDENTITY_FOUND_MESSAGE} (${Code.NO_IDENTITY_FOUND})`)
+codeToMessage.set(Code.NO_CREATE_CAREER_REQ_DETAIL_FOUND.toString(), `${Message.NO_CREATE_CAREER_REQ_DETAIL_FOUND_MESSAGE} (${Code.NO_CREATE_CAREER_REQ_DETAIL_FOUND})`)
