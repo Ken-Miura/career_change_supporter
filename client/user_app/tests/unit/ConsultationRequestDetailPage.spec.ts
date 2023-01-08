27f8b0fd36f544c74a78bd254fd7c1e831d88927
kmiura <ken.miura1102@gmail.com>
2023-01-08 14:21:07 +0000

    Add test

--- client/user_app/tests/unit/ConsultationRequestDetailPage.spec.ts
+++ client/user_app/tests/unit/ConsultationRequestDetailPage.spec.ts
@@ -211,6 +211,23 @@ describe('ConsultationRequestDetailPage.vue', () => {
     expect(errorMessage.text()).toContain(Code.NON_POSITIVE_CONSULTATION_REQ_ID.toString())
   })
 
+  it(`displays ${Message.NO_IDENTITY_REGISTERED_MESSAGE} if refresh returns ${Code.NO_IDENTITY_REGISTERED}`, async () => {
+    const apiErrResp = ApiErrorResp.create(400, ApiError.create(Code.NO_IDENTITY_REGISTERED))
+    getConsultationRequestDetailFuncMock.mockResolvedValue(apiErrResp)
+    const wrapper = mount(ConsultationRequestDetailPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const errorMessage = wrapper.find('[data-test="error-message"]')
+    expect(errorMessage.text()).toContain(Message.NO_IDENTITY_REGISTERED_MESSAGE)
+    expect(errorMessage.text()).toContain(Code.NO_IDENTITY_REGISTERED.toString())
+  })
+
   it('displays consultation request detail case 1', async () => {
     const result = createDummyConsultationRequestDetail1(parseInt(routeParam))
     const resp = GetConsultationRequestDetailResp.create(result)
