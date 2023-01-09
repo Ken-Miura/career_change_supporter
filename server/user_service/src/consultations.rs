acceadbbca9f36b2bdd45203bccdac5c4e4722c1
kmiura <ken.miura1102@gmail.com>
2023-01-09 13:29:46 +0000

    Add impl

--- server/user_service/src/consultations.rs
+++ server/user_service/src/consultations.rs
@@ -6,7 +6,7 @@ use common::{RespResult, JAPANESE_TIME_ZONE};
 use entity::sea_orm::DatabaseConnection;
 use serde::Serialize;
 
-use crate::util::session::User;
+use crate::util::{consultation::ConsultationDateTime, session::User};
 
 pub(crate) async fn get_consultations(
     User { account_id }: User,
@@ -17,4 +17,23 @@ pub(crate) async fn get_consultations(
 }
 
 #[derive(Clone, Debug, Serialize, PartialEq)]
-pub(crate) struct ConsultationsResult {}
+pub(crate) struct ConsultationsResult {
+    pub(crate) user_side_consultations: Vec<UserSideConsultation>,
+    pub(crate) consultant_side_consultations: Vec<ConsultantSideConsultation>,
+}
+
+/// 相談申し込み者として行う相談
+#[derive(Clone, Debug, Serialize, PartialEq)]
+pub(crate) struct UserSideConsultation {
+    pub(crate) consultation_id: i64,
+    pub(crate) consultant_id: i64, // 相談相手のユーザーID
+    pub(crate) meeting_date_time_in_jst: ConsultationDateTime,
+}
+
+/// 相談相手として行う相談
+#[derive(Clone, Debug, Serialize, PartialEq)]
+pub(crate) struct ConsultantSideConsultation {
+    pub(crate) consultation_id: i64,
+    pub(crate) user_account_id: i64, // 相談申し込み者のユーザーID
+    pub(crate) meeting_date_time_in_jst: ConsultationDateTime,
+}
