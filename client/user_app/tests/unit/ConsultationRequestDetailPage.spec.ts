e6bb0d4e3936cdcfc87c0f5415ad8a8912badc0d
kmiura <ken.miura1102@gmail.com>
2023-01-09 08:18:33 +0000

    Add test

--- client/user_app/tests/unit/ConsultationRequestDetailPage.spec.ts
+++ client/user_app/tests/unit/ConsultationRequestDetailPage.spec.ts
@@ -1002,4 +1002,74 @@ describe('ConsultationRequestDetailPage.vue', () => {
     expect(errorBelowBtn.text()).toContain(Message.INVALID_CANDIDATE_MESSAGE)
     expect(errorBelowBtn.text()).toContain(Code.INVALID_CANDIDATE.toString())
   })
+
+  it(`displays ${Message.USER_DOES_NOT_CHECK_CONFIRMATION_ITEMS_MESSAGE} if ${Code.USER_DOES_NOT_CHECK_CONFIRMATION_ITEMS} is returned`, async () => {
+    const result = createDummyConsultationRequestDetail1(parseInt(routeParam))
+    const resp = GetConsultationRequestDetailResp.create(result)
+    getConsultationRequestDetailFuncMock.mockResolvedValue(resp)
+    const wrapper = mount(ConsultationRequestDetailPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const thirdCandidate = wrapper.find('[data-test="third-candidate"]')
+    thirdCandidate.setValue(true)
+    const userChecked = wrapper.find('[data-test="user-checked"]')
+    userChecked.setValue(true)
+    await flushPromises()
+
+    const apiErrResp = ApiErrorResp.create(400, ApiError.create(Code.USER_DOES_NOT_CHECK_CONFIRMATION_ITEMS))
+    postConsultationRequestAcceptanceFuncMock.mockResolvedValue(apiErrResp)
+    const acceptBtn = wrapper.find('[data-test="accept-btn"]')
+    await acceptBtn.trigger('click')
+    await flushPromises()
+
+    expect(postConsultationRequestAcceptanceFuncMock).toHaveBeenCalledTimes(1)
+    const data = JSON.parse(`{"consultation_req_id": ${result.consultation_req_id}, "picked_candidate": 3, "user_checked": true}`)
+    expect(postConsultationRequestAcceptanceFuncMock).toHaveBeenCalledWith(data)
+    expect(routerPushMock).toHaveBeenCalledTimes(0)
+    const errorBelowBtn = wrapper.find('[data-test="error-below-btn"]')
+    expect(errorBelowBtn.exists()).toBe(true)
+    expect(errorBelowBtn.text()).toContain(Message.USER_DOES_NOT_CHECK_CONFIRMATION_ITEMS_MESSAGE)
+    expect(errorBelowBtn.text()).toContain(Code.USER_DOES_NOT_CHECK_CONFIRMATION_ITEMS.toString())
+  })
+
+  it(`displays ${Message.NON_POSITIVE_CONSULTATION_REQ_ID_MESSAGE} if ${Code.NON_POSITIVE_CONSULTATION_REQ_ID} is returned`, async () => {
+    const result = createDummyConsultationRequestDetail1(parseInt(routeParam))
+    const resp = GetConsultationRequestDetailResp.create(result)
+    getConsultationRequestDetailFuncMock.mockResolvedValue(resp)
+    const wrapper = mount(ConsultationRequestDetailPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const thirdCandidate = wrapper.find('[data-test="third-candidate"]')
+    thirdCandidate.setValue(true)
+    const userChecked = wrapper.find('[data-test="user-checked"]')
+    userChecked.setValue(true)
+    await flushPromises()
+
+    const apiErrResp = ApiErrorResp.create(400, ApiError.create(Code.NON_POSITIVE_CONSULTATION_REQ_ID))
+    postConsultationRequestAcceptanceFuncMock.mockResolvedValue(apiErrResp)
+    const acceptBtn = wrapper.find('[data-test="accept-btn"]')
+    await acceptBtn.trigger('click')
+    await flushPromises()
+
+    expect(postConsultationRequestAcceptanceFuncMock).toHaveBeenCalledTimes(1)
+    const data = JSON.parse(`{"consultation_req_id": ${result.consultation_req_id}, "picked_candidate": 3, "user_checked": true}`)
+    expect(postConsultationRequestAcceptanceFuncMock).toHaveBeenCalledWith(data)
+    expect(routerPushMock).toHaveBeenCalledTimes(0)
+    const errorBelowBtn = wrapper.find('[data-test="error-below-btn"]')
+    expect(errorBelowBtn.exists()).toBe(true)
+    expect(errorBelowBtn.text()).toContain(Message.NON_POSITIVE_CONSULTATION_REQ_ID_MESSAGE)
+    expect(errorBelowBtn.text()).toContain(Code.NON_POSITIVE_CONSULTATION_REQ_ID.toString())
+  })
 })
