22d26234be8e090f1a81b15cc16984d1a518b712
kmiura <ken.miura1102@gmail.com>
2023-01-22 14:39:36 +0000

    Change constant

--- server/user_service/src/consultation_room.rs
+++ server/user_service/src/consultation_room.rs
@@ -43,8 +43,10 @@ static SKY_WAY_SECRET_KEY: Lazy<String> = Lazy::new(|| {
 /// 600 - 90000 の間を設定する必要がある
 /// https://github.com/skyway/skyway-peer-authentication-samples/blob/master/README.jp.md#ttl
 ///
-/// 相談時間（[LENGTH_OF_MEETING_IN_MINUTE]分）+ 余裕（20分）を設定し、必ず相談時間中にCredentialの期限が切れないようにする。
-const SKY_WAY_CREDENTIAL_TTL_IN_SECONDS: u32 = 60 * (LENGTH_OF_MEETING_IN_MINUTE as u32 + 20);
+/// 相談時間（[LENGTH_OF_MEETING_IN_MINUTE]分）+ 相談開始時刻前から入室可能な分の余裕（[TIME_BEFORE_CONSULTATION_STARTS_IN_MINUTES]分) + 余裕（5分）を設定し、
+/// 必ず相談時間中にCredentialの期限が切れないようにする。
+const SKY_WAY_CREDENTIAL_TTL_IN_SECONDS: u32 = 60
+    * (LENGTH_OF_MEETING_IN_MINUTE as u32 + TIME_BEFORE_CONSULTATION_STARTS_IN_MINUTES as u32 + 5);
 
 /// SkyWayでPeer認証に用いるCredential
 ///
