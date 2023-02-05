b8a3d1329a097daa4dc98558b8b732ba5a232314
kmiura <ken.miura1102@gmail.com>
2023-02-05 14:17:33 +0000

    Fix issue

--- client/user_app/src/util/personalized/profile/useGetProfile.ts
+++ client/user_app/src/util/personalized/profile/useGetProfile.ts
@@ -3,9 +3,10 @@ import { ref } from 'vue'
 
 // eslint-disable-next-line
 export function useGetProfile () {
-  const getProfileDone = ref(false)
+  const getProfileDone = ref(true)
   const getProfileFunc = async () => {
     try {
+      getProfileDone.value = false
       const response = await getProfile()
       return response
     } finally {
