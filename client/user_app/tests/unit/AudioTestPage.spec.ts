4d45c579c8996d4d7aeff036c6a909d2ae612f67
kmiura <ken.miura1102@gmail.com>
2023-02-06 14:44:42 +0000

    Add test

---
+++ client/user_app/tests/unit/AudioTestPage.spec.ts
@@ -0,0 +1,9 @@
+describe('AudioTestPage.vue', () => {
+  beforeEach(() => {
+    console.log('test')
+  })
+
+  it('has WaitingCircle and TheHeader while waiting response', async () => {
+    console.log('test')
+  })
+})
