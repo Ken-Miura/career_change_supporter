ffcc10f377d53529657dbd1ca89a698385b82970
kmiura <ken.miura1102@gmail.com>
2023-02-06 15:19:29 +0000

    Add mock

--- client/user_app/tests/unit/AudioTestPage.spec.ts
+++ client/user_app/tests/unit/AudioTestPage.spec.ts
@@ -1,7 +1,6 @@
 import { ref } from 'vue'
-
-// getAudioMediaStream mock
-// generatePitchFactor mock
+import { getAudioMediaStream } from '@/util/personalized/AudioMediaStream'
+import { generatePitchFactor } from '@/util/personalized/audio-test/PitchFacter'
 
 const refreshDoneMock = ref(true)
 const refreshFuncMock = jest.fn()
@@ -12,10 +11,18 @@ jest.mock('@/util/personalized/refresh/useRefresh', () => ({
   })
 }))
 
+jest.mock('@/util/personalized/AudioMediaStream')
+const getAudioMediaStreamMock = getAudioMediaStream as jest.MockedFunction<typeof getAudioMediaStream>
+
+jest.mock('@/util/personalized/audio-test/PitchFacter')
+const generatePitchFactorMock = generatePitchFactor as jest.MockedFunction<typeof generatePitchFactor>
+
 describe('AudioTestPage.vue', () => {
   beforeEach(() => {
     refreshDoneMock.value = true
     refreshFuncMock.mockReset()
+    getAudioMediaStreamMock.mockReset()
+    generatePitchFactorMock.mockReset()
   })
 
   it('has WaitingCircle and TheHeader while waiting response', async () => {
