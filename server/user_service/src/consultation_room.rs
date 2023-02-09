1b2a9b1edec3b85e0c2cd52994e5bf6b89929318
kmiura <ken.miura1102@gmail.com>
2023-02-09 13:33:59 +0000

    Add JWT payload struct

--- server/user_service/src/consultation_room.rs
+++ server/user_service/src/consultation_room.rs
@@ -47,12 +47,15 @@ static SKY_WAY_SECRET_KEY: Lazy<String> = Lazy::new(|| {
     })
 });
 
-// JWTのペイロード
+// クライアントがSkay WayにアクセスするためのJWTのペイロード部分を表す構造体
+// このサービスに必要な分のメンバーのみを定義する
+//
+// https://skyway.ntt.com/ja/docs/user-guide/authentication/
 #[derive(Clone, Debug, Serialize, PartialEq)]
 struct SkyWayAuthToken {
-    jti: String, // UUID v4 => 6668affc-5afa-4996-b65a-6afe2f72756b みたいなハイフンあり形式
-    iat: i64,    // DateTime<FixedOffset>.timestamp()で取れる値
-    exp: i64,    // DateTime<FixedOffset>.timestamp()で取れる値
+    jti: String, // UUID V4（6668affc-5afa-4996-b65a-6afe2f72756b のようなハイフン有り形式）
+    iat: i64,    // 秒単位のタイムスタンプ（DateTime<FixedOffset>.timestamp()で取得できる値）
+    exp: i64,    // 秒単位のタイムスタンプ（DateTime<FixedOffset>.timestamp()で取得できる値）
     scope: SkyWayScope,
 }
 
@@ -63,7 +66,36 @@ struct SkyWayScope {
 
 #[derive(Clone, Debug, Serialize, PartialEq)]
 struct SkyWayAppScope {
-    id: String, // アプリケーションID
+    id: String,           // アプリケーションID
+    actions: Vec<String>, // 使える値はreadのみ
+    channels: Vec<SkyWayChannelScope>,
+}
+
+#[derive(Clone, Debug, Serialize, PartialEq)]
+struct SkyWayChannelScope {
+    name: String, // idまたはnameのどちらかの指定が必須。このサービスではnameを指定する
+    actions: Vec<String>, // 使える値はwrite, read, create, delete, updateMetadataの5つ。このサービスでは必要なread, create, deleteのみを指定する。
+    members: Vec<SkyWayMemberScope>,
+}
+
+#[derive(Clone, Debug, Serialize, PartialEq)]
+struct SkyWayMemberScope {
+    name: String, // idまたはnameのどちらかの指定が必須。このサービスではnameを指定する
+    actions: Vec<String>, // 使える値はwrite, create, delete, updateMetadata, signalの5つ。このサービスでは必要なcreate, delete, signalのみを指定する。
+    publication: SkyWayPublicationScope,
+    subscription: SkyWaySubscriptionScope,
+}
+
+#[derive(Clone, Debug, Serialize, PartialEq)]
+struct SkyWayPublicationScope {
+    actions: Vec<String>, // 使える値はwrite, create, delete, updateMetadata, enable, disableの6つ。このサービスでは必要なcreate, deleteのみを指定する。
+}
+
+#[derive(Clone, Debug, Serialize, PartialEq)]
+struct SkyWaySubscriptionScope {
+    // 使える値はwrite, create, deleteの3つ。このサービスではcreate, deleteを指定する。
+    // （2023年2月時点では、このリソースに関してcreate, deleteを指定することはwriteと同じだが、他と合わせるためにcreate, deleteを指定）
+    actions: Vec<String>,
 }
 
 // ここより下は旧SkyWay用の必要ないコード
