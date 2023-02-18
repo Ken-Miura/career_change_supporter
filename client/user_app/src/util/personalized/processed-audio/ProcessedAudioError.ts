2b6d6cd656f4de545942c2be2070c638c54fa6da
kmiura <ken.miura1102@gmail.com>
2023-02-18 10:34:33 +0000

    Add workaround

--- client/user_app/src/util/personalized/processed-audio/ProcessedAudioError.ts
+++ client/user_app/src/util/personalized/processed-audio/ProcessedAudioError.ts
@@ -1,2 +1,9 @@
 export class ProcessedAudioError extends Error {
+  constructor (message: string) {
+    super(message)
+    this.name = 'ProcessedAudioError'
+    // instanceofを正しく動作させるためのワークアラウンド
+    // https://blog.n-t.jp/post/tech/typescript-custom-error/
+    Object.setPrototypeOf(this, ProcessedAudioError.prototype)
+  }
 }
--- client/user_app/src/util/personalized/skyway/SkyWayOriginatedError.ts
+++ client/user_app/src/util/personalized/skyway/SkyWayOriginatedError.ts
@@ -1,3 +1,9 @@
 export class SkyWayOriginatedError extends Error {
-
+  constructor (message: string) {
+    super(message)
+    this.name = 'SkyWayOriginatedError'
+    // instanceofを正しく動作させるためのワークアラウンド
+    // https://blog.n-t.jp/post/tech/typescript-custom-error/
+    Object.setPrototypeOf(this, SkyWayOriginatedError.prototype)
+  }
 }
