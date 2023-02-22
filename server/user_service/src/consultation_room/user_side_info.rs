49f658e78606fd67ba818824c01de8aea53066a6
kmiura <ken.miura1102@gmail.com>
2023-02-22 16:28:09 +0000

    Add test

--- server/user_service/src/consultation_room/user_side_info.rs
+++ server/user_service/src/consultation_room/user_side_info.rs
@@ -510,6 +510,49 @@ mod tests {
                     }),
                 )),
             },
+            TestCase {
+                name: "success case 3 (current_date_time passed consultation_date_time_in_jst)"
+                    .to_string(),
+                input: Input {
+                    account_id: account_id_of_user,
+                    consultation_id,
+                    current_date_time: *CURRENT_DATE_TIME,
+                    identification: SkyWayIdentification {
+                        application_id: DUMMY_APPLICATION_ID.to_string(),
+                        secret: DUMMY_SECRET.to_string(),
+                    },
+                    token_id: TOKEN_ID.to_string(),
+                    audio_test_done: true,
+                    op: UserSideInfoOperationMock {
+                        account_id: account_id_of_user,
+                        consultation_id,
+                        consultation: Consultation {
+                            user_account_id: account_id_of_user,
+                            consultant_id: account_id_of_consultant,
+                            consultation_date_time_in_jst: *CURRENT_DATE_TIME
+                                - Duration::minutes(10), // 現在時刻が相談開始時刻を過ぎていることを表したいだけで10分は適当な数字
+                            room_name: ROOM_NAME.to_string(),
+                        },
+                        consultant: UserAccount {
+                            email_address: consultant_email_address.to_string(),
+                            disabled_at: None,
+                        },
+                        user_account: UserAccount {
+                            email_address: user_account_email_address.to_string(),
+                            disabled_at: None,
+                        },
+                        current_date_time: *CURRENT_DATE_TIME,
+                    },
+                },
+                expected: Ok((
+                    StatusCode::OK,
+                    Json(UserSideInfoResult {
+                        token: TOKEN.to_string(),
+                        room_name: ROOM_NAME.to_string(),
+                        member_name: MEMBER_NAME.to_string(),
+                    }),
+                )),
+            },
         ]
     });
 
