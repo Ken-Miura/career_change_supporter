a50a2e9bb94610abf625ec2dcffe991df506a048
kmiura <ken.miura1102@gmail.com>
2023-02-22 14:09:02 +0000

    Add test case

--- server/user_service/src/consultation_room/consultant_side_info.rs
+++ server/user_service/src/consultation_room/consultant_side_info.rs
@@ -427,47 +427,134 @@ mod tests {
             *CURRENT_DATE_TIME + Duration::minutes(LEEWAY_IN_MINUTES); // LEEWAY_IN_MINUTES分前丁度はミーティングルームへ入れる
         let consultant_email_address = "test0@test.com";
         let user_account_email_address = "test1@test.com";
-        vec![TestCase {
-            name: "success case".to_string(),
-            input: Input {
-                account_id: account_id_of_consultant,
-                consultation_id,
-                current_date_time: *CURRENT_DATE_TIME,
-                identification: SkyWayIdentification {
-                    application_id: DUMMY_APPLICATION_ID.to_string(),
-                    secret: DUMMY_SECRET.to_string(),
-                },
-                token_id: TOKEN_ID.to_string(),
-                audio_test_done: true,
-                op: ConsultantSideInfoOperationMock {
+        vec![
+            TestCase {
+                name: "success case 1".to_string(),
+                input: Input {
                     account_id: account_id_of_consultant,
                     consultation_id,
-                    consultation: Consultation {
-                        user_account_id: account_id_of_user,
-                        consultant_id: account_id_of_consultant,
-                        consultation_date_time_in_jst,
-                        room_name: ROOM_NAME.to_string(),
-                    },
-                    consultant: UserAccount {
-                        email_address: consultant_email_address.to_string(),
-                        disabled_at: None,
-                    },
-                    user_account: UserAccount {
-                        email_address: user_account_email_address.to_string(),
-                        disabled_at: None,
-                    },
                     current_date_time: *CURRENT_DATE_TIME,
+                    identification: SkyWayIdentification {
+                        application_id: DUMMY_APPLICATION_ID.to_string(),
+                        secret: DUMMY_SECRET.to_string(),
+                    },
+                    token_id: TOKEN_ID.to_string(),
+                    audio_test_done: true,
+                    op: ConsultantSideInfoOperationMock {
+                        account_id: account_id_of_consultant,
+                        consultation_id,
+                        consultation: Consultation {
+                            user_account_id: account_id_of_user,
+                            consultant_id: account_id_of_consultant,
+                            consultation_date_time_in_jst,
+                            room_name: ROOM_NAME.to_string(),
+                        },
+                        consultant: UserAccount {
+                            email_address: consultant_email_address.to_string(),
+                            disabled_at: None,
+                        },
+                        user_account: UserAccount {
+                            email_address: user_account_email_address.to_string(),
+                            disabled_at: None,
+                        },
+                        current_date_time: *CURRENT_DATE_TIME,
+                    },
                 },
+                expected: Ok((
+                    StatusCode::OK,
+                    Json(ConsultantSideInfoResult {
+                        token: TOKEN.to_string(),
+                        room_name: ROOM_NAME.to_string(),
+                        member_name: MEMBER_NAME.to_string(),
+                    }),
+                )),
             },
-            expected: Ok((
-                StatusCode::OK,
-                Json(ConsultantSideInfoResult {
-                    token: TOKEN.to_string(),
-                    room_name: ROOM_NAME.to_string(),
-                    member_name: MEMBER_NAME.to_string(),
-                }),
-            )),
-        }]
+            TestCase {
+                name: "success case 2 (consultation_date_time_in_jst is same as current_date_time)"
+                    .to_string(),
+                input: Input {
+                    account_id: account_id_of_consultant,
+                    consultation_id,
+                    current_date_time: *CURRENT_DATE_TIME,
+                    identification: SkyWayIdentification {
+                        application_id: DUMMY_APPLICATION_ID.to_string(),
+                        secret: DUMMY_SECRET.to_string(),
+                    },
+                    token_id: TOKEN_ID.to_string(),
+                    audio_test_done: true,
+                    op: ConsultantSideInfoOperationMock {
+                        account_id: account_id_of_consultant,
+                        consultation_id,
+                        consultation: Consultation {
+                            user_account_id: account_id_of_user,
+                            consultant_id: account_id_of_consultant,
+                            consultation_date_time_in_jst: *CURRENT_DATE_TIME,
+                            room_name: ROOM_NAME.to_string(),
+                        },
+                        consultant: UserAccount {
+                            email_address: consultant_email_address.to_string(),
+                            disabled_at: None,
+                        },
+                        user_account: UserAccount {
+                            email_address: user_account_email_address.to_string(),
+                            disabled_at: None,
+                        },
+                        current_date_time: *CURRENT_DATE_TIME,
+                    },
+                },
+                expected: Ok((
+                    StatusCode::OK,
+                    Json(ConsultantSideInfoResult {
+                        token: TOKEN.to_string(),
+                        room_name: ROOM_NAME.to_string(),
+                        member_name: MEMBER_NAME.to_string(),
+                    }),
+                )),
+            },
+            TestCase {
+                name: "success case 3 (current_date_time passed consultation_date_time_in_jst)"
+                    .to_string(),
+                input: Input {
+                    account_id: account_id_of_consultant,
+                    consultation_id,
+                    current_date_time: *CURRENT_DATE_TIME,
+                    identification: SkyWayIdentification {
+                        application_id: DUMMY_APPLICATION_ID.to_string(),
+                        secret: DUMMY_SECRET.to_string(),
+                    },
+                    token_id: TOKEN_ID.to_string(),
+                    audio_test_done: true,
+                    op: ConsultantSideInfoOperationMock {
+                        account_id: account_id_of_consultant,
+                        consultation_id,
+                        consultation: Consultation {
+                            user_account_id: account_id_of_user,
+                            consultant_id: account_id_of_consultant,
+                            consultation_date_time_in_jst: *CURRENT_DATE_TIME
+                                - Duration::minutes(10), // 現在時刻が相談開始時刻を過ぎていることを表したいだけで10分は適当な数字
+                            room_name: ROOM_NAME.to_string(),
+                        },
+                        consultant: UserAccount {
+                            email_address: consultant_email_address.to_string(),
+                            disabled_at: None,
+                        },
+                        user_account: UserAccount {
+                            email_address: user_account_email_address.to_string(),
+                            disabled_at: None,
+                        },
+                        current_date_time: *CURRENT_DATE_TIME,
+                    },
+                },
+                expected: Ok((
+                    StatusCode::OK,
+                    Json(ConsultantSideInfoResult {
+                        token: TOKEN.to_string(),
+                        room_name: ROOM_NAME.to_string(),
+                        member_name: MEMBER_NAME.to_string(),
+                    }),
+                )),
+            },
+        ]
     });
 
     #[tokio::test]
