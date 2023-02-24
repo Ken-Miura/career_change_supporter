f40244a6cfc88615dad1fe6be8f4f49ad29883c1
kmiura <ken.miura1102@gmail.com>
2023-02-24 15:38:58 +0000

    Add impl

--- client/user_app/src/views/personalized/AwaitingRatingListPage.vue
+++ client/user_app/src/views/personalized/AwaitingRatingListPage.vue
@@ -2,7 +2,9 @@
   <TheHeader/>
   <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
     <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 class="font-bold text-lg">{{ message }}</h3>
+      <h3 class="font-bold text-lg">getAwaitingRatingsDone: {{ getAwaitingRatingsDone }}</h3>
+      <h3 class="font-bold text-lg">errMessage: {{ errMessage }}</h3>
+      <h3 class="font-bold text-lg">awaitingRatings: {{ awaitingRatings }}</h3>
     </main>
     <footer class="max-w-lg mx-auto flex justify-center text-white">
       <router-link to="/" class="hover:underline">トップページへ</router-link>
@@ -14,26 +16,34 @@
 import { defineComponent, onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import TheHeader from '@/components/TheHeader.vue'
-import { refresh } from '@/util/personalized/refresh/Refresh'
-import { RefreshResp } from '@/util/personalized/refresh/RefreshResp'
+import { useGetAwaitingRatings } from '@/util/personalized/awaiting-rating-list/useGetAwaitingRatings'
 import { ApiErrorResp } from '@/util/ApiError'
-import { Code } from '@/util/Error'
+import { Code, createErrorMessage } from '@/util/Error'
+import { AwaitingRatingsResp } from '@/util/personalized/awaiting-rating-list/AwaitingRatingsResp'
+import { Message } from '@/util/Message'
+import { AwaitingRatings } from '@/util/personalized/awaiting-rating-list/AwaitingRatings'
 
 export default defineComponent({
-  name: 'RatingWaitListPage',
+  name: 'AwaitingRatingListPage',
   components: {
     TheHeader
   },
   setup () {
-    const message = ref('評価待ちリスト用テストページ')
+    const errMessage = ref(null as string | null)
     const router = useRouter()
+    const {
+      getAwaitingRatingsDone,
+      getAwaitingRatingsFunc
+    } = useGetAwaitingRatings()
+    const awaitingRatings = ref(null as AwaitingRatings | null)
+
     onMounted(async () => {
       try {
-        const resp = await refresh()
-        if (resp instanceof RefreshResp) {
-          // セッションが存在し、利用規約に同意済のため、ログイン後のページを表示可能
-          // TODO: 正常系の処理
-        } else if (resp instanceof ApiErrorResp) {
+        const resp = await getAwaitingRatingsFunc()
+        if (!(resp instanceof AwaitingRatingsResp)) {
+          if (!(resp instanceof ApiErrorResp)) {
+            throw new Error(`unexpected result on getting request detail: ${resp}`)
+          }
           const code = resp.getApiError().getCode()
           if (code === Code.UNAUTHORIZED) {
             await router.push('/login')
@@ -42,14 +52,19 @@ export default defineComponent({
             await router.push('/terms-of-use')
             return
           }
-          // TODO: エラー処理
+          errMessage.value = createErrorMessage(resp.getApiError().getCode())
+          return
         }
+        awaitingRatings.value = resp.getAwaitingRatings()
       } catch (e) {
-        // TODO: エラー処理
+        errMessage.value = `${Message.UNEXPECTED_ERR}: ${e}`
       }
-      console.log('TODO: 実装後削除')
     })
-    return { message }
+    return {
+      getAwaitingRatingsDone,
+      errMessage,
+      awaitingRatings
+    }
   }
 })
 </script>
