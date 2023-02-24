7fe0288b91baf1bae17b37c744aa27e0d2a931c4
kmiura <ken.miura1102@gmail.com>
2023-02-24 14:25:20 +0000

    Add impl

--- server/user_service/src/rating/awaiting_ratings.rs
+++ server/user_service/src/rating/awaiting_ratings.rs
@@ -2,12 +2,21 @@
 
 use axum::async_trait;
 use axum::extract::State;
-use chrono::{DateTime, FixedOffset, Utc};
+use chrono::{DateTime, Datelike, FixedOffset, Timelike, Utc};
 use common::{ErrResp, RespResult, JAPANESE_TIME_ZONE};
-use entity::sea_orm::DatabaseConnection;
+use entity::{
+    sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, QueryOrder, QuerySelect},
+    user_rating,
+};
 use serde::Serialize;
+use tracing::error;
 
-use crate::util::{self, request_consultation::ConsultationDateTime, session::User};
+use crate::{
+    err::unexpected_err_resp,
+    util::{request_consultation::ConsultationDateTime, session::User},
+};
+
+const MAX_NUM_OF_USER_SIDE_AWAITING_RATINGS: u64 = 20;
 
 pub(crate) async fn get_awaiting_ratings(
     User { account_id }: User,
@@ -27,7 +36,7 @@ pub(crate) struct AwaitingRatingsResult {
 /// 相談申し込み者として行う評価
 #[derive(Clone, Debug, Serialize, PartialEq)]
 struct UserSideAwaitingRating {
-    consultation_id: i64,
+    user_rating_id: i64,
     consultant_id: i64, // 相談相手のユーザーID
     meeting_date_time_in_jst: ConsultationDateTime,
 }
@@ -35,7 +44,7 @@ struct UserSideAwaitingRating {
 /// 相談相手として行う評価
 #[derive(Clone, Debug, Serialize, PartialEq)]
 struct ConsultantSideAwaitingRating {
-    consultation_id: i64,
+    consultant_rating_id: i64,
     user_account_id: i64, // 相談申し込み者のユーザーID
     meeting_date_time_in_jst: ConsultationDateTime,
 }
@@ -45,14 +54,72 @@ struct ConsultantSideAwaitingRating {
 // 従って身分のチェックができていないユーザーは空の結果が返るだけなので
 // わざわざ身分チェックをする処理を入れない
 #[async_trait]
-trait AwaitingRatingsOperation {}
+trait AwaitingRatingsOperation {
+    async fn filter_user_side_awaiting_ratings(
+        &self,
+        user_account_id: i64,
+        start_criteria: DateTime<FixedOffset>,
+    ) -> Result<Vec<UserSideAwaitingRating>, ErrResp>;
+
+    async fn filter_consultant_side_awaiting_ratings(
+        &self,
+        consultant_id: i64,
+        start_criteria: DateTime<FixedOffset>,
+    ) -> Result<Vec<ConsultantSideAwaitingRating>, ErrResp>;
+}
 
 struct AwaitingRatingsOperationImpl {
     pool: DatabaseConnection,
 }
 
 #[async_trait]
-impl AwaitingRatingsOperation for AwaitingRatingsOperationImpl {}
+impl AwaitingRatingsOperation for AwaitingRatingsOperationImpl {
+    async fn filter_user_side_awaiting_ratings(
+        &self,
+        user_account_id: i64,
+        start_criteria: DateTime<FixedOffset>,
+    ) -> Result<Vec<UserSideAwaitingRating>, ErrResp> {
+        let results = user_rating::Entity::find()
+            .filter(user_rating::Column::MeetingAt.lt(start_criteria))
+            .filter(user_rating::Column::UserAccountId.eq(user_account_id))
+            .filter(user_rating::Column::Rating.is_null()) // null -> まだ未評価であるもの
+            .limit(MAX_NUM_OF_USER_SIDE_AWAITING_RATINGS)
+            .order_by_asc(user_rating::Column::MeetingAt)
+            .all(&self.pool)
+            .await
+            .map_err(|e| {
+                error!(
+                    "failed to filter user_rating (user_account_id: {}, start_criteria: {}): {}",
+                    user_account_id, start_criteria, e
+                );
+                unexpected_err_resp()
+            })?;
+        Ok(results
+            .into_iter()
+            .map(|m| {
+                let meeting_at_in_jst = m.meeting_at.with_timezone(&*JAPANESE_TIME_ZONE);
+                UserSideAwaitingRating {
+                    user_rating_id: m.user_rating_id,
+                    consultant_id: m.consultant_id,
+                    meeting_date_time_in_jst: ConsultationDateTime {
+                        year: meeting_at_in_jst.year(),
+                        month: meeting_at_in_jst.month(),
+                        day: meeting_at_in_jst.day(),
+                        hour: meeting_at_in_jst.hour(),
+                    },
+                }
+            })
+            .collect::<Vec<UserSideAwaitingRating>>())
+    }
+
+    async fn filter_consultant_side_awaiting_ratings(
+        &self,
+        consultant_id: i64,
+        start_criteria: DateTime<FixedOffset>,
+    ) -> Result<Vec<ConsultantSideAwaitingRating>, ErrResp> {
+        todo!()
+    }
+}
 
 async fn handle_awaiting_ratings(
     account_id: i64,
