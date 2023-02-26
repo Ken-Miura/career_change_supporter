fbab421f57af1ea139e91de92e35e1e0e4c3e08a
kmiura <ken.miura1102@gmail.com>
2023-02-26 13:00:03 +0000

    Add impl

--- client/user_app/src/views/personalized/RateConsultantPage.vue
+++ client/user_app/src/views/personalized/RateConsultantPage.vue
@@ -2,7 +2,7 @@
   <TheHeader/>
   <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
     <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 class="font-bold text-lg">{{ message }}</h3>
+      <h3 class="font-bold text-lg">{{ message }}, {{ consultantRatingId }}, {{ userId }}, {{ year }}, {{ month }}, {{ day }}, {{ hour }}</h3>
     </main>
     <footer class="max-w-lg mx-auto flex justify-center text-white">
       <router-link to="/" class="hover:underline">トップページへ</router-link>
@@ -13,6 +13,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import TheHeader from '@/components/TheHeader.vue'
+import { useRoute } from 'vue-router'
 
 export default defineComponent({
   name: 'RateConsultantPage',
@@ -21,7 +22,24 @@ export default defineComponent({
   },
   setup () {
     const message = 'RateConsultantPage'
-    return { message }
+    const route = useRoute()
+    const consultantRatingId = route.params.consultant_rating_id as string
+    const query = route.query
+    const userId = query['user-id']
+    const year = query.year
+    const month = query.month
+    const day = query.day
+    const hour = query.hour
+
+    return {
+      message,
+      consultantRatingId,
+      userId,
+      year,
+      month,
+      day,
+      hour
+    }
   }
 })
 </script>
--- client/user_app/src/views/personalized/RateUserPage.vue
+++ client/user_app/src/views/personalized/RateUserPage.vue
@@ -2,7 +2,7 @@
   <TheHeader/>
   <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
     <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 class="font-bold text-lg">{{ message }}</h3>
+      <h3 class="font-bold text-lg">{{ message }}, {{ userRatingId }}, {{ consultantId }}, {{ year }}, {{ month }}, {{ day }}, {{ hour }}</h3>
     </main>
     <footer class="max-w-lg mx-auto flex justify-center text-white">
       <router-link to="/" class="hover:underline">トップページへ</router-link>
@@ -13,6 +13,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import TheHeader from '@/components/TheHeader.vue'
+import { useRoute } from 'vue-router'
 
 export default defineComponent({
   name: 'RateUserPage',
@@ -21,7 +22,24 @@ export default defineComponent({
   },
   setup () {
     const message = 'RateUserPage'
-    return { message }
+    const route = useRoute()
+    const userRatingId = route.params.user_rating_id as string
+    const query = route.query
+    const consultantId = query['consultant-id']
+    const year = query.year
+    const month = query.month
+    const day = query.day
+    const hour = query.hour
+
+    return {
+      message,
+      userRatingId,
+      consultantId,
+      year,
+      month,
+      day,
+      hour
+    }
   }
 })
 </script>
