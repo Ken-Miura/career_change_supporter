41605714fbfd6a112ab88bbe630be1bf5ef36bbd
kmiura <ken.miura1102@gmail.com>
2023-03-01 15:27:02 +0000

    Add memo

--- server/user_service/src/rating/user_rating.rs
+++ server/user_service/src/rating/user_rating.rs
@@ -1,10 +1,14 @@
 // Copyright 2023 Ken Miura
 
+use axum::async_trait;
 use axum::{extract::State, Json};
-use common::RespResult;
+use chrono::{DateTime, FixedOffset, Utc};
+use common::{ErrResp, RespResult, JAPANESE_TIME_ZONE};
 use entity::sea_orm::DatabaseConnection;
 use serde::{Deserialize, Serialize};
 
+use crate::util;
+use crate::util::available_user_account::UserAccount;
 use crate::util::session::User;
 
 pub(crate) async fn post_user_rating(
@@ -12,7 +16,9 @@ pub(crate) async fn post_user_rating(
     State(pool): State<DatabaseConnection>,
     Json(req): Json<UserRatingParam>,
 ) -> RespResult<UserRatingResult> {
-    todo!()
+    let current_date_time = Utc::now().with_timezone(&(*JAPANESE_TIME_ZONE));
+    let op = UserRatingOperationImpl { pool };
+    handle_user_rating(account_id, req.user_rating_id, &current_date_time, op).await
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -22,3 +28,53 @@ pub(crate) struct UserRatingParam {
 
 #[derive(Serialize, Debug, PartialEq)]
 pub(crate) struct UserRatingResult {}
+
+#[async_trait]
+trait UserRatingOperation {
+    async fn check_if_identity_exists(&self, account_id: i64) -> Result<bool, ErrResp>;
+    /// コンサルタントが利用可能な場合（UserAccountが存在し、かつdisabled_atがNULLである場合）、[UserAccount]を返す
+    async fn get_consultant_if_available(
+        &self,
+        consultant_id: i64,
+    ) -> Result<Option<UserAccount>, ErrResp>;
+}
+
+struct UserRatingOperationImpl {
+    pool: DatabaseConnection,
+}
+
+#[async_trait]
+impl UserRatingOperation for UserRatingOperationImpl {
+    async fn check_if_identity_exists(&self, account_id: i64) -> Result<bool, ErrResp> {
+        util::identity_checker::check_if_identity_exists(&self.pool, account_id).await
+    }
+
+    /// コンサルタントが利用可能な場合（UserAccountが存在し、かつdisabled_atがNULLである場合）、[UserAccount]を返す
+    async fn get_consultant_if_available(
+        &self,
+        consultant_id: i64,
+    ) -> Result<Option<UserAccount>, ErrResp> {
+        util::available_user_account::get_if_user_account_is_available(&self.pool, consultant_id)
+            .await
+    }
+}
+
+async fn handle_user_rating(
+    account_id: i64,
+    user_rating_id: i64,
+    current_date_time: &DateTime<FixedOffset>,
+    op: impl UserRatingOperation,
+) -> RespResult<UserRatingResult> {
+    // user_rating_idが正の整数であることをチェック
+    // user_rating_idでuser_ratingを取得
+    // user_ratingのコンサルタントとaccount_idが一致していることを確認する
+    // user_ratingにある相談時間とcurrent_date_timeを用いて評価を実施可能かチェックする
+    // 身分証チェック
+    // コンサルタントのDisabledチェック
+    // user_ratingを更新する
+    //   ユーザーの存在チェック＋ロック -> 仮に存在しない場合はそれ以降の操作は何もしないで成功で終わらせる
+    //   user_ratingの取得＋ロック
+    //   user_ratingのratingがNULLであることを確認 -> NULLでないなら既に評価済を示すエラーを返す
+    //   user_ratingのratingに値を入れる
+    todo!()
+}
