357c96c5aa301898b27c09df8d8f26277dfd2cc0
kmiura <ken.miura1102@gmail.com>
2023-03-05 13:23:45 +0000

    Add test

--- server/user_service/src/rating/user_rating.rs
+++ server/user_service/src/rating/user_rating.rs
@@ -540,6 +540,33 @@ mod tests {
                     }),
                 )),
             },
+            TestCase {
+                name: "fail NoUserRatingFound (really not found)".to_string(),
+                input: Input {
+                    consultant_id,
+                    user_rating_id,
+                    rating,
+                    current_date_time,
+                    op: UserRatingOperationMock {
+                        account_id: consultant_id,
+                        consultant_available: true,
+                        user_rating_id: user_rating_id + 3,
+                        user_rating: UserRating {
+                            user_account_id,
+                            consultant_id,
+                            consultation_date_time_in_jst,
+                        },
+                        rating,
+                        current_date_time,
+                    },
+                },
+                expected: Err((
+                    StatusCode::BAD_REQUEST,
+                    Json(ApiError {
+                        code: Code::NoUserRatingFound as u32,
+                    }),
+                )),
+            },
         ]
     });
 
