47d75001636be0e4e88776d2d9c011ff1879e179
kmiura <ken.miura1102@gmail.com>
2023-03-12 14:10:45 +0000

    Add impl

--- server/user_service/src/rating/consultant_rating.rs
+++ server/user_service/src/rating/consultant_rating.rs
@@ -2,8 +2,8 @@
 
 use axum::async_trait;
 use axum::{extract::State, Json};
-use chrono::{DateTime, FixedOffset};
-use common::{ErrResp, RespResult};
+use chrono::{DateTime, FixedOffset, Utc};
+use common::{ErrResp, RespResult, JAPANESE_TIME_ZONE};
 use entity::sea_orm::DatabaseConnection;
 use opensearch::OpenSearch;
 use serde::{Deserialize, Serialize};
@@ -18,7 +18,16 @@ pub(crate) async fn post_consultant_rating(
     State(index_client): State<OpenSearch>,
     Json(req): Json<ConsultantRatingParam>,
 ) -> RespResult<ConsultantRatingResult> {
-    todo!()
+    let op = ConsultantRatingOperationImpl { pool, index_client };
+    let current_date_time = Utc::now().with_timezone(&(*JAPANESE_TIME_ZONE));
+    handle_consultant_rating(
+        account_id,
+        req.consultant_rating_id,
+        req.rating,
+        &current_date_time,
+        op,
+    )
+    .await
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -66,9 +75,65 @@ trait ConsultantRatingOperation {
     async fn make_payment_if_needed(&self, consultation_id: i64) -> Result<(), ErrResp>;
 }
 
+struct ConsultantRatingOperationImpl {
+    pool: DatabaseConnection,
+    index_client: OpenSearch,
+}
+
+#[async_trait]
+impl ConsultantRatingOperation for ConsultantRatingOperationImpl {
+    async fn check_if_identity_exists(&self, account_id: i64) -> Result<bool, ErrResp> {
+        todo!()
+    }
+
+    async fn check_if_user_account_is_available(
+        &self,
+        user_account_id: i64,
+    ) -> Result<bool, ErrResp> {
+        todo!()
+    }
+
+    async fn find_consultation_info_from_consultant_rating(
+        &self,
+        consultant_rating_id: i64,
+    ) -> Result<Option<ConsultationInfo>, ErrResp> {
+        todo!()
+    }
+
+    async fn update_consultant_rating(
+        &self,
+        consultant_id: i64,
+        consultant_rating_id: i64,
+        rating: i16,
+        current_date_time: DateTime<FixedOffset>,
+    ) -> Result<(), ErrResp> {
+        todo!()
+    }
+
+    async fn filter_consultant_rating_by_consultant_id(
+        &self,
+        consultant_id: i64,
+    ) -> Result<Vec<i16>, ErrResp> {
+        todo!()
+    }
+
+    async fn update_rating_on_document_if_needed(
+        &self,
+        consultant_id: i64,
+        averate_rating: f64,
+    ) -> Result<(), ErrResp> {
+        todo!()
+    }
+
+    async fn make_payment_if_needed(&self, consultation_id: i64) -> Result<(), ErrResp> {
+        todo!()
+    }
+}
+
 async fn handle_consultant_rating(
     account_id: i64,
     consultant_rating_id: i64,
+    rating: i16,
     current_date_time: &DateTime<FixedOffset>,
     op: impl ConsultantRatingOperation,
 ) -> RespResult<ConsultantRatingResult> {
