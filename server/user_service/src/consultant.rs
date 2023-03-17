eabec2230b6b2d5036b444c200a949be74f2f8be
kmiura <ken.miura1102@gmail.com>
2023-03-17 13:03:35 +0000

    Add copyright

--- server/user_service/src/consultant.rs
+++ server/user_service/src/consultant.rs
@@ -1,2 +1,4 @@
+// Copyright 2023 Ken Miura
+
 pub(super) mod detail;
 pub(super) mod search;
--- server/user_service/src/request_consultation.rs
+++ server/user_service/src/request_consultation.rs
@@ -1,3 +1,5 @@
+// Copyright 2023 Ken Miura
+
 pub(super) mod begin;
 pub(super) mod fee_per_hour_in_yen_for_application;
 pub(super) mod finish;
