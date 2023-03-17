022fa25df50b2364e203ee5fda5d7da7b57a4211
kmiura <ken.miura1102@gmail.com>
2023-03-17 15:37:16 +0000

    Add test

--- client/user_app/tests/unit/LoginPage.spec.ts
+++ client/user_app/tests/unit/LoginPage.spec.ts
@@ -10,6 +10,7 @@ import { Code } from '@/util/Error'
 import { nextTick, ref } from 'vue'
 import { refresh } from '@/util/personalized/refresh/Refresh'
 import { RefreshResp } from '@/util/personalized/refresh/RefreshResp'
+import WaitingCircle from '@/components/WaitingCircle.vue'
 
 const loginDoneMock = ref(true)
 const loginFuncMock = jest.fn()
@@ -139,6 +140,28 @@ describe('LoginPage.vue', () => {
     const resultMessage = alertMessage.text()
     expect(resultMessage).toContain(Message.UNEXPECTED_ERR)
     expect(resultMessage).toContain(errDetail)
+  })
+
+  it('displays header and WaitingCircle, no AlertMessage while requesting', async () => {
+    const apiErrResp = ApiErrorResp.create(401, ApiError.create(Code.UNAUTHORIZED))
+    refreshMock.mockResolvedValue(apiErrResp)
+    loginDoneMock.value = false
+    loginFuncMock.mockResolvedValue(LoginResp.create())
+
+    const wrapper = mount(LoginPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+
+    const headers = wrapper.findAll('header')
+    expect(headers.length).toBe(1)
+    const waitingCircles = wrapper.findAllComponents(WaitingCircle)
+    expect(waitingCircles.length).toBe(1)
+    const alertMessages = wrapper.findAllComponents(AlertMessage)
+    expect(alertMessages.length).toBe(0)
   })
 
   it('moves to profile when login is successful', async () => {
