91f4d124c855a923397cc0c9dd4524e2f8c84fd8
kmiura <ken.miura1102@gmail.com>
2023-03-30 15:29:59 +0000

    Add impl

--- server/user_service/src/pass_code.rs
+++ server/user_service/src/pass_code.rs
@@ -1,9 +1,11 @@
 // Copyright 2023 Ken Miura
 
 use async_redis_session::RedisSessionStore;
+use async_session::SessionStore;
 use axum::{extract::State, http::StatusCode, Json};
 use axum_extra::extract::SignedCookieJar;
-use common::{ApiError, RespResult};
+use chrono::{DateTime, FixedOffset, Utc};
+use common::{ApiError, RespResult, JAPANESE_TIME_ZONE};
 use entity::sea_orm::DatabaseConnection;
 use serde::{Deserialize, Serialize};
 use tracing::error;
@@ -29,16 +31,16 @@ pub(crate) async fn post_pass_code(
             ));
         }
     };
-    // セッションを取得する。なければUnauthirized
-    // セッション内のアカウントIDからUserInfoを取得
-    // Disabledチェック
-    // 二段階認証の有効化チェック（シークレットが存在することを前提とした処理をするために事前チェックは必要）
-    // セッション内のLoginStatusのチェック（Finishなら何もせずに早期リターン。いらないかも）
-    // シークレット、現在時刻に対してパスコードが一致するか確認
-    // セッションのLoginStatusを更新（セッションの期限も更新する）
-    // 最終ログイン時刻を更新
-    // NOTE: Sessionは更新するが、Cookieを更新するわけではない。従ってSignedCookieJarをハンドラのレスポンスに含める必要はないように見える。要確認
-    todo!()
+    let current_date_time = Utc::now().with_timezone(&(*JAPANESE_TIME_ZONE));
+    let op = PassCodeOperationImpl { pool };
+    handle_pass_code_req(
+        session_id.as_str(),
+        &current_date_time,
+        req.pass_code.as_str(),
+        &op,
+        &store,
+    )
+    .await
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -48,3 +50,30 @@ pub(crate) struct PassCodeReq {
 
 #[derive(Serialize)]
 pub(crate) struct PassCodeReqResult {}
+
+trait PassCodeOperation {}
+
+struct PassCodeOperationImpl {
+    pool: DatabaseConnection,
+}
+
+impl PassCodeOperation for PassCodeOperationImpl {}
+
+async fn handle_pass_code_req(
+    session_id: &str,
+    current_date_time: &DateTime<FixedOffset>,
+    pass_code: &str,
+    op: &impl PassCodeOperation,
+    store: &impl SessionStore,
+) -> RespResult<PassCodeReqResult> {
+    // pass_codeのvalidation
+    // セッションを取得する。なければUnauthirized
+    // セッション内のアカウントIDからUserInfoを取得
+    // Disabledチェック
+    // 二段階認証の有効化チェック（シークレットが存在することを前提とした処理をするために事前チェックは必要）
+    // セッション内のLoginStatusのチェック（Finishなら何もせずに早期リターン。いらないかも）
+    // シークレット、現在時刻に対してパスコードが一致するか確認
+    // セッションのLoginStatusを更新（セッションの期限も更新する）
+    // 最終ログイン時刻を更新
+    todo!()
+}
