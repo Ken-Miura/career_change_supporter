1e82c3782ace0e93c5fc705ebe5a6ea84613818a
kmiura <ken.miura1102@gmail.com>
2023-04-01 14:42:15 +0000

    Add impl

--- server/user_service/src/mfa/mfa_request/recovery_code.rs
+++ server/user_service/src/mfa/mfa_request/recovery_code.rs
@@ -1,14 +1,20 @@
 // Copyright 2023 Ken Miura
 
+use std::time::Duration;
+
 use async_redis_session::RedisSessionStore;
+use async_session::SessionStore;
+use axum::async_trait;
 use axum::http::StatusCode;
 use axum::{extract::State, Json};
 use axum_extra::extract::SignedCookieJar;
-use common::{ApiError, RespResult};
+use chrono::{DateTime, FixedOffset, Utc};
+use common::{ApiError, RespResult, JAPANESE_TIME_ZONE};
 use entity::sea_orm::DatabaseConnection;
 use serde::{Deserialize, Serialize};
 use tracing::error;
 
+use crate::util::session::LOGIN_SESSION_EXPIRY;
 use crate::{err::Code, util::session::SESSION_ID_COOKIE_NAME};
 
 pub(crate) async fn post_recovery_code(
@@ -30,6 +36,48 @@ pub(crate) async fn post_recovery_code(
             ));
         }
     };
+    let current_date_time = Utc::now().with_timezone(&(*JAPANESE_TIME_ZONE));
+    let op = RecoveryCodeOperationImpl {
+        pool,
+        expiry: LOGIN_SESSION_EXPIRY,
+    };
+
+    handle_recovery_code(
+        session_id.as_str(),
+        &current_date_time,
+        req.recovery_code.as_str(),
+        &op,
+        &store,
+    )
+    .await
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub(crate) struct RecoveryCodeReq {
+    recovery_code: String,
+}
+
+#[derive(Serialize)]
+pub(crate) struct RecoveryCodeReqResult {}
+
+#[async_trait]
+trait RecoveryCodeOperation {}
+
+struct RecoveryCodeOperationImpl {
+    pool: DatabaseConnection,
+    expiry: Duration,
+}
+
+#[async_trait]
+impl RecoveryCodeOperation for RecoveryCodeOperationImpl {}
+
+async fn handle_recovery_code(
+    session_id: &str,
+    current_date_time: &DateTime<FixedOffset>,
+    recovery_code: &str,
+    op: &impl RecoveryCodeOperation,
+    store: &impl SessionStore,
+) -> RespResult<RecoveryCodeReqResult> {
     // リカバリーコードのvalidation
     // セッションIDでセッションを取得
     // セッションからアカウントIDを取得
@@ -44,11 +92,3 @@ pub(crate) async fn post_recovery_code(
     // ログイン日時を更新
     todo!()
 }
-
-#[derive(Clone, Debug, Deserialize)]
-pub(crate) struct RecoveryCodeReq {
-    recovery_code: String,
-}
-
-#[derive(Serialize)]
-pub(crate) struct RecoveryCodeReqResult {}
