268c75c93a1e48e1873227b363d276445dfeac68
kmiura <ken.miura1102@gmail.com>
2023-04-01 13:11:37 +0000

    Add impl

--- server/user_service/src/mfa/mfa_request/recovery_code.rs
+++ server/user_service/src/mfa/mfa_request/recovery_code.rs
@@ -1,11 +1,15 @@
 // Copyright 2023 Ken Miura
 
 use async_redis_session::RedisSessionStore;
+use axum::http::StatusCode;
 use axum::{extract::State, Json};
 use axum_extra::extract::SignedCookieJar;
-use common::RespResult;
+use common::{ApiError, RespResult};
 use entity::sea_orm::DatabaseConnection;
 use serde::{Deserialize, Serialize};
+use tracing::error;
+
+use crate::{err::Code, util::session::SESSION_ID_COOKIE_NAME};
 
 pub(crate) async fn post_recovery_code(
     jar: SignedCookieJar,
@@ -13,6 +17,19 @@ pub(crate) async fn post_recovery_code(
     State(store): State<RedisSessionStore>,
     Json(req): Json<RecoveryCodeReq>,
 ) -> RespResult<RecoveryCodeReqResult> {
+    let option_cookie = jar.get(SESSION_ID_COOKIE_NAME);
+    let session_id = match option_cookie {
+        Some(s) => s.value().to_string(),
+        None => {
+            error!("no sessoin cookie found on recovery code req");
+            return Err((
+                StatusCode::UNAUTHORIZED,
+                Json(ApiError {
+                    code: Code::Unauthorized as u32,
+                }),
+            ));
+        }
+    };
     todo!()
 }
 
