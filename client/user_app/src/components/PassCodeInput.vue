4ccdf0ab112db4755e19b0a05e7aae691e209583
kmiura <ken.miura1102@gmail.com>
2023-04-03 13:23:10 +0000

    Change pass code text size and add profile link on disable success

--- client/user_app/src/components/PassCodeInput.vue
+++ client/user_app/src/components/PassCodeInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pt-3 rounded bg-gray-200">
     <label class="block text-gray-700 text-sm font-bold mb-2 ml-3">{{ label }}</label>
-    <input v-on:input="onInput" type="text" inputmode="numeric" pattern="[0-9]{6}" title="半角数字のみの6桁でご入力下さい。" required minlength="6" maxlength="6" class="text-right bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
+    <input v-on:input="onInput" type="text" inputmode="numeric" pattern="[0-9]{6}" title="半角数字のみの6桁でご入力下さい。" required minlength="6" maxlength="6" class="text-3xl text-right bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
   </div>
 </template>
 
