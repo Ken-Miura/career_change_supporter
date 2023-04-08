81230de8ddc211bd0363bc8ae50baaaa9a9c4ec6
kmiura <ken.miura1102@gmail.com>
2023-04-08 15:23:04 +0000

    Add test template

--- server/user_service/src/mfa/mfa_request/pass_code.rs
+++ server/user_service/src/mfa/mfa_request/pass_code.rs
@@ -71,7 +71,7 @@ pub(crate) struct PassCodeReq {
     pass_code: String,
 }
 
-#[derive(Serialize)]
+#[derive(Debug, Serialize, PartialEq)]
 pub(crate) struct PassCodeReqResult {}
 
 #[async_trait]
@@ -178,4 +178,94 @@ fn get_login_status_from_session(session: &Session) -> Result<LoginStatus, ErrRe
         }
     };
     Ok(LoginStatus::from(login_status))
+}
+
+#[cfg(test)]
+mod tests {
+    use async_session::{MemoryStore, Session};
+    use axum::async_trait;
+    use chrono::{DateTime, FixedOffset};
+    use common::{ErrResp, RespResult};
+    use once_cell::sync::Lazy;
+
+    use crate::{mfa::mfa_request::MfaInfo, util::user_info::UserInfo};
+
+    use super::{handle_pass_code_req, PassCodeOperation, PassCodeReqResult};
+
+    #[derive(Debug)]
+    struct TestCase {
+        name: String,
+        input: Input,
+        expected: RespResult<PassCodeReqResult>,
+    }
+
+    #[derive(Debug)]
+    struct Input {
+        session_id: String,
+        current_date_time: DateTime<FixedOffset>,
+        pass_code: String,
+        op: PassCodeOperationMock,
+        store: MemoryStore,
+    }
+
+    #[derive(Clone, Debug)]
+    struct PassCodeOperationMock {}
+
+    #[async_trait]
+    impl PassCodeOperation for PassCodeOperationMock {
+        async fn get_user_info_if_available(&self, account_id: i64) -> Result<UserInfo, ErrResp> {
+            todo!()
+        }
+
+        async fn get_mfa_info_by_account_id(&self, account_id: i64) -> Result<MfaInfo, ErrResp> {
+            todo!()
+        }
+
+        fn set_login_session_expiry(&self, session: &mut Session) {
+            todo!()
+        }
+
+        async fn update_last_login(
+            &self,
+            account_id: i64,
+            login_time: &DateTime<FixedOffset>,
+        ) -> Result<(), ErrResp> {
+            todo!()
+        }
+    }
+
+    static TEST_CASE_SET: Lazy<Vec<TestCase>> = Lazy::new(|| vec![]);
+
+    #[tokio::test]
+    async fn handle_pass_code_req_tests() {
+        for test_case in TEST_CASE_SET.iter() {
+            let session_id = test_case.input.session_id.clone();
+            let current_date_time = test_case.input.current_date_time;
+            let pass_code = test_case.input.pass_code.clone();
+            let op = test_case.input.op.clone();
+            let store = test_case.input.store.clone();
+
+            let result = handle_pass_code_req(
+                session_id.as_str(),
+                &current_date_time,
+                pass_code.as_str(),
+                &op,
+                &store,
+            )
+            .await;
+
+            let message = format!("test case \"{}\" failed", test_case.name.clone());
+            if test_case.expected.is_ok() {
+                let resp = result.expect("failed to get Ok");
+                let expected = test_case.expected.as_ref().expect("failed to get Ok");
+                assert_eq!(expected.0, resp.0, "{}", message);
+                assert_eq!(expected.1 .0, resp.1 .0, "{}", message);
+            } else {
+                let resp = result.expect_err("failed to get Err");
+                let expected = test_case.expected.as_ref().expect_err("failed to get Err");
+                assert_eq!(expected.0, resp.0, "{}", message);
+                assert_eq!(expected.1 .0, resp.1 .0, "{}", message);
+            }
+        }
+    }
 }
