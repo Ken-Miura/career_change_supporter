4c3aa46a441298dfc4feb7338a2b0a4c5668f0d5
kmiura <ken.miura1102@gmail.com>
2023-04-12 14:24:16 +0000

    Add mock

--- client/user_app/tests/unit/EnableMfaConfirmationPage.spec.ts
+++ client/user_app/tests/unit/EnableMfaConfirmationPage.spec.ts
@@ -1,3 +1,23 @@
+import { ref } from 'vue'
+
+const getTempMfaSecretDoneMock = ref(true)
+const getTempMfaSecretFuncMock = jest.fn()
+jest.mock('@/util/personalized/enable-mfa-confirmation/useGetTempMfaSecret', () => ({
+  useGetTempMfaSecret: () => ({
+    getTempMfaSecretDone: getTempMfaSecretDoneMock,
+    getTempMfaSecretFunc: getTempMfaSecretFuncMock
+  })
+}))
+
+const postEnableMfaReqDoneMock = ref(true)
+const postEnableMfaReqFuncMock = jest.fn()
+jest.mock('@/util/personalized/enable-mfa-confirmation/usePostEnableMfaReq', () => ({
+  usePostEnableMfaReq: () => ({
+    postEnableMfaReqDone: postEnableMfaReqDoneMock,
+    postEnableMfaReqFunc: postEnableMfaReqFuncMock
+  })
+}))
+
 const routerPushMock = jest.fn()
 jest.mock('vue-router', () => ({
   useRouter: () => ({
@@ -8,6 +28,10 @@ jest.mock('vue-router', () => ({
 describe('EnableMfaConfirmationPage.vue', () => {
   beforeEach(() => {
     routerPushMock.mockClear()
+    getTempMfaSecretDoneMock.value = true
+    getTempMfaSecretFuncMock.mockReset()
+    postEnableMfaReqDoneMock.value = true
+    postEnableMfaReqFuncMock.mockReset()
   })
 
   it('', async () => {
