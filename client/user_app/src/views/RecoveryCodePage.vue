4ea42d48e6cf09527dac1badc6644b9301373076
kmiura <ken.miura1102@gmail.com>
2023-04-15 13:40:25 +0000

    Add test

--- client/user_app/src/views/RecoveryCodePage.vue
+++ client/user_app/src/views/RecoveryCodePage.vue
@@ -2,7 +2,7 @@
   <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
     <header class="max-w-lg mx-auto">
       <router-link to="/">
-        <h1 class="text-2xl font-bold text-white text-center">就職先・転職先を見極めるためのサイト</h1>
+        <h1 data-test="header" class="text-2xl font-bold text-white text-center">就職先・転職先を見極めるためのサイト</h1>
       </router-link>
     </header>
     <div v-if="!postRecoveryCodeDone" class="m-6">
--- client/user_app/tests/unit/RecoveryCodePage.spec.ts
+++ client/user_app/tests/unit/RecoveryCodePage.spec.ts
@@ -1,3 +1,9 @@
+import { flushPromises, mount, RouterLinkStub } from '@vue/test-utils'
+import { ref } from 'vue'
+import WaitingCircle from '@/components/WaitingCircle.vue'
+import AlertMessage from '@/components/AlertMessage.vue'
+import RecoveryCodePage from '@/views/RecoveryCodePage.vue'
+
 const routerPushMock = jest.fn()
 jest.mock('vue-router', () => ({
   useRouter: () => ({
@@ -5,12 +11,39 @@ jest.mock('vue-router', () => ({
   })
 }))
 
+const postRecoveryCodeDoneMock = ref(true)
+const postRecoveryCodeFuncMock = jest.fn()
+jest.mock('@/util/mfa/usePostRecoveryCode', () => ({
+  usePostRecoveryCode: () => ({
+    postRecoveryCodeDone: postRecoveryCodeDoneMock,
+    postRecoveryCodeFunc: postRecoveryCodeFuncMock
+  })
+}))
+
 describe('RecoveryCodePage.vue', () => {
   beforeEach(() => {
     routerPushMock.mockClear()
+    postRecoveryCodeDoneMock.value = true
+    postRecoveryCodeFuncMock.mockReset()
   })
 
-  it('test', async () => {
-    console.log('test')
+  it('has WaitingCircle while calling postRecoveryCode', async () => {
+    postRecoveryCodeDoneMock.value = false
+    const wrapper = mount(RecoveryCodePage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const alertMessages = wrapper.findAllComponents(AlertMessage)
+    expect(alertMessages.length).toBe(0)
+
+    const header = wrapper.find('[data-test="header"]')
+    expect(header.text()).toContain('就職先・転職先を見極めるためのサイト')
+    const waitingCircles = wrapper.findAllComponents(WaitingCircle)
+    expect(waitingCircles.length).toBe(1)
   })
 })
