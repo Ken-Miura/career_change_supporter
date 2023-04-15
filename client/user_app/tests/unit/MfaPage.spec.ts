3740946ffd1a730899eedb19f4abe20f780f4312
kmiura <ken.miura1102@gmail.com>
2023-04-15 12:55:46 +0000

    Add test

--- client/user_app/tests/unit/MfaPage.spec.ts
+++ client/user_app/tests/unit/MfaPage.spec.ts
@@ -160,4 +160,94 @@ describe('MfaPage.vue', () => {
     expect(resultMessage).toContain(Message.LOGIN_FAILED)
     expect(resultMessage).toContain(errDetail)
   })
+
+  it(`displays alert message ${Message.INVALID_PASS_CODE_MESSAGE} if ${Code.INVALID_PASS_CODE} is returned`, async () => {
+    const apiErrResp = ApiErrorResp.create(400, ApiError.create(Code.INVALID_PASS_CODE))
+    postPassCodeFuncMock.mockResolvedValue(apiErrResp)
+    const wrapper = mount(MfaPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const passCodeInputComponent = wrapper.findComponent(PassCodeInput)
+    const passCodeInput = passCodeInputComponent.find('input')
+    await passCodeInput.setValue('123456')
+
+    const loginButton = wrapper.find('[data-test="login-button"]')
+    await loginButton.trigger('submit')
+    await flushPromises()
+
+    expect(routerPushMock).toHaveBeenCalledTimes(0)
+
+    const alertMessage = wrapper.findComponent(AlertMessage)
+    const classes = alertMessage.classes()
+    expect(classes).not.toContain('hidden')
+    const resultMessage = alertMessage.text()
+    expect(resultMessage).toContain(Message.INVALID_PASS_CODE_MESSAGE)
+    expect(resultMessage).toContain(Code.INVALID_PASS_CODE.toString())
+  })
+
+  it(`displays alert message ${Message.MFA_IS_NOT_ENABLED_MESSAGE} if ${Code.MFA_IS_NOT_ENABLED} is returned`, async () => {
+    const apiErrResp = ApiErrorResp.create(400, ApiError.create(Code.MFA_IS_NOT_ENABLED))
+    postPassCodeFuncMock.mockResolvedValue(apiErrResp)
+    const wrapper = mount(MfaPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const passCodeInputComponent = wrapper.findComponent(PassCodeInput)
+    const passCodeInput = passCodeInputComponent.find('input')
+    await passCodeInput.setValue('123456')
+
+    const loginButton = wrapper.find('[data-test="login-button"]')
+    await loginButton.trigger('submit')
+    await flushPromises()
+
+    expect(routerPushMock).toHaveBeenCalledTimes(0)
+
+    const alertMessage = wrapper.findComponent(AlertMessage)
+    const classes = alertMessage.classes()
+    expect(classes).not.toContain('hidden')
+    const resultMessage = alertMessage.text()
+    expect(resultMessage).toContain(Message.MFA_IS_NOT_ENABLED_MESSAGE)
+    expect(resultMessage).toContain(Code.MFA_IS_NOT_ENABLED.toString())
+  })
+
+  it(`displays alert message ${Message.PASS_CODE_DOES_NOT_MATCH_MESSAGE} if ${Code.PASS_CODE_DOES_NOT_MATCH} is returned`, async () => {
+    const apiErrResp = ApiErrorResp.create(400, ApiError.create(Code.PASS_CODE_DOES_NOT_MATCH))
+    postPassCodeFuncMock.mockResolvedValue(apiErrResp)
+    const wrapper = mount(MfaPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const passCodeInputComponent = wrapper.findComponent(PassCodeInput)
+    const passCodeInput = passCodeInputComponent.find('input')
+    await passCodeInput.setValue('123456')
+
+    const loginButton = wrapper.find('[data-test="login-button"]')
+    await loginButton.trigger('submit')
+    await flushPromises()
+
+    expect(routerPushMock).toHaveBeenCalledTimes(0)
+
+    const alertMessage = wrapper.findComponent(AlertMessage)
+    const classes = alertMessage.classes()
+    expect(classes).not.toContain('hidden')
+    const resultMessage = alertMessage.text()
+    expect(resultMessage).toContain(Message.PASS_CODE_DOES_NOT_MATCH_MESSAGE)
+    expect(resultMessage).toContain(Code.PASS_CODE_DOES_NOT_MATCH.toString())
+  })
 })
