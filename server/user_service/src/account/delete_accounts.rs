b064da1949e731ea2bf87de4d0f84788bd680668
kmiura <ken.miura1102@gmail.com>
2023-04-17 15:11:55 +0000

    Add mock

--- server/user_service/src/account/delete_accounts.rs
+++ server/user_service/src/account/delete_accounts.rs
@@ -1,5 +1,6 @@
 // Copyright 2023 Ken Miura
 
+use axum::async_trait;
 use axum::extract::State;
 use common::RespResult;
 use entity::sea_orm::DatabaseConnection;
@@ -14,9 +15,33 @@ pub(crate) async fn delete_accounts(
     }: User,
     State(_pool): State<DatabaseConnection>,
     State(_index_client): State<OpenSearch>,
-) -> RespResult<DeleteAccountResult> {
+) -> RespResult<DeleteAccountsResult> {
     todo!()
 }
 
 #[derive(Clone, Debug, Serialize, PartialEq)]
-pub(crate) struct DeleteAccountResult {}
+pub(crate) struct DeleteAccountsResult {}
+
+#[async_trait]
+trait DeleteAccountsOperation {}
+
+struct DeleteAccountsOperationImpl {
+    pool: DatabaseConnection,
+    index_client: OpenSearch,
+}
+
+#[async_trait]
+impl DeleteAccountsOperation for DeleteAccountsOperationImpl {}
+
+async fn handle_delete_accounts(
+    account_id: i64,
+    email_address: String,
+    op: &impl DeleteAccountsOperation,
+) -> RespResult<DeleteAccountsResult> {
+    // user_accountの排他ロック
+    //   settlementに何かあればstopped_settlementへ移動
+    //   opensearchから職歴の削除
+    //   user_accountの削除（user_accountからdeleted_user_accountへ移動）
+    // 削除成功のメール通知
+    todo!()
+}
