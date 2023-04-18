f5af0fcdcd60d8b45ff69589a514e7508d1d968a
kmiura <ken.miura1102@gmail.com>
2023-04-18 12:25:13 +0000

    Add mock

--- server/user_service/src/account/delete_accounts.rs
+++ server/user_service/src/account/delete_accounts.rs
@@ -2,7 +2,8 @@
 
 use axum::async_trait;
 use axum::extract::State;
-use common::RespResult;
+use chrono::{DateTime, FixedOffset};
+use common::{RespResult, JAPANESE_TIME_ZONE};
 use entity::sea_orm::DatabaseConnection;
 use opensearch::OpenSearch;
 use serde::Serialize;
@@ -10,13 +11,19 @@ use serde::Serialize;
 use crate::util::session::user::User;
 
 pub(crate) async fn delete_accounts(
-    User {
-        user_info: _user_info,
-    }: User,
-    State(_pool): State<DatabaseConnection>,
-    State(_index_client): State<OpenSearch>,
+    User { user_info }: User,
+    State(pool): State<DatabaseConnection>,
+    State(index_client): State<OpenSearch>,
 ) -> RespResult<DeleteAccountsResult> {
-    todo!()
+    let current_date_time = chrono::Utc::now().with_timezone(&(*JAPANESE_TIME_ZONE));
+    let op = DeleteAccountsOperationImpl { pool, index_client };
+    handle_delete_accounts(
+        user_info.account_id,
+        user_info.email_address,
+        current_date_time,
+        &op,
+    )
+    .await
 }
 
 #[derive(Clone, Debug, Serialize, PartialEq)]
@@ -36,11 +43,14 @@ impl DeleteAccountsOperation for DeleteAccountsOperationImpl {}
 async fn handle_delete_accounts(
     account_id: i64,
     email_address: String,
+    current_date_time: DateTime<FixedOffset>,
     op: &impl DeleteAccountsOperation,
 ) -> RespResult<DeleteAccountsResult> {
+    // settlement一覧取得
+    // settlement一覧をイテレート
+    //   settlementの排他ロックを取得し、stopped_settlementへ移動
     // user_accountの排他ロック
-    //   settlementに何かあればstopped_settlementへ移動
-    //   opensearchから職歴の削除
+    //   opensearchから職歴の削除 (documentのデータは後の定期処理で削除する)
     //   user_accountの削除（user_accountからdeleted_user_accountへ移動）
     // 削除成功のメール通知
     todo!()
