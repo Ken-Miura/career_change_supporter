47efe5979950f28deb7af8f0be179de81657b7c6
kmiura <ken.miura1102@gmail.com>
2023-04-22 15:04:17 +0000

    Add test

--- client/user_app/tests/unit/DeleteAccountConfirmationPage.spec.ts
+++ client/user_app/tests/unit/DeleteAccountConfirmationPage.spec.ts
@@ -1,4 +1,9 @@
+import { RefreshResp } from '@/util/personalized/refresh/RefreshResp'
+import { mount, flushPromises, RouterLinkStub } from '@vue/test-utils'
 import { ref } from 'vue'
+import TheHeader from '@/components/TheHeader.vue'
+import WaitingCircle from '@/components/WaitingCircle.vue'
+import DeleteAccountConfirmationPage from '@/views/personalized/DeleteAccountConfirmationPage.vue'
 
 const routerPushMock = jest.fn()
 jest.mock('vue-router', () => ({
@@ -34,7 +39,23 @@ describe('DeleteAccountConfirmationPage.spec.vue', () => {
     routerPushMock.mockClear()
   })
 
-  it('has WaitingCircle while calling ', async () => {
-    console.log('test')
+  it('has WaitingCircle while calling refresh', async () => {
+    refreshDoneMock.value = false
+    refreshFuncMock.mockResolvedValue(RefreshResp.create())
+    const wrapper = mount(DeleteAccountConfirmationPage, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub
+        }
+      }
+    })
+    await flushPromises()
+
+    const waitingCircles = wrapper.findAllComponents(WaitingCircle)
+    expect(waitingCircles.length).toBe(1)
+    const headers = wrapper.findAllComponents(TheHeader)
+    expect(headers.length).toBe(1)
+    // ユーザーに待ち時間を表すためにWaitingCircleが出ていることが確認できれば十分のため、
+    // mainが出ていないことまで確認しない。
   })
 })
