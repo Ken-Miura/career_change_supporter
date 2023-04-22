98e772b6d1b553f1fe4d5ff20f54d2d499c6522d
kmiura <ken.miura1102@gmail.com>
2023-04-22 14:51:18 +0000

    Add mock

--- client/user_app/tests/unit/DeleteAccountConfirmationPage.spec.ts
+++ client/user_app/tests/unit/DeleteAccountConfirmationPage.spec.ts
@@ -1,3 +1,5 @@
+import { ref } from 'vue'
+
 const routerPushMock = jest.fn()
 jest.mock('vue-router', () => ({
   useRouter: () => ({
@@ -5,8 +7,30 @@ jest.mock('vue-router', () => ({
   })
 }))
 
+const refreshDoneMock = ref(true)
+const refreshFuncMock = jest.fn()
+jest.mock('@/util/personalized/refresh/useRefresh', () => ({
+  useRefresh: () => ({
+    refreshDone: refreshDoneMock,
+    refreshFunc: refreshFuncMock
+  })
+}))
+
+const deleteAccountDoneMock = ref(true)
+const deleteAccountFuncMock = jest.fn()
+jest.mock('@/util/personalized/delete-account-confirmation/useDeleteAccount', () => ({
+  useDeleteAccount: () => ({
+    deleteAccountDone: deleteAccountDoneMock,
+    deleteAccountFunc: deleteAccountFuncMock
+  })
+}))
+
 describe('DeleteAccountConfirmationPage.spec.vue', () => {
   beforeEach(() => {
+    refreshDoneMock.value = true
+    refreshFuncMock.mockReset()
+    deleteAccountDoneMock.value = true
+    deleteAccountFuncMock.mockReset()
     routerPushMock.mockClear()
   })
 
