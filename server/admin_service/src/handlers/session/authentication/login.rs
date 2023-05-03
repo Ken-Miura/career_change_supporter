25d135e4d15449cc6eef326a07783e920e6ece11
kmiura <ken.miura1102@gmail.com>
2023-05-03 17:13:11 +0000

    Add TODO

--- server/admin_service/src/handlers/session/authentication/login.rs
+++ server/admin_service/src/handlers/session/authentication/login.rs
@@ -70,6 +70,7 @@ async fn handle_login_req(
         // 処理時間からアカウントが見つからなかったのか、パスワードが見つからなかったのかを露見させないようにするため
         // アカウントが見つからなかった場合でもis_password_matchの処理を行い、パスワードが一致しなかった場合の計算量に近づける
         // 計算量を同等にするためだけにis_password_matchを呼ぶので、戻り値は意図的に無視する
+        // TODO: Bcryptのコストが決まったら更新する
         let dummy = "$2b$07$crI33XZdfhjE4oZwjn9Sc.0WWpTRjdAhFV3vbmd2unM6F70Rdr8im".as_bytes();
         let _ = is_password_match(password, dummy);
         error!("unauthorized: no email address ({}) found", email_addr);
