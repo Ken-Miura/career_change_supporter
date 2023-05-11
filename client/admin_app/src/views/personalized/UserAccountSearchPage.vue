e3ae02baef2002e9eba860a6c4eaf0442a0528c4
kmiura <ken.miura1102@gmail.com>
2023-05-11 17:18:39 +0000

    Add test

--- client/admin_app/src/views/personalized/UserAccountSearchPage.vue
+++ client/admin_app/src/views/personalized/UserAccountSearchPage.vue
@@ -2,21 +2,21 @@
   <TheHeader/>
   <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
     <main class="flex flex-col justify-center bg-white max-w-2xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 class="font-bold text-2xl">アカウント検索</h3>
-      <p class="ml-2 mt-4 mb-6 text-lg">アカウントID、またはメールアドレスを入力して検索を押して下さい。既に削除されたアカウントは、メールアドレスではなくアカウントIDで検索して下さい。削除されたアカウントは、そうでないアカウントと比較し、確認できる情報が限定されています。</p>
+      <h3 data-test="label" class="font-bold text-2xl">アカウント検索</h3>
+      <p data-test="description" class="ml-2 mt-4 mb-6 text-lg">アカウントID、またはメールアドレスを入力して検索を押して下さい。既に削除されたアカウントは、メールアドレスではなくアカウントIDで検索して下さい。削除されたアカウントは、そうでないアカウントと比較し、確認できる情報が限定されています。</p>
       <form class="flex flex-col" @submit.prevent="searchUserAccountHandler">
         <div class="pt-3 rounded bg-gray-200">
-          <label class="block text-gray-700 text-sm font-bold mb-2 ml-3">アカウントID</label>
+          <label data-test="account-id-label" class="block text-gray-700 text-sm font-bold mb-2 ml-3">アカウントID</label>
           <input v-model="accountId" minlength="1" pattern="\d+" title="数値" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
         </div>
         <div class="ml-2 p-2 text-xl">
           or
         </div>
         <div class="mb-6 pt-3 rounded bg-gray-200">
-          <label class="block text-gray-700 text-sm font-bold mb-2 ml-3">メールアドレス</label>
+          <label data-test="email-address-label" class="block text-gray-700 text-sm font-bold mb-2 ml-3">メールアドレス</label>
           <input v-model="emailAddress" type="email" minlength="1" maxlength="254" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
         </div>
-        <button v-bind:disabled="disabled" class="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200 disabled:bg-slate-100 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none" type="submit">検索</button>
+        <button data-test="button" v-bind:disabled="disabled" class="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200 disabled:bg-slate-100 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none" type="submit">検索</button>
         <div v-if="errorMessage">
           <AlertMessage class="mt-6" v-bind:message="errorMessage"/>
         </div>
--- client/admin_app/tests/unit/UserAccountSearchPage.spec.ts
+++ client/admin_app/tests/unit/UserAccountSearchPage.spec.ts
@@ -39,5 +39,19 @@ describe('UserAccountSearchPage.vue', () => {
 
     const headers = wrapper.findAllComponents(TheHeader)
     expect(headers.length).toBe(1)
+
+    const label = wrapper.find('[data-test="label"]')
+    expect(label.text()).toContain('アカウント検索')
+    const description = wrapper.find('[data-test="description"]')
+    expect(description.text()).toContain('アカウントID、またはメールアドレスを入力して検索を押して下さい。既に削除されたアカウントは、メールアドレスではなくアカウントIDで検索して下さい。削除されたアカウントは、そうでないアカウントと比較し、確認できる情報が限定されています。')
+    const accountIdLabel = wrapper.find('[data-test="account-id-label"]')
+    expect(accountIdLabel.text()).toContain('アカウントID')
+    const emailAddressLabel = wrapper.find('[data-test="email-address-label"]')
+    expect(emailAddressLabel.text()).toContain('メールアドレス')
+
+    const button = wrapper.find('[data-test="button"]')
+    expect(button.text()).toContain('検索')
+    const buttonDisabledAttr = button.attributes('disabled')
+    expect(buttonDisabledAttr).toBeDefined()
   })
 })
