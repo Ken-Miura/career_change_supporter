715f75602f641ad3335a72b1074e24e8a7c1a0b8
kmiura <ken.miura1102@gmail.com>
2023-05-20 08:07:04 +0000

    Add impl

--- server/admin_service/src/handlers/session/authentication/authenticated_handlers/user_account.rs
+++ server/admin_service/src/handlers/session/authentication/authenticated_handlers/user_account.rs
@@ -2,6 +2,7 @@
 
 pub(crate) mod agreements_by_user_account_id;
 pub(crate) mod careers_by_user_account_id;
+pub(crate) mod consultation_reqs_by_consultant_id;
 pub(crate) mod consultation_reqs_by_user_account_id;
 pub(crate) mod fee_per_hour_in_yen_by_user_account_id;
 pub(crate) mod identity_option_by_user_account_id;
@@ -18,22 +19,22 @@ use crate::err::Code;
 
 #[derive(Deserialize)]
 pub(crate) struct UserAccountIdQuery {
-    pub(super) user_account_id: i64,
+    user_account_id: i64,
 }
 
 #[derive(Serialize, Debug, Clone, PartialEq)]
 pub(crate) struct UserAccountRetrievalResult {
-    pub(super) user_account: Option<UserAccount>,
+    user_account: Option<UserAccount>,
 }
 
 #[derive(Serialize, Debug, Clone, PartialEq)]
-pub(super) struct UserAccount {
-    pub(super) user_account_id: i64,
-    pub(super) email_address: String,
-    pub(super) last_login_time: Option<String>, // RFC 3339形式の文字列
-    pub(super) created_at: String,              // RFC 3339形式の文字列
-    pub(super) mfa_enabled_at: Option<String>,  // RFC 3339形式の文字列
-    pub(super) disabled_at: Option<String>,     // RFC 3339形式の文字列
+struct UserAccount {
+    user_account_id: i64,
+    email_address: String,
+    last_login_time: Option<String>, // RFC 3339形式の文字列
+    created_at: String,              // RFC 3339形式の文字列
+    mfa_enabled_at: Option<String>,  // RFC 3339形式の文字列
+    disabled_at: Option<String>,     // RFC 3339形式の文字列
 }
 
 fn validate_user_account_id_is_positive(user_account_id: i64) -> Result<(), ErrResp> {
@@ -47,6 +48,16 @@ fn validate_user_account_id_is_positive(user_account_id: i64) -> Result<(), ErrR
         ));
     }
     Ok(())
+}
+
+#[derive(Deserialize)]
+pub(crate) struct ConsultantIdQuery {
+    consultant_id: i64,
+}
+
+#[derive(Serialize, Clone, Debug, PartialEq, Eq)]
+pub(crate) struct ConsultationReqsResult {
+    consultation_reqs: Vec<ConsultationReq>,
 }
 
 #[derive(Serialize, Clone, Debug, PartialEq, Eq)]
--- server/admin_service/src/handlers/session/authentication/authenticated_handlers/user_account/consultation_reqs_by_consultant_id.rs
+++ server/admin_service/src/handlers/session/authentication/authenticated_handlers/user_account/consultation_reqs_by_consultant_id.rs
@@ -10,24 +10,24 @@ use tracing::error;
 use crate::err::unexpected_err_resp;
 
 use super::super::admin::Admin;
-use super::{ConsultationReq, ConsultationReqsResult, UserAccountIdQuery};
+use super::{ConsultantIdQuery, ConsultationReq, ConsultationReqsResult};
 
-pub(crate) async fn get_consultation_reqs_by_user_account_id(
+pub(crate) async fn get_consultation_reqs_by_consultant_id(
     Admin { admin_info: _ }: Admin, // 認証されていることを保証するために必須のパラメータ
-    query: Query<UserAccountIdQuery>,
+    query: Query<ConsultantIdQuery>,
     State(pool): State<DatabaseConnection>,
 ) -> RespResult<ConsultationReqsResult> {
     let query = query.0;
     let op = ConsultationReqsOperationImpl { pool };
-    get_consultation_reqs_by_user_account_id_internal(query.user_account_id, op).await
+    get_consultation_reqs_by_consultant_id_internal(query.consultant_id, op).await
 }
 
-async fn get_consultation_reqs_by_user_account_id_internal(
-    user_account_id: i64,
+async fn get_consultation_reqs_by_consultant_id_internal(
+    consultant_id: i64,
     op: impl ConsultationReqsOperation,
 ) -> RespResult<ConsultationReqsResult> {
     let consultation_reqs = op
-        .get_consultation_reqs_by_user_account_id(user_account_id)
+        .get_consultation_reqs_by_consultant_id(consultant_id)
         .await?;
     Ok((
         StatusCode::OK,
@@ -37,9 +37,9 @@ async fn get_consultation_reqs_by_user_account_id_internal(
 
 #[async_trait]
 trait ConsultationReqsOperation {
-    async fn get_consultation_reqs_by_user_account_id(
+    async fn get_consultation_reqs_by_consultant_id(
         &self,
-        user_account_id: i64,
+        consultant_id: i64,
     ) -> Result<Vec<ConsultationReq>, ErrResp>;
 }
 
@@ -49,18 +49,18 @@ struct ConsultationReqsOperationImpl {
 
 #[async_trait]
 impl ConsultationReqsOperation for ConsultationReqsOperationImpl {
-    async fn get_consultation_reqs_by_user_account_id(
+    async fn get_consultation_reqs_by_consultant_id(
         &self,
-        user_account_id: i64,
+        consultant_id: i64,
     ) -> Result<Vec<ConsultationReq>, ErrResp> {
         let models = entity::consultation_req::Entity::find()
-            .filter(entity::consultation_req::Column::UserAccountId.eq(user_account_id))
+            .filter(entity::consultation_req::Column::ConsultantId.eq(consultant_id))
             .all(&self.pool)
             .await
             .map_err(|e| {
                 error!(
-                    "failed to filter consultation_req (user_account_id: {}): {}",
-                    user_account_id, e
+                    "failed to filter consultation_req (consultant_id: {}): {}",
+                    consultant_id, e
                 );
                 unexpected_err_resp()
             })?;
@@ -107,28 +107,28 @@ mod tests {
     use super::*;
 
     struct ConsultationReqsOperationMock {
-        user_account_id: i64,
+        consultant_id: i64,
         consultation_reqs: Vec<ConsultationReq>,
     }
 
     #[async_trait]
     impl ConsultationReqsOperation for ConsultationReqsOperationMock {
-        async fn get_consultation_reqs_by_user_account_id(
+        async fn get_consultation_reqs_by_consultant_id(
             &self,
-            user_account_id: i64,
+            consultant_id: i64,
         ) -> Result<Vec<ConsultationReq>, ErrResp> {
-            if self.user_account_id != user_account_id {
+            if self.consultant_id != consultant_id {
                 return Ok(vec![]);
             }
             Ok(self.consultation_reqs.clone())
         }
     }
 
-    fn create_dummy_consultation_req1(user_account_id: i64) -> ConsultationReq {
+    fn create_dummy_consultation_req1(consultant_id: i64) -> ConsultationReq {
         ConsultationReq {
             consultation_req_id: 1,
-            user_account_id,
-            consultant_id: 510,
+            user_account_id: 510,
+            consultant_id,
             first_candidate_date_time: "2023-04-13T14:00:00.0000+09:00 ".to_string(),
             second_candidate_date_time: "2023-04-14T14:00:00.0000+09:00 ".to_string(),
             third_candidate_date_time: "2023-04-15T14:00:00.0000+09:00 ".to_string(),
@@ -140,11 +140,11 @@ mod tests {
         }
     }
 
-    fn create_dummy_consultation_req2(user_account_id: i64) -> ConsultationReq {
+    fn create_dummy_consultation_req2(consultant_id: i64) -> ConsultationReq {
         ConsultationReq {
             consultation_req_id: 2,
-            user_account_id,
-            consultant_id: 6110,
+            user_account_id: 6110,
+            consultant_id,
             first_candidate_date_time: "2023-04-13T16:00:00.0000+09:00 ".to_string(),
             second_candidate_date_time: "2023-04-14T16:00:00.0000+09:00 ".to_string(),
             third_candidate_date_time: "2023-04-15T16:00:00.0000+09:00 ".to_string(),
@@ -158,16 +158,15 @@ mod tests {
 
     #[tokio::test]
 
-    async fn get_consultation_reqs_by_user_account_id_internal_success_1_result() {
-        let user_account_id = 64431;
-        let consultation_req1 = create_dummy_consultation_req1(user_account_id);
+    async fn get_consultation_reqs_by_consultant_id_internal_success_1_result() {
+        let consultant_id = 64431;
+        let consultation_req1 = create_dummy_consultation_req1(consultant_id);
         let op_mock = ConsultationReqsOperationMock {
-            user_account_id,
+            consultant_id,
             consultation_reqs: vec![consultation_req1.clone()],
         };
 
-        let result =
-            get_consultation_reqs_by_user_account_id_internal(user_account_id, op_mock).await;
+        let result = get_consultation_reqs_by_consultant_id_internal(consultant_id, op_mock).await;
 
         let resp = result.expect("failed to get Ok");
         assert_eq!(StatusCode::OK, resp.0);
@@ -176,17 +175,16 @@ mod tests {
 
     #[tokio::test]
 
-    async fn get_consultation_reqs_by_user_account_id_internal_success_2_results() {
-        let user_account_id = 64431;
-        let consultation_req1 = create_dummy_consultation_req1(user_account_id);
-        let consultation_req2 = create_dummy_consultation_req2(user_account_id);
+    async fn get_consultation_reqs_by_consultant_id_internal_success_2_results() {
+        let consultant_id = 64431;
+        let consultation_req1 = create_dummy_consultation_req1(consultant_id);
+        let consultation_req2 = create_dummy_consultation_req2(consultant_id);
         let op_mock = ConsultationReqsOperationMock {
-            user_account_id,
+            consultant_id,
             consultation_reqs: vec![consultation_req1.clone(), consultation_req2.clone()],
         };
 
-        let result =
-            get_consultation_reqs_by_user_account_id_internal(user_account_id, op_mock).await;
+        let result = get_consultation_reqs_by_consultant_id_internal(consultant_id, op_mock).await;
 
         let resp = result.expect("failed to get Ok");
         assert_eq!(StatusCode::OK, resp.0);
@@ -198,15 +196,14 @@ mod tests {
 
     #[tokio::test]
 
-    async fn get_consultation_reqs_by_user_account_id_internal_success_no_result() {
-        let user_account_id = 64431;
+    async fn get_consultation_reqs_by_consultant_id_internal_success_no_result() {
+        let consultant_id = 64431;
         let op_mock = ConsultationReqsOperationMock {
-            user_account_id,
+            consultant_id,
             consultation_reqs: vec![],
         };
 
-        let result =
-            get_consultation_reqs_by_user_account_id_internal(user_account_id, op_mock).await;
+        let result = get_consultation_reqs_by_consultant_id_internal(consultant_id, op_mock).await;
 
         let resp = result.expect("failed to get Ok");
         assert_eq!(StatusCode::OK, resp.0);
