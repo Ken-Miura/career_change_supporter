7b4e0d6dbd9f5cf1d6b23ed019f8e495ecdf28f7
kmiura <ken.miura1102@gmail.com>
2023-05-26 15:39:23 +0000

    Add tests

--- server/admin_service/src/handlers/session/authentication/authenticated_handlers/user_account/rating_info_by_user_account_id.rs
+++ server/admin_service/src/handlers/session/authentication/authenticated_handlers/user_account/rating_info_by_user_account_id.rs
@@ -100,137 +100,133 @@ impl RatingInfoOperation for RatingInfoOperationImpl {
 
 #[cfg(test)]
 mod tests {
-    // use axum::async_trait;
-    // use axum::http::StatusCode;
-    // use common::ErrResp;
+    use axum::async_trait;
+    use axum::http::StatusCode;
+    use common::ErrResp;
 
-    // use crate::err::Code;
+    use crate::err::Code;
 
-    // use super::*;
+    use super::*;
 
-    // struct RatingInfoOperationMock {
-    //     user_account_id: i64,
-    //     consultations: Vec<Consultation>,
-    // }
+    struct RatingInfoOperationMock {
+        user_account_id: i64,
+        ratings: Vec<i16>,
+    }
 
-    // #[async_trait]
-    // impl RatingInfoOperation for RatingInfoOperationMock {
-    //     async fn get_rating_info_by_user_account_id(
-    //         &self,
-    //         user_account_id: i64,
-    //     ) -> Result<Vec<i16>, ErrResp> {
-    //         if self.user_account_id != user_account_id {
-    //             return Ok(vec![]);
-    //         }
-    //         Ok(self.consultations.clone())
-    //     }
-    // }
+    #[async_trait]
+    impl RatingInfoOperation for RatingInfoOperationMock {
+        async fn get_rating_info_by_user_account_id(
+            &self,
+            user_account_id: i64,
+        ) -> Result<Vec<i16>, ErrResp> {
+            if self.user_account_id != user_account_id {
+                panic!("never reach here");
+            }
+            Ok(self.ratings.clone())
+        }
+    }
 
-    // fn create_dummy_consultation1(user_account_id: i64) -> Consultation {
-    //     Consultation {
-    //         consultation_id: 1,
-    //         user_account_id,
-    //         consultant_id: 510,
-    //         meeting_at: "2023-04-13T14:00:00.0000+09:00 ".to_string(),
-    //         room_name: "1241cd91a9c3433f98d16f40f51a5090".to_string(),
-    //         user_account_entered_at: None,
-    //         consultant_entered_at: None,
-    //     }
-    // }
+    #[tokio::test]
 
-    // fn create_dummy_consultation2(user_account_id: i64) -> Consultation {
-    //     Consultation {
-    //         consultation_id: 2,
-    //         user_account_id,
-    //         consultant_id: 6110,
-    //         meeting_at: "2023-04-15T14:00:00.0000+09:00 ".to_string(),
-    //         room_name: "3241cd91a9c3433f98d16f40f51a5090".to_string(),
-    //         user_account_entered_at: Some("2023-04-15T13:58:32.2424+09:00 ".to_string()),
-    //         consultant_entered_at: Some("2023-04-15T13:57:42.3435+09:00 ".to_string()),
-    //     }
-    // }
+    async fn get_rating_info_by_user_account_id_internal_success_no_result() {
+        let user_account_id = 64431;
+        let ratings = vec![];
+        let op_mock = RatingInfoOperationMock {
+            user_account_id,
+            ratings,
+        };
 
-    // #[tokio::test]
+        let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
 
-    // async fn get_rating_info_by_user_account_id_internal_success_1_result() {
-    //     let user_account_id = 64431;
-    //     let consultation1 = create_dummy_consultation1(user_account_id);
-    //     let op_mock = RatingInfoOperationMock {
-    //         user_account_id,
-    //         consultations: vec![consultation1.clone()],
-    //     };
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(None, resp.1 .0.average_rating);
+        assert_eq!(0, resp.1 .0.count);
+    }
 
-    //     let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
+    #[tokio::test]
 
-    //     let resp = result.expect("failed to get Ok");
-    //     assert_eq!(StatusCode::OK, resp.0);
-    //     assert_eq!(vec![consultation1], resp.1 .0.consultations);
-    // }
+    async fn get_rating_info_by_user_account_id_internal_success_1() {
+        let user_account_id = 64431;
+        let ratings = vec![3];
+        let op_mock = RatingInfoOperationMock {
+            user_account_id,
+            ratings,
+        };
 
-    // #[tokio::test]
+        let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
 
-    // async fn get_rating_info_by_user_account_id_internal_success_2_results() {
-    //     let user_account_id = 64431;
-    //     let consultation1 = create_dummy_consultation1(user_account_id);
-    //     let consultation2 = create_dummy_consultation2(user_account_id);
-    //     let op_mock = RatingInfoOperationMock {
-    //         user_account_id,
-    //         consultations: vec![consultation1.clone(), consultation2.clone()],
-    //     };
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(Some("3.0".to_string()), resp.1 .0.average_rating);
+        assert_eq!(1, resp.1 .0.count);
+    }
 
-    //     let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
+    #[tokio::test]
 
-    //     let resp = result.expect("failed to get Ok");
-    //     assert_eq!(StatusCode::OK, resp.0);
-    //     assert_eq!(vec![consultation1, consultation2], resp.1 .0.consultations);
-    // }
+    async fn get_rating_info_by_user_account_id_internal_success_2() {
+        let user_account_id = 64431;
+        let ratings = vec![3, 4];
+        let op_mock = RatingInfoOperationMock {
+            user_account_id,
+            ratings,
+        };
 
-    // #[tokio::test]
+        let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
 
-    // async fn get_rating_info_by_user_account_id_internal_success_no_result() {
-    //     let user_account_id = 64431;
-    //     let op_mock = RatingInfoOperationMock {
-    //         user_account_id,
-    //         consultations: vec![],
-    //     };
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(Some("3.5".to_string()), resp.1 .0.average_rating);
+        assert_eq!(2, resp.1 .0.count);
+    }
 
-    //     let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
+    #[tokio::test]
 
-    //     let resp = result.expect("failed to get Ok");
-    //     assert_eq!(StatusCode::OK, resp.0);
-    //     assert_eq!(
-    //         Vec::<Consultation>::with_capacity(0),
-    //         resp.1 .0.consultations
-    //     );
-    // }
+    async fn get_rating_info_by_user_account_id_internal_success_3() {
+        let user_account_id = 64431;
+        let ratings = vec![3, 4, 1];
+        let op_mock = RatingInfoOperationMock {
+            user_account_id,
+            ratings,
+        };
 
-    // #[tokio::test]
-    // async fn get_rating_info_by_user_account_id_internal_fail_user_account_id_is_zero() {
-    //     let user_account_id = 0;
-    //     let op_mock = RatingInfoOperationMock {
-    //         user_account_id,
-    //         consultations: vec![],
-    //     };
+        let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
 
-    //     let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
+        let resp = result.expect("failed to get Ok");
+        assert_eq!(StatusCode::OK, resp.0);
+        assert_eq!(Some("2.7".to_string()), resp.1 .0.average_rating);
+        assert_eq!(3, resp.1 .0.count);
+    }
 
-    //     let resp = result.expect_err("failed to get Err");
-    //     assert_eq!(resp.0, StatusCode::BAD_REQUEST);
-    //     assert_eq!(resp.1 .0.code, Code::AccountIdIsNotPositive as u32)
-    // }
+    #[tokio::test]
+    async fn get_rating_info_by_user_account_id_internal_fail_user_account_id_is_zero() {
+        let user_account_id = 0;
+        let ratings = vec![3, 4, 1];
+        let op_mock = RatingInfoOperationMock {
+            user_account_id,
+            ratings,
+        };
 
-    // #[tokio::test]
-    // async fn get_rating_info_by_user_account_id_internal_fail_user_account_id_is_negative() {
-    //     let user_account_id = -1;
-    //     let op_mock = RatingInfoOperationMock {
-    //         user_account_id,
-    //         consultations: vec![],
-    //     };
+        let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
 
-    //     let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
+        let resp = result.expect_err("failed to get Err");
+        assert_eq!(resp.0, StatusCode::BAD_REQUEST);
+        assert_eq!(resp.1 .0.code, Code::AccountIdIsNotPositive as u32)
+    }
 
-    //     let resp = result.expect_err("failed to get Err");
-    //     assert_eq!(resp.0, StatusCode::BAD_REQUEST);
-    //     assert_eq!(resp.1 .0.code, Code::AccountIdIsNotPositive as u32)
-    // }
+    #[tokio::test]
+    async fn get_rating_info_by_user_account_id_internal_fail_user_account_id_is_negative() {
+        let user_account_id = -1;
+        let ratings = vec![3, 4, 1];
+        let op_mock = RatingInfoOperationMock {
+            user_account_id,
+            ratings,
+        };
+
+        let result = get_rating_info_by_user_account_id_internal(user_account_id, op_mock).await;
+
+        let resp = result.expect_err("failed to get Err");
+        assert_eq!(resp.0, StatusCode::BAD_REQUEST);
+        assert_eq!(resp.1 .0.code, Code::AccountIdIsNotPositive as u32)
+    }
 }
