1ee47d72b5a8d6d4a4caadf5f1a1a6561ebbcfab
kmiura <ken.miura1102@gmail.com>
2023-06-07 11:41:56 +0000

    Add mock

--- client/admin_app/tests/unit/UserAccountInfoPage.spec.ts
+++ client/admin_app/tests/unit/UserAccountInfoPage.spec.ts
@@ -1,6 +1,29 @@
+import { ref } from 'vue'
+
+const routerPushMock = jest.fn()
+jest.mock('vue-router', () => ({
+  useRouter: () => ({
+    push: routerPushMock
+  })
+}))
+
+const postUserAccountRetrievalDoneMock = ref(true)
+const postUserAccountRetrievalByUserAccountIdFuncMock = jest.fn()
+const postUserAccountRetrievalByEmailAddressFuncMock = jest.fn()
+jest.mock('@/util/personalized/user-account-info/usePostUserAccountRetrieval', () => ({
+  usePostUserAccountRetrieval: () => ({
+    postUserAccountRetrievalDone: postUserAccountRetrievalDoneMock,
+    postUserAccountRetrievalByUserAccountIdFunc: postUserAccountRetrievalByUserAccountIdFuncMock,
+    postUserAccountRetrievalByEmailAddressFunc: postUserAccountRetrievalByEmailAddressFuncMock
+  })
+}))
+
 describe('UserAccountInfoPage.vue', () => {
   beforeEach(() => {
-    console.log('beforeEach')
+    routerPushMock.mockClear()
+    postUserAccountRetrievalDoneMock.value = true
+    postUserAccountRetrievalByUserAccountIdFuncMock.mockReset()
+    postUserAccountRetrievalByEmailAddressFuncMock.mockReset()
   })
 
   it('tests', () => {
