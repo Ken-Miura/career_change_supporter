0138f1a7d9013c434996f087dd3bcfa5b845aa21
kmiura <ken.miura1102@gmail.com>
2023-08-24 17:14:07 +0000

    Add tests

--- server/delete_expired_consultation_reqs/src/main.rs
+++ server/delete_expired_consultation_reqs/src/main.rs
@@ -620,32 +620,32 @@ mod tests {
         assert_eq!(num_deleted, 1);
     }
 
-    // #[tokio::test]
-    // async fn delete_expired_consultation_reqs_success4() {
-    //     let current_date_time = JAPANESE_TIME_ZONE
-    //         .with_ymd_and_hms(2023, 8, 5, 21, 00, 40)
-    //         .unwrap();
-    //     let max_num_of_target_records = 2;
-    //     let op = DeleteExpiredConsultationReqsOperationMock {
-    //         consultation_reqs: create_dummy_1_expired_consultation_req(current_date_time),
-    //         current_date_time,
-    //         limit: max_num_of_target_records,
-    //     };
-    //     // 成功時はメールを送らないので、わざと失敗するような内容でモックを生成する
-    //     let send_mail_mock =
-    //         SendMailMock::new("".to_string(), "".to_string(), "".to_string(), vec![]);
+    #[tokio::test]
+    async fn delete_expired_consultation_reqs_success4() {
+        let current_date_time = JAPANESE_TIME_ZONE
+            .with_ymd_and_hms(2023, 8, 27, 8, 00, 40)
+            .unwrap();
+        let max_num_of_target_records = 2;
+        let op = DeleteExpiredConsultationReqsOperationMock {
+            consultation_reqs: create_dummy_1_expired_consultation_req(current_date_time),
+            current_date_time,
+            limit: max_num_of_target_records,
+        };
+        // 成功時はメールを送らないので、わざと失敗するような内容でモックを生成する
+        let send_mail_mock =
+            SendMailMock::new("".to_string(), "".to_string(), "".to_string(), vec![]);
 
-    //     let result = delete_expired_consultation_reqs(
-    //         current_date_time,
-    //         max_num_of_target_records,
-    //         &op,
-    //         &send_mail_mock,
-    //     )
-    //     .await;
+        let result = delete_expired_consultation_reqs(
+            current_date_time,
+            max_num_of_target_records,
+            &op,
+            &send_mail_mock,
+        )
+        .await;
 
-    //     let num_deleted = result.expect("failed to get Ok");
-    //     assert_eq!(num_deleted, 1);
-    // }
+        let num_deleted = result.expect("failed to get Ok");
+        assert_eq!(num_deleted, 1);
+    }
 
     // #[tokio::test]
     // async fn delete_expired_consultation_reqs_success5() {
