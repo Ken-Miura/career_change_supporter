f68697449e2ddaf4d2f8275d4628d6da95a41cea
kmiura <ken.miura1102@gmail.com>
2023-08-26 14:43:23 +0000

    Add tests

--- server/delete_expired_deleted_user_accounts/src/main.rs
+++ server/delete_expired_deleted_user_accounts/src/main.rs
@@ -829,86 +829,86 @@ mod tests {
         map
     }
 
-    // #[tokio::test]
-    // async fn delete_expired_deleted_user_accounts_success6() {
-    //     let current_date_time = JAPANESE_TIME_ZONE
-    //         .with_ymd_and_hms(2023, 8, 27, 21, 8, 40)
-    //         .unwrap();
-    //     let max_num_of_target_records = 1;
-    //     let op = DeleteExpiredDeletedUserAccountsOperationMock {
-    //         deleted_user_accounts: create_dummy_2_expired_deleted_user_accounts(current_date_time),
-    //         current_date_time,
-    //         limit: max_num_of_target_records,
-    //     };
-    //     // 成功時はメールを送らないので、わざと失敗するような内容でモックを生成する
-    //     let send_mail_mock =
-    //         SendMailMock::new("".to_string(), "".to_string(), "".to_string(), vec![]);
+    #[tokio::test]
+    async fn delete_expired_deleted_user_accounts_success6() {
+        let current_date_time = JAPANESE_TIME_ZONE
+            .with_ymd_and_hms(2023, 8, 27, 21, 8, 40)
+            .unwrap();
+        let max_num_of_target_records = 1;
+        let op = DeleteExpiredDeletedUserAccountsOperationMock {
+            deleted_user_accounts: create_dummy_2_expired_deleted_user_accounts(current_date_time),
+            current_date_time,
+            limit: max_num_of_target_records,
+        };
+        // 成功時はメールを送らないので、わざと失敗するような内容でモックを生成する
+        let send_mail_mock =
+            SendMailMock::new("".to_string(), "".to_string(), "".to_string(), vec![]);
 
-    //     let result = delete_expired_deleted_user_accounts(
-    //         current_date_time,
-    //         max_num_of_target_records,
-    //         &op,
-    //         &send_mail_mock,
-    //     )
-    //     .await;
+        let result = delete_expired_deleted_user_accounts(
+            current_date_time,
+            max_num_of_target_records,
+            &op,
+            &send_mail_mock,
+        )
+        .await;
 
-    //     let num_deleted = result.expect("failed to get Ok");
-    //     assert_eq!(num_deleted, 1);
-    // }
+        let num_deleted = result.expect("failed to get Ok");
+        assert_eq!(num_deleted, 1);
+    }
 
-    // #[tokio::test]
-    // async fn delete_expired_deleted_user_accounts_success7() {
-    //     let current_date_time = JAPANESE_TIME_ZONE
-    //         .with_ymd_and_hms(2023, 8, 27, 21, 8, 40)
-    //         .unwrap();
-    //     let max_num_of_target_records = 2;
-    //     let op = DeleteExpiredDeletedUserAccountsOperationMock {
-    //         deleted_user_accounts: create_dummy_2_expired_deleted_user_accounts(current_date_time),
-    //         current_date_time,
-    //         limit: max_num_of_target_records,
-    //     };
-    //     // 成功時はメールを送らないので、わざと失敗するような内容でモックを生成する
-    //     let send_mail_mock =
-    //         SendMailMock::new("".to_string(), "".to_string(), "".to_string(), vec![]);
+    #[tokio::test]
+    async fn delete_expired_deleted_user_accounts_success7() {
+        let current_date_time = JAPANESE_TIME_ZONE
+            .with_ymd_and_hms(2023, 8, 27, 21, 8, 40)
+            .unwrap();
+        let max_num_of_target_records = 2;
+        let op = DeleteExpiredDeletedUserAccountsOperationMock {
+            deleted_user_accounts: create_dummy_2_expired_deleted_user_accounts(current_date_time),
+            current_date_time,
+            limit: max_num_of_target_records,
+        };
+        // 成功時はメールを送らないので、わざと失敗するような内容でモックを生成する
+        let send_mail_mock =
+            SendMailMock::new("".to_string(), "".to_string(), "".to_string(), vec![]);
 
-    //     let result = delete_expired_deleted_user_accounts(
-    //         current_date_time,
-    //         max_num_of_target_records,
-    //         &op,
-    //         &send_mail_mock,
-    //     )
-    //     .await;
+        let result = delete_expired_deleted_user_accounts(
+            current_date_time,
+            max_num_of_target_records,
+            &op,
+            &send_mail_mock,
+        )
+        .await;
 
-    //     let num_deleted = result.expect("failed to get Ok");
-    //     assert_eq!(num_deleted, 2);
-    // }
+        let num_deleted = result.expect("failed to get Ok");
+        assert_eq!(num_deleted, 2);
+    }
 
-    // #[tokio::test]
-    // async fn delete_expired_deleted_user_accounts_success8() {
-    //     let current_date_time = JAPANESE_TIME_ZONE
-    //         .with_ymd_and_hms(2023, 8, 27, 8, 0, 40)
-    //         .unwrap();
-    //     let max_num_of_target_records = 3;
-    //     let op = DeleteExpiredDeletedUserAccountsOperationMock {
-    //         deleted_user_accounts: create_dummy_2_expired_deleted_user_accounts(current_date_time),
-    //         current_date_time,
-    //         limit: max_num_of_target_records,
-    //     };
-    //     // 成功時はメールを送らないので、わざと失敗するような内容でモックを生成する
-    //     let send_mail_mock =
-    //         SendMailMock::new("".to_string(), "".to_string(), "".to_string(), vec![]);
+    #[tokio::test]
+    async fn delete_expired_deleted_user_accounts_success8() {
+        let current_date_time = JAPANESE_TIME_ZONE
+            .with_ymd_and_hms(2023, 8, 27, 8, 0, 40)
+            .unwrap();
+        let max_num_of_target_records = 3;
+        let op = DeleteExpiredDeletedUserAccountsOperationMock {
+            deleted_user_accounts: create_dummy_2_expired_deleted_user_accounts(current_date_time),
+            current_date_time,
+            limit: max_num_of_target_records,
+        };
+        // 成功時はメールを送らないので、わざと失敗するような内容でモックを生成する
+        let send_mail_mock =
+            SendMailMock::new("".to_string(), "".to_string(), "".to_string(), vec![]);
 
-    //     let result = delete_expired_deleted_user_accounts(
-    //         current_date_time,
-    //         max_num_of_target_records,
-    //         &op,
-    //         &send_mail_mock,
-    //     )
-    //     .await;
+        let result = delete_expired_deleted_user_accounts(
+            current_date_time,
+            max_num_of_target_records,
+            &op,
+            &send_mail_mock,
+        )
+        .await;
 
-    //     let num_deleted = result.expect("failed to get Ok");
-    //     assert_eq!(num_deleted, 2);
-    // }
+        let num_deleted = result.expect("failed to get Ok");
+        assert_eq!(num_deleted, 2);
+    }
 
     // #[tokio::test]
     // async fn delete_expired_deleted_user_accounts_success9() {
