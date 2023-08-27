21044d4f71502550ce1901c78ccb28e49163686e
kmiura <ken.miura1102@gmail.com>
2023-08-27 14:23:38 +0000

    Adjust layout

--- client/user_app/src/views/PrivacyPolicyPage.vue
+++ client/user_app/src/views/PrivacyPolicyPage.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
+  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 lg:pt-20 pb-6 px-2 lg:px-0" style="font-family:'Lato',sans-serif;">
     <header class="max-w-lg mx-auto">
       <router-link to="/">
-        <h1 class="text-2xl font-bold text-white text-center">就職先・転職先を見極めるためのサイト</h1>
+        <h1 class="text-xl lg:text-2xl font-bold text-white text-center">就職先・転職先を見極めるためのサイト</h1>
       </router-link>
     </header>
-    <main class="bg-white max-w-3xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+    <main class="bg-white max-w-3xl mx-auto p-8 lg:p-12 my-10 rounded-lg shadow-2xl">
       <h1 class="font-bold text-2xl">プライバシーポリシー</h1>
       <p class="mt-2">{{ businessOperatorName }}（以下、「サイト運営者」といいます）は、本ウェブサイト上で提供するサービス（以下、「本サービス」といいます）における、サイト利用者の個人情報の取扱いについて、以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます）を定めます。</p>
       <div class="mt-4">
