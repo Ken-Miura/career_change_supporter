8d7dc646a27bc9b2f177d3a7e5ffdfbf55b3dda2
kmiura <ken.miura1102@gmail.com>
2023-08-30 15:47:46 +0000

    Adjust layout

--- client/user_app/src/views/personalized/DeleteCareerSuccessPage.vue
+++ client/user_app/src/views/personalized/DeleteCareerSuccessPage.vue
@@ -1,11 +1,12 @@
 <template>
   <TheHeader/>
-  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
-    <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 class="font-bold text-lg">{{ message }}</h3>
+  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 lg:pt-20 pb-6 px-2 lg:px-0" style="font-family:'Lato',sans-serif;">
+    <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 lg:p-12 my-10 rounded-lg shadow-2xl">
+      <h3 class="font-bold text-center text-lg lg:text-xl">{{ message }}</h3>
     </main>
-    <footer class="max-w-lg mx-auto flex justify-center text-white">
-      <router-link to="/" class="hover:underline">トップページへ</router-link>
+    <footer class="max-w-lg mx-auto flex flex-col text-white">
+      <router-link to="/profile" class="hover:underline text-center">プロフィールへ</router-link>
+      <router-link to="/" class="mt-6 hover:underline text-center">トップページへ</router-link>
     </footer>
   </div>
 </template>
