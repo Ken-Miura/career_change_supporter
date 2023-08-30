29cc932e665a92bf8e4d82b899f0071a23e7f7b3
kmiura <ken.miura1102@gmail.com>
2023-08-30 15:55:44 +0000

    Adjust layout

--- client/user_app/src/views/personalized/FeePerHourInYenPage.vue
+++ client/user_app/src/views/personalized/FeePerHourInYenPage.vue
@@ -1,14 +1,14 @@
 <template>
   <TheHeader/>
-  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
+  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 lg:pt-20 pb-6 px-2 lg:px-0" style="font-family:'Lato',sans-serif;">
     <div v-if="!postFeePerHourInYenDone" class="m-6">
       <WaitingCircle />
     </div>
-    <main v-else class="flex flex-col justify-center bg-white max-w-2xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 class="font-bold text-2xl">相談一回（１時間）の相談料</h3>
-      <p class="mt-2 text-lg">相談一回（１時間）の相談料には、{{ MIN_FEE_PER_HOUR_IN_YEN }}円以上、{{ MAX_FEE_PER_HOUR_IN_YEN }}円以下の値を設定出来ます。</p>
+    <main v-else class="flex flex-col justify-center bg-white max-w-2xl mx-auto p-8 lg:p-12 my-10 rounded-lg shadow-2xl">
+      <h3 class="font-bold text-xl lg:text-2xl">相談一回（１時間）の相談料</h3>
+      <p class="mt-2 text-base lg:text-lg">相談一回（１時間）の相談料には、{{ MIN_FEE_PER_HOUR_IN_YEN }}円以上、{{ MAX_FEE_PER_HOUR_IN_YEN }}円以下の値を設定出来ます。</p>
       <form @submit.prevent="submitFeePerHourInYen">
-        <div class="m-4 text-2xl grid grid-cols-6">
+        <div class="m-4 text-xl lg:text-2xl grid grid-cols-6">
           <div data-test="fee-input-div" class="mt-2 min-w-full justify-self-start col-span-5 pt-3 rounded bg-gray-200">
             <input v-bind:value="feePerHourInYen" v-on:input="setFeePerHourInYen" type="text" minlength="4" maxlength="5" pattern="\d*" title="半角数字でご入力下さい。" class="text-right bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-gray-600 transition duration-500 px-3 pb-3">
           </div>
@@ -16,7 +16,7 @@
             円
           </div>
         </div>
-        <button data-test="submit-button" class="mt-4 min-w-full bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">相談一回（１時間）の相談料を設定する</button>
+        <button data-test="submit-button" class="mt-4 min-w-full bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">相談料を設定する</button>
         <AlertMessage v-bind:class="['mt-6', { 'hidden': !error.exists }]" v-bind:message="error.message"/>
       </form>
     </main>
