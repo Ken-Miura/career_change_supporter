974e9672ae01db15a19419181c2f57e6c58a994f
kmiura <ken.miura1102@gmail.com>
2023-08-30 15:34:50 +0000

    Adjust layout

--- client/user_app/src/views/personalized/CareerDetailPage.vue
+++ client/user_app/src/views/personalized/CareerDetailPage.vue
@@ -1,39 +1,40 @@
 <template>
   <TheHeader/>
-  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
+  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 lg:pt-20 pb-6 px-2 lg:px-0" style="font-family:'Lato',sans-serif;">
     <div v-if="!getCareerDone" class="m-6">
       <WaitingCircle />
     </div>
     <main v-else>
       <div v-if="error.exists">
-        <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+        <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 lg:p-12 my-10 rounded-lg shadow-2xl">
           <AlertMessage class="mt-2" v-bind:message="error.message"/>
         </div>
       </div>
       <div v-else>
-        <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-          <h3 class="font-bold text-2xl">職務経歴</h3>
-          <div v-if="career !== null" data-test="career-set" class="m-4 text-2xl grid grid-cols-3">
-            <div class="mt-2 justify-self-start col-span-1">勤務先名称</div><div class="mt-2 justify-self-start col-span-2">{{ career.company_name }}</div>
-            <div v-if="career.department_name !== null" class="mt-2 justify-self-start col-span-1">部署名</div><div v-if="career.department_name !== null" class="mt-2 justify-self-start col-span-2">{{ career.department_name }}</div>
-            <div v-if="career.office !== null" class="mt-2 justify-self-start col-span-1">勤務地</div><div v-if="career.office !== null" class="mt-2 justify-self-start col-span-2">{{ career.office }}</div>
-            <div class="mt-2 justify-self-start col-span-1">入社日</div><div class="mt-2 justify-self-start col-span-2">{{ career.career_start_date.year }}年{{ career.career_start_date.month }}月{{ career.career_start_date.day }}日</div>
-            <div v-if="career.career_end_date !== null" class="mt-2 justify-self-start col-span-1">退社日</div><div v-if="career.career_end_date !== null" class="mt-2 justify-self-start col-span-2">{{ career.career_end_date.year }}年{{ career.career_end_date.month }}月{{ career.career_end_date.day }}日</div>
+        <div class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 lg:p-12 my-10 rounded-lg shadow-2xl">
+          <h3 class="font-bold text-xl lg:text-2xl">職務経歴</h3>
+          <div v-if="career !== null" data-test="career-set" class="m-4 text-base lg:text-2xl grid grid-cols-2">
+            <div class="mt-2 justify-self-start col-span-1">勤務先名称</div><div class="mt-2 justify-self-start col-span-1">{{ career.company_name }}</div>
+            <div v-if="career.department_name !== null" class="mt-2 justify-self-start col-span-1">部署名</div><div v-if="career.department_name !== null" class="mt-2 justify-self-start col-span-1">{{ career.department_name }}</div>
+            <div v-if="career.office !== null" class="mt-2 justify-self-start col-span-1">勤務地</div><div v-if="career.office !== null" class="mt-2 justify-self-start col-span-1">{{ career.office }}</div>
+            <div class="mt-2 justify-self-start col-span-1">入社日</div><div class="mt-2 justify-self-start col-span-1">{{ career.career_start_date.year }}年{{ career.career_start_date.month }}月{{ career.career_start_date.day }}日</div>
+            <div v-if="career.career_end_date !== null" class="mt-2 justify-self-start col-span-1">退社日</div><div v-if="career.career_end_date !== null" class="mt-2 justify-self-start col-span-1">{{ career.career_end_date.year }}年{{ career.career_end_date.month }}月{{ career.career_end_date.day }}日</div>
             <div class="mt-2 justify-self-start col-span-1">雇用形態</div>
-            <div v-if="career.contract_type === 'regular'" class="mt-2 justify-self-start col-span-2">正社員</div>
-            <div v-else-if="career.contract_type === 'contract'" class="mt-2 justify-self-start col-span-2">契約社員</div>
-            <div v-else-if="career.contract_type === 'other'" class="mt-2 justify-self-start col-span-2">その他</div>
-            <div v-else class="mt-2 justify-self-start col-span-2">想定外の値です。管理者にご連絡下さい</div>
-            <div v-if="career.profession !== null" class="mt-2 justify-self-start col-span-1">職種</div><div v-if="career.profession !== null" class="mt-2 justify-self-start col-span-2">{{ career.profession }}</div>
-            <div v-if="career.annual_income_in_man_yen !== null" class="mt-2 justify-self-start col-span-1">年収（単位：万円）</div><div v-if="career.annual_income_in_man_yen !== null" class="mt-2 justify-self-start col-span-2">{{ career.annual_income_in_man_yen }}</div>
+            <div v-if="career.contract_type === 'regular'" class="mt-2 justify-self-start col-span-1">正社員</div>
+            <div v-else-if="career.contract_type === 'contract'" class="mt-2 justify-self-start col-span-1">契約社員</div>
+            <div v-else-if="career.contract_type === 'other'" class="mt-2 justify-self-start col-span-1">その他</div>
+            <div v-else class="mt-2 justify-self-start col-span-1">想定外の値です。管理者にご連絡下さい</div>
+            <div v-if="career.profession !== null" class="mt-2 justify-self-start col-span-1">職種</div><div v-if="career.profession !== null" class="mt-2 justify-self-start col-span-1">{{ career.profession }}</div>
+            <div v-if="career.annual_income_in_man_yen !== null" class="mt-2 justify-self-start col-span-1">年収（単位：万円）</div><div v-if="career.annual_income_in_man_yen !== null" class="mt-2 justify-self-start col-span-1">{{ career.annual_income_in_man_yen }}</div>
             <div class="mt-2 justify-self-start col-span-1">管理職区分</div>
-            <div v-if="career.is_manager" class="mt-2 justify-self-start col-span-2">管理職</div>
-            <div v-else class="mt-2 justify-self-start col-span-2">非管理職</div>
-            <div v-if="career.position_name !== null" class="mt-2 justify-self-start col-span-1">職位</div><div v-if="career.position_name !== null" class="mt-2 justify-self-start col-span-2">{{ career.position_name }}</div>
+            <div v-if="career.is_manager" class="mt-2 justify-self-start col-span-1">管理職</div>
+            <div v-else class="mt-2 justify-self-start col-span-1">非管理職</div>
+            <div v-if="career.position_name !== null" class="mt-2 justify-self-start col-span-1">職位</div><div v-if="career.position_name !== null" class="mt-2 justify-self-start col-span-1">{{ career.position_name }}</div>
             <div class="mt-2 justify-self-start col-span-1">入社区分</div>
-            <div v-if="career.is_new_graduate" class="mt-2 justify-self-start col-span-2">新卒入社</div>
-            <div v-else class="mt-2 justify-self-start col-span-2">中途入社</div>
-            <div v-if="career.note !== null" class="mt-2 justify-self-start col-span-1">備考</div><div v-if="career.note !== null" class="mt-2 justify-self-start col-span-2 whitespace-pre-wrap">{{ career.note }}</div>
+            <div v-if="career.is_new_graduate" class="mt-2 justify-self-start col-span-1">新卒入社</div>
+            <div v-else class="mt-2 justify-self-start col-span-1">中途入社</div>
+            <div v-if="career.note !== null" class="mt-2 justify-self-start col-span-2">備考</div>
+            <div v-if="career.note !== null" class="mt-1 ml-1 justify-self-start col-span-2 whitespace-pre-wrap">{{ career.note }}</div>
           </div>
           <p v-else data-test="no-career-set" class="m-4 text-xl">職務経歴を取得出来ませんでした。</p>
           <button data-test="move-to-career-deletion-confirm-page-button" v-on:click="moveToCareerDeletionConfirmPage" class="mt-4 bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200">職務経歴の削除</button>
