6f16f1fc94683d8a34fb6d16cc77d649cb2aa419
kmiura <ken.miura1102@gmail.com>
2023-08-30 16:07:33 +0000

    Adjust layout

--- client/user_app/src/views/personalized/MfaSettingPage.vue
+++ client/user_app/src/views/personalized/MfaSettingPage.vue
@@ -1,19 +1,19 @@
 <template>
   <TheHeader/>
-  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
+  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 lg:pt-20 pb-6 px-2 lg:px-0" style="font-family:'Lato',sans-serif;">
     <div v-if="!(postTempMfaSecretDone && postDisableMfaReqDone)" class="m-6">
       <WaitingCircle />
     </div>
     <main v-else>
-      <div v-if="errMessageOnOpen" class="flex flex-col justify-center bg-white max-w-2xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+      <div v-if="errMessageOnOpen" class="flex flex-col justify-center bg-white max-w-2xl mx-auto p-8 lg:p-12 my-10 rounded-lg shadow-2xl">
         <AlertMessage v-bind:message="errMessageOnOpen"/>
       </div>
-      <div v-else class="flex flex-col justify-center bg-white max-w-2xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-        <h3 data-test="mfa-setting-label" class="font-bold text-2xl">二段回認証設定</h3>
-        <p data-test="mfa-setting-description" class="mt-4 ml-2 text-xl">二段回認証の設定を変更します。本サービスにおける二段階認証には認証アプリを利用します。二段階認証を有効化するためには、事前にスマートフォンにGoogle Authenticator (<a class="hover:underline" href="https://apps.apple.com/jp/app/google-authenticator/id388497605">iOS版リンク</a>、<a class="hover:underline" href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=ja&gl=US">Android OS版リンク</a>) またはそれに準ずる認証アプリをインストールして下さい。</p>
+      <div v-else class="flex flex-col justify-center bg-white max-w-2xl mx-auto p-8 lg:p-12 my-10 rounded-lg shadow-2xl">
+        <h3 data-test="mfa-setting-label" class="font-bold text-xl lg:text-2xl">二段回認証設定</h3>
+        <p data-test="mfa-setting-description" class="mt-4 ml-2 text-base lg:text-xl">二段回認証の設定を変更します。本サービスにおける二段階認証には認証アプリを利用します。二段階認証を有効化するためには、事前にスマートフォンにGoogle Authenticator (<a class="hover:underline" href="https://apps.apple.com/jp/app/google-authenticator/id388497605">iOS版リンク</a>、<a class="hover:underline" href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=ja&gl=US">Android OS版リンク</a>) またはそれに準ずる認証アプリをインストールして下さい。</p>
         <div class="mt-2 ml-4 grid grid-cols-3">
-          <p data-test="mfa-enabled-label" class="mt-4 justify-self-start text-xl col-span-2">現在の二段回認証の設定</p>
-          <p data-test="mfa-enabled-value" class="mt-4 justify-self-center text-xl col-span-1">{{ mfaStatus }}</p>
+          <p data-test="mfa-enabled-label" class="mt-4 justify-self-start text-xl lg:text-2xl col-span-2">現在の設定</p>
+          <p data-test="mfa-enabled-value" class="mt-4 justify-self-center text-xl lg:text-2xl col-span-1">{{ mfaStatus }}</p>
         </div>
         <button data-test="change-mfa-setting-button" v-on:click="changeMfaSetting" class="mt-4 min-w-full bg-gray-600 hover:bg-gray-700 text-white font-bold px-6 py-3 rounded shadow-lg hover:shadow-xl transition duration-200 disabled:bg-slate-100 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none">{{ mfaBtnLabel }}</button>
         <div v-if="errMessageOnSubmit">
