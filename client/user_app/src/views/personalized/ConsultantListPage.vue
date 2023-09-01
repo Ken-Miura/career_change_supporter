0b87fc6e8b78dcdd241ddce5f9441389f0f3ff15
kmiura <ken.miura1102@gmail.com>
2023-09-01 14:21:46 +0000

    Adjust layout

--- client/user_app/src/views/personalized/ConsultantListPage.vue
+++ client/user_app/src/views/personalized/ConsultantListPage.vue
@@ -29,18 +29,18 @@
         <div data-test="consultants-area">
           <div v-if="consultantsSearchResult.consultants.length !== 0" class="flex flex-col justify-center my-5">
             <div v-bind:data-test="'consultant-id-' + consultant.consultant_id" v-for="consultant in consultantsSearchResult.consultants" v-bind:key="consultant.consultant_id" class="bg-white p-8 lg:p-12 my-5 rounded-lg shadow-2xl">
-              <h3 class="font-bold text-xl">コンサルタントID: {{ consultant.consultant_id }}</h3>
-              <p class="mt-3 text-xl">相談一回（１時間）の相談料：{{ consultant.fee_per_hour_in_yen }} 円</p>
-              <div class="mt-3 text-xl">評価：<span v-if="consultant.rating"> {{ consultant.rating }}</span><span v-else>0</span>/5（評価件数：{{ consultant.num_of_rated }} 件）</div>
-              <div class="mt-5 font-bold text-xl">職務経歴概要</div>
+              <h3 class="font-bold text-lg lg:text-xl">コンサルタントID: {{ consultant.consultant_id }}</h3>
+              <p class="mt-3 text-lg lg:text-xl">相談一回（１時間）の相談料：{{ consultant.fee_per_hour_in_yen }} 円</p>
+              <div class="mt-3 text-lg lg:text-xl">評価：<span v-if="consultant.rating"> {{ consultant.rating }}</span><span v-else>0</span>/5（評価件数：{{ consultant.num_of_rated }} 件）</div>
+              <div class="mt-5 font-bold text-lg lg:text-xl">職務経歴概要</div>
               <ul>
                 <li v-for="(consultantCareerDescription, index) in consultant.careers" v-bind:key="consultantCareerDescription.consultant_career_id">
-                  <div class="mt-2">
+                  <div class="mt-3 text-lg lg:text-xl">
                     <div class="bg-gray-600 text-white font-bold rounded-t px-4 py-2">職務経歴概要{{ index + 1 }}</div>
-                    <div class="border border-t-0 border-gray-600 rounded-b bg-white px-4 py-3 text-black text-xl grid grid-cols-3">
-                      <div class="mt-2 justify-self-start col-span-1">勤務先名称</div><div class="justify-self-start col-span-2">{{ consultantCareerDescription.company_name }}</div>
-                      <div v-if="consultantCareerDescription.profession" class="mt-2 justify-self-start col-span-1">職種</div><div v-if="consultantCareerDescription.profession" class="justify-self-start col-span-2">{{ consultantCareerDescription.profession }}</div>
-                      <div v-if="consultantCareerDescription.office" class="mt-2 justify-self-start col-span-1">勤務地</div><div v-if="consultantCareerDescription.office" class="justify-self-start col-span-2">{{ consultantCareerDescription.office }}</div>
+                    <div class="border border-t-0 border-gray-600 rounded-b bg-white px-4 py-3 text-black grid grid-cols-3">
+                      <div class="justify-self-start col-span-1">勤務先名称</div><div class="justify-self-start col-span-2">{{ consultantCareerDescription.company_name }}</div>
+                      <div v-if="consultantCareerDescription.profession" class="justify-self-start col-span-1">職種</div><div v-if="consultantCareerDescription.profession" class="justify-self-start col-span-2">{{ consultantCareerDescription.profession }}</div>
+                      <div v-if="consultantCareerDescription.office" class="justify-self-start col-span-1">勤務地</div><div v-if="consultantCareerDescription.office" class="justify-self-start col-span-2">{{ consultantCareerDescription.office }}</div>
                     </div>
                   </div>
                 </li>
