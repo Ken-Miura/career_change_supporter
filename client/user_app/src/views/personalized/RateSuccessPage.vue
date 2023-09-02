fc857a88f995c5c291d8385edbc9a7c8417935ad
kmiura <ken.miura1102@gmail.com>
2023-09-02 12:55:18 +0000

    Adjust layout

--- client/user_app/src/views/personalized/RateSuccessPage.vue
+++ client/user_app/src/views/personalized/RateSuccessPage.vue
@@ -1,14 +1,14 @@
 <template>
   <TheHeader/>
-  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
-    <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 class="font-bold text-lg text-center">{{ message }}</h3>
-      <router-link to="/unrated-item-list" class="mt-4 text-center hover:underline">未評価のコンサルタント、ユーザー一覧へ</router-link>
+  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 lg:pt-20 pb-6 px-2 lg:px-0" style="font-family:'Lato',sans-serif;">
+    <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 lg:p-12 my-10 rounded-lg shadow-2xl">
+      <h3 class="font-bold text-lg lg:text-xl text-center">{{ message }}</h3>
     </main>
-    <footer class="max-w-lg mx-auto flex justify-center text-white">
-      <router-link to="/" class="hover:underline">トップページへ</router-link>
+    <footer class="max-w-lg mx-auto flex flex-col text-white">
+      <router-link to="/unrated-item-list" class="hover:underline text-center">評価へ</router-link>
+      <router-link to="/" class="mt-6 hover:underline text-center">トップページへ</router-link>
     </footer>
-  </div>
+</div>
 </template>
 
 <script lang="ts">
