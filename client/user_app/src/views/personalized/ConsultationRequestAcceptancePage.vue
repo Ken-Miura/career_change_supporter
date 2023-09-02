8c7412f633f4a5cf4dcd792b8bee5131965b7812
kmiura <ken.miura1102@gmail.com>
2023-09-02 05:47:47 +0000

    Adjust layout

--- client/user_app/src/views/personalized/ConsultationRequestAcceptancePage.vue
+++ client/user_app/src/views/personalized/ConsultationRequestAcceptancePage.vue
@@ -1,12 +1,12 @@
 <template>
   <TheHeader/>
-  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
-    <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-      <h3 class="font-bold text-lg text-center">{{ message }}</h3>
-      <router-link to="/consultation-request-list" class="mt-6 text-center hover:underline">相談受け付けへ</router-link>
+  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 lg:pt-20 pb-6 px-2 lg:px-0" style="font-family:'Lato',sans-serif;">
+    <main class="flex flex-col justify-center bg-white max-w-lg mx-auto p-8 lg:p-12 my-10 rounded-lg shadow-2xl">
+      <h3 class="font-bold text-lg lg:text-xl text-center">{{ message }}</h3>
     </main>
-    <footer class="max-w-lg mx-auto flex justify-center text-white">
-      <router-link to="/" class="hover:underline">トップページへ</router-link>
+    <footer class="max-w-lg mx-auto flex flex-col text-white">
+      <router-link to="/consultation-request-list" class="hover:underline text-center">相談受け付けへ</router-link>
+      <router-link to="/" class="mt-6 hover:underline text-center">トップページへ</router-link>
     </footer>
   </div>
 </template>
