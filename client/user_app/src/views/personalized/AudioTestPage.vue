a99e67094df1feb10acb74fbd0c8cdf2294aec7e
kmiura <ken.miura1102@gmail.com>
2023-09-02 06:18:29 +0000

    Adjust layout

--- client/user_app/src/views/personalized/AudioTestPage.vue
+++ client/user_app/src/views/personalized/AudioTestPage.vue
@@ -1,18 +1,18 @@
 <template>
   <TheHeader/>
-  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
+  <div class="bg-gradient-to-r from-gray-500 to-gray-900 min-h-screen pt-12 lg:pt-20 pb-6 px-2 lg:px-0" style="font-family:'Lato',sans-serif;">
     <div v-if="(!refreshDone || audioContextInitilizing)" class="m-6">
       <WaitingCircle />
     </div>
-    <main v-else class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+    <main v-else class="flex flex-col justify-center bg-white max-w-4xl mx-auto p-8 lg:p-12 my-10 rounded-lg shadow-2xl">
       <div v-if="refreshErrorMessage">
         <AlertMessage class="mt-2" v-bind:message="refreshErrorMessage"/>
       </div>
       <div v-else>
-        <h3 class="font-bold text-2xl">音声入出力テスト</h3>
-        <p class="mt-4 ml-2 text-xl">あなたがお使いの環境で相談を実施可能かテストを行うことが出来ます（※1）下記の「音声入出力テストを開始」を押し、何かマイクに向けて話しかけて下さい。加工された音声（普段のあなたの声より高い、または低い声）（※2）が聞こえれば、あなたがお使いの環境で相談を実施可能です。</p>
-        <p class="mt-2 ml-2">（※1）音声入出力に関する内容のみがテスト対象です。相談を行う前に通信環境が問題ないことは別途ご確認下さい。</p>
-        <p class="ml-2">（※2）（音声入出力テスト開始時、相談開始時のどちらの場合でも）声の高さの変化具合はランダムに決まります。もし、加工後の音声が聞き取りづらい場合、「音声入出力テストを停止」を押し、その後、再度「音声入出力テストを開始」を押して下さい。</p>
+        <h3 class="font-bold text-xl lg:text-2xl">音声入出力テスト</h3>
+        <p class="mt-4 ml-2 text-base lg:text-xl">あなたがお使いの環境で相談を実施可能かテストを行うことが出来ます（※1）下記の「音声入出力テストを開始」を押し、何かマイクに向けて話しかけて下さい。加工された音声（普段のあなたの声より高い、または低い声）（※2）が聞こえれば、あなたがお使いの環境で相談を実施可能です。</p>
+        <p class="mt-2 ml-2 text-sm lg:text-base">（※1）音声入出力に関する内容のみがテスト対象です。相談を行う前に通信環境が問題ないことは別途ご確認下さい。</p>
+        <p class="ml-2 text-sm lg:text-base">（※2）（音声入出力テスト開始時、相談開始時のどちらの場合でも）声の高さの変化具合はランダムに決まります。もし、加工後の音声が聞き取りづらい場合、「音声入出力テストを停止」を押し、その後、再度「音声入出力テストを開始」を押して下さい。</p>
         <div class="mt-4 ml-4">
           <div v-if="audioTestErrorMessage">
             <AlertMessage class="mt-2" v-bind:message="audioTestErrorMessage"/>
