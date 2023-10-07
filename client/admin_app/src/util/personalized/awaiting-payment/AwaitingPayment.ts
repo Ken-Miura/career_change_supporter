65d833911590bb88a84eda6f5f6fe9ab0ca0eb9a
kmiura <ken.miura1102@gmail.com>
2023-10-07 08:31:50 +0000

    Change impl

--- client/admin_app/src/util/personalized/awaiting-payment/AwaitingPayment.ts
+++ client/admin_app/src/util/personalized/awaiting-payment/AwaitingPayment.ts
@@ -5,7 +5,6 @@ export type AwaitingPayment = {
     user_account_id: number,
     meeting_at: string, // RFC 3339形式の文字列
     fee_per_hour_in_yen: number,
-    sender_name: string,
-    sender_name_suffix: string,
+    sender_name: string | null,
     /* eslint-enable camelcase */
 }
